3ecfa19291c1cec0c440d2530b12b40de7d84795
Murilo Montino <51464383+murilomontino@users.noreply.github.com>
2023-01-13 15:58:01 -0300

    init commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
+}
+
+module.exports = nextConfig
