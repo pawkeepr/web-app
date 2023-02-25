23f2db321526f2d89cdaa49e0505b6617d2a7c5b
murilomontino <murilomontinojr@hotmail.com>
2023-02-25 18:57:50 -0300

    🧹 chore: configurado vitest para testes unitários e de integração ("re #43", "re #44".)
    "re #43", "re #44".

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vitest/config'
+
+import react from '@vitejs/plugin-react'
+import check from 'vite-plugin-checker'
+
+import { config } from 'dotenv'
+import path from 'path'
+
+config()
+
+export default defineConfig({
+	root: __dirname,
+	plugins: [
+		react(),
+		check({
+			typescript: true,
+		})
+	],
+	test: {
+		globals: true,
+		environment: 'jsdom',  // <==
+		coverage: {
+			provider: 'istanbul', // or 'c8'
+		},
+		reporters: ['html', 'default'],
+		testTimeout: 5000000,
+		exclude: [
+			'**/node_modules/**',
+			'**/.next/**',
+			'**/.docker/**'
+		],
+	},
+	resolve: {
+		alias: {
+			'~/': path.resolve(__dirname, 'source'),
+		},
+	},
+})
