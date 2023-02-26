f947496fb2b08ffdb8475bceada2f938f9efd646
murilomontino <murilomontinojr@hotmail.com>
2023-02-25 22:29:05 -0300

    🧪 test(footer-auth): fix de testes unitários e novo snap (fix #43)
    fix #43

--- source/Components/organism/footer-auth/footer-auth.spec.tsx
+++ source/Components/organism/footer-auth/footer-auth.spec.tsx
@@ -1,18 +1,16 @@
-import { cleanup, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
 import FooterAuth from './footer-auth';
 
-afterEach(cleanup)
-
-describe('FooterAuth', () => {
-    it('renders', async () => {
-        const { baseElement } = render(<FooterAuth />);
-        expect(baseElement).toMatchSnapshot();
+describe('Test FooterAuth component', () => {
+    it('should render snap footer auth (Unit)', async () => {
+        const { asFragment } = render(<FooterAuth />);
+        expect(asFragment()).toMatchSnapshot();
     });
-    it('should ensure the footer contains the PawKeeprs text', async () => {
+    it('should ensure the footer contains the PawKeeprs text (Unit)', async () => {
         render(<FooterAuth />);
         // Verifica se PawKeeprs está no footer
-        expect(screen.getByText(/PawKeeprs/i)).toBeTruthy();
+        expect(screen.getByText(/PawKeeprs/i)).toBeInTheDocument();
     })
 })
