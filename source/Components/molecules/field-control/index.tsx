f083e7ec05d62882324dd16456aea10bb1e1e2ff
murilomontino <murilomontinojr@hotmail.com>
2023-03-03 15:52:29 -0300

    ✨ feat(field-control): novo componente para input de texto, com label e erro acoplados

---
+++ source/Components/molecules/field-control/index.tsx
@@ -0,0 +1,65 @@
+import { useField } from 'formik';
+import Form from 'react-bootstrap/Form';
+import { If } from '~/utils/tsx-control-statements';
+
+import React from 'react';
+import InputGroup from 'react-bootstrap/InputGroup';
+import ErrMessage from '~/Components/atoms/err-message';
+
+type InputControlProps = {
+    [key: string]: any
+    name: string
+    label?: string
+    required?: boolean
+    component?: JSX.Element
+    startChildren?: React.ReactNode | React.ReactNode[] | JSX.Element | JSX.Element[]
+    children?: React.ReactNode | React.ReactNode[] | JSX.Element | JSX.Element[]
+
+}
+
+const FieldControl = ({ label, children, required = false, component, startChildren, ...props }: InputControlProps) => {
+
+    const [inputProps, meta] = useField(props)
+    const id = props.name || props.id
+
+    const InputComponent = component as any
+
+
+    return (
+        // <div className="mb-2 position-relative" style={{ height: '92px', maxHeight: '92px' }}>
+        <>
+            <If condition={!!label}>
+                <Form.Label htmlFor={id}>
+                    {label}
+                    <If condition={required}>
+                        <span className="text-danger">*</span>
+                    </If>
+                </Form.Label>
+            </If>
+            <InputGroup className="position-relative mb-2">
+
+                {startChildren}
+
+                <InputComponent
+                    id={id}
+                    required={required}
+                    {...inputProps}
+                    {...props}
+                />
+
+                {children}
+
+            </InputGroup>
+            <If condition={meta.touched && !!meta.error}>
+                <ErrMessage message={meta.error?.toString() as string} />
+            </If>
+        </>
+        //  </div> 
+    )
+}
+
+FieldControl.defaultProps = {
+    component: 'input'
+}
+
+export default FieldControl
