de510e99c3159e9b66c59660bf883d943757128a
murilomontino <murilomontinojr@hotmail.com>
2023-03-05 23:48:16 -0300

    🧪 test(err-handler): crição de testes unitários para err-handler

--- source/store/auth/loginV2/types.ts
+++ source/store/auth/loginV2/types.ts
@@ -2,21 +2,23 @@ import LOADING from '~/constants/loading';
 
 export const name = 'Auth/Login'
 
+export type IUser = {
+    id: number,
+    username: string,
+    email: string,
+    eh_admin: boolean,
+    tipo_perfil: number,
+    autenticado: boolean,
+    ativo: boolean,
+    data_cadastro: string,
+    nome?: string,
+    sobrenome?: string
+} | null;
+
 export type LoginState = {
     isAuthenticated: boolean;
     rememberMe: boolean;
-    user: {
-        id: number,
-        username: string,
-        email: string,
-        eh_admin: boolean,
-        tipo_perfil: number,
-        autenticado: boolean,
-        ativo: boolean,
-        data_cadastro: string,
-        nome?: string,
-        sobrenome?: string
-    } | null;
+    user: IUser
     token: string;
     error?: string | null;
     isLoading: LOADING;
