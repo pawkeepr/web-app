9057a6640214ed7c95d7ec813df790fc7a5e22a9
murilomontino <murilomontinojr@hotmail.com>
2023-03-08 18:54:35 -0300

    🧪 test(col-auth): adição de testes unitários que verificam o componente

---
+++ source/pages/Authentication/components/organism/col-auth/col-auth.spec.tsx
@@ -0,0 +1,76 @@
+/* eslint-disable testing-library/no-unnecessary-act */
+import { act, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { describe, expect, it, vi } from 'vitest';
+import RouterProvider from '~/__mocks__/next-router';
+import { AuthProvider } from '~/contexts/auth-context';
+import ProviderClient from '~/store';
+import ColAuth from './col-auth';
+
+const Wrapper = () => (
+    <RouterProvider>
+        <ProviderClient>
+            <AuthProvider>
+                <ColAuth />
+            </AuthProvider>
+        </ProviderClient>
+    </RouterProvider>
+)
+
+describe('ColAuth Unit (Unit)', () => {
+
+    afterEach(() => {
+        vi.clearAllMocks()
+    })
+
+    it('should render without errors', () => {
+        const { baseElement } = render(<Wrapper />)
+
+        expect(baseElement).toMatchSnapshot();
+    });
+
+    it('should enable button when form is valid', async () => {
+        render(<Wrapper />)
+        const usernameInput = screen.getByTestId('username-input');
+        const passwordInput = screen.getByTestId('password-input');
+        const submitButton = screen.getByTestId('submit-button');
+
+        act(() => {
+            userEvent.type(usernameInput, 'test-username');
+            userEvent.type(passwordInput, 'test-password');
+        });
+
+        expect(submitButton).toBeEnabled();
+    });
+
+    it('should toggle visible password when clicking the eye icon', async () => {
+        render(<Wrapper />);
+        const passwordInput = screen.getByTestId('password-input');
+        const eyeIcon = screen.getByTestId('toggle-password');
+
+        expect(passwordInput).toHaveAttribute('type', 'password');
+
+        await userEvent.click(eyeIcon);
+
+        expect(passwordInput).toHaveAttribute('type', 'text');
+
+        await userEvent.click(eyeIcon);
+
+        expect(passwordInput).toHaveAttribute('type', 'password');
+
+    });
+
+    it('should disable button when form is invalid', async () => {
+        render(<Wrapper />)
+        const usernameInput = screen.getByTestId('username-input');
+        const passwordInput = screen.getByTestId('password-input');
+        const submitButton = screen.getByTestId('submit-button');
+
+        await userEvent.type(usernameInput, ' ');
+        await userEvent.type(passwordInput, ' ');
+
+        expect(submitButton).toBeDisabled();
+    });
+
+});
+
--- source/pages/Authentication/components/organism/col-auth/col-auth.tsx
+++ source/pages/Authentication/components/organism/col-auth/col-auth.tsx
@@ -3,11 +3,27 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
-import { useAuth } from '~/contexts/auth-context';
+import useAuth from '~/hooks/use-auth';
 import { onChangePassword, onChangeUsername } from '~/store/auth/login/slice';
 import { useAppDispatch } from '~/store/hooks';
 
-import type { ChangeEvent, FormEvent } from 'react';
+import type { ChangeEvent } from 'react';
+import FieldControl from '~/Components/molecules/field-control';
+
+import { Formik } from 'formik';
+import { SignInCredentials } from '~/services/helpers/auth';
+
+import * as Yup from 'yup';
+
+const initialValues: SignInCredentials = {
+    username: '',
+    password: '',
+}
+
+const validationSchema = Yup.object({
+    username: Yup.string().required('Este campo é obrigatório'),
+    password: Yup.string().required('Este campo é obrigatório'),
+})
 
 const ColAuth = () => {
     const dispatch = useAppDispatch();
@@ -22,8 +38,7 @@ const ColAuth = () => {
         visiblePassword,
     } = useAuth();
 
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+    const handleSubmit = ({ password, username }: SignInCredentials) => {
         signIn({
             username,
             password,
@@ -39,76 +54,92 @@ const ColAuth = () => {
     };
 
     return (
-        <Form.Group onSubmit={handleSubmit}>
-            <div className="mb-3">
-                <Form.Label htmlFor="username" className="form-label">
-                    Nome do Usuário:
-                </Form.Label>
-                <Form.Control
-                    type="text"
-                    className="form-control"
-                    name="username"
-                    placeholder="Digite seu nome de usuário"
-                    required
-                    value={username}
-                    onChange={handleChangeUsername}
-                />
-            </div>
+        <Formik
+            validationSchema={validationSchema}
+            initialValues={initialValues}
+            onSubmit={handleSubmit}
+        >
+            {
+                ({ isValid }) => (
+                    <>
+                        <div className="mb-3">
+                            <FieldControl
+                                label='Nome do Usuário:'
+                                type="text"
+                                className="form-control"
+                                name="username"
+                                placeholder="Digite seu nome de usuário"
+                                data-testid="username-input"
+                                required
+                                value={username}
+                                onChange={handleChangeUsername}
+                                disabledError
+                            />
+                        </div>
 
-            <div className="mb-3">
-                <div className="float-end">
-                    <Link href="/forget-password" className="text-muted">
-                        Esqueceu a Senha?
-                    </Link>
-                </div>
-                <Form.Label className="form-label" htmlFor="password-input">
-                    Senha
-                </Form.Label>
-                <div className="position-relative auth-pass-inputgroup mb-3">
-                    <InputGroup>
-                        <Form.Control
-                            type={visiblePassword ? 'text' : 'password'}
-                            className="form-control pe-5 password-input border-end-0"
-                            placeholder="Digite sua senha"
-                            name="password"
-                            required
-                            value={password}
-                            onChange={handleChangePassword}
-                        />
+                        <div className="mb-3">
+                            <div className="float-end">
+                                <Link href="/forget-password" className="text-muted">
+                                    Esqueceu a Senha?
+                                </Link>
+                            </div>
+                            <Form.Label className="form-label" htmlFor="password-input">
+                                Senha
+                            </Form.Label>
+                            <div className="position-relative auth-pass-inputgroup mb-3">
+                                <FieldControl
+                                    type={visiblePassword ? 'text' : 'password'}
+                                    className="form-control pe-5 password-input border-end-0"
+                                    placeholder="Digite sua senha"
+                                    name="password"
+                                    data-testid="password-input"
+                                    required
+                                    value={password}
+                                    onChange={handleChangePassword}
+                                    disabledError
+                                >
+                                    <InputGroup.Text className="bg-transparent border-start-0" >
+                                        <i
+                                            onClick={onToggleVisiblePassword}
+                                            data-testid="toggle-password"
+                                            className={
+                                                visiblePassword ? 'ri-eye-off-fill' : 'ri-eye-fill'
+                                            }
+                                        ></i>
+                                    </InputGroup.Text>
+                                </FieldControl>
+                            </div>
+                        </div>
 
-                        <InputGroup.Text className="bg-transparent border-start-0">
-                            <i
-                                onClick={onToggleVisiblePassword}
-                                className={
-                                    visiblePassword ? 'ri-eye-off-fill' : 'ri-eye-fill'
-                                }
-                            ></i>
-                        </InputGroup.Text>
-                    </InputGroup>
-                </div>
-            </div>
+                        <Form.Check
+                            type="checkbox"
+                            className="w-100"
+                            name="rememberMe"
+                            id="rememberMe"
+                            onChange={onToggleRememberMe}
+                            checked={rememberMe}
+                            label={
+                                <p className="mb-0 fs-12 text-muted fst-italic">
+                                    Lembrar-me
+                                </p>
+                            } />
 
-            <div className="form-check">
-                <Form.Check
-                    value=""
-                    name="auth-remember-check"
-                    checked={rememberMe}
-                    onChange={onToggleRememberMe}
-                />
-                <Form.Label
-                    className="form-check-label"
-                    htmlFor="auth-remember-check"
-                >
-                    Lembrar-me
-                </Form.Label>
-            </div>
+                        <div className="mt-4">
+                            <Button
+                                color="success"
+                                className="w-100"
+                                type="submit"
+                                data-testid="submit-button"
+                                disabled={!isValid}
+                            >
+                                Entrar
+                            </Button>
+                        </div>
+                    </>
+                )
+            }
 
-            <div className="mt-4">
-                <Button color="success" className="w-100" type="submit">
-                    Entrar
-                </Button>
-            </div>
-        </Form.Group>
+        </Formik>
     );
 };
 
