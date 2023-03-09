0a058bdccec779c5eee944a4081b18e543569deb
murilomontino <murilomontinojr@hotmail.com>
2023-03-09 15:10:52 -0300

    🧹 chore: add dep react-input-mask

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,8 @@ import type { AppProps } from 'next/app';
 import { AuthProvider } from "~/contexts/auth-context";
 import LayoutProvider from "~/contexts/layout-context";
 
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '~/globals.scss';
-
 
 export default function App({ Component, pageProps }: AppProps) {
 
