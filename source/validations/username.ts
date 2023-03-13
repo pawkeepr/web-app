0232ef2eba7e88d51b8b1e229973ca68d40429ed
murilomontino <murilomontinojr@hotmail.com>
2023-03-13 10:13:48 -0300

    ♻️ refactor(validations/address): adição de novas regras de validação yup

--- source/validations/username.ts
+++
@@ -1,9 +0,0 @@
-import * as Yup from 'yup';
-
-const validate = Yup.string()
-    .matches(/^[a-zA-Z\s-]+$/, 'O nome deve conter apenas letras, espaços e hífens')
-    .max(50, 'O nome deve ter no máximo 50 caracteres')
-    .min(3, 'O nome de usuário deve ter pelo menos 3 caracteres')
-    .required('Este campo é obrigatório')
-
-export default validate;
