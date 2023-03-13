7032611989535199b0d23bb43bab0887fdd6fe7b
murilomontino <murilomontinojr@hotmail.com>
2023-03-13 10:19:10 -0300

    ✨ feat(step-address): componente de etapa de cadastro endereço finalizada

--- source/pages/Authentication/components/molecules/address/address.tsx
+++ source/pages/Authentication/components/molecules/address/address.tsx
@@ -1,126 +1,93 @@
 import { useFormikContext } from 'formik';
-import { useCallback, useEffect, useState } from 'react';
 import Form from 'react-bootstrap/Form';
-import MaskedInput from 'react-input-mask';
-import Loader from '~/Components/Common/Loader';
 import FieldControl from '~/Components/molecules/field-control/field-control';
-import { fetchAddressByCep } from '~/helpers/fetch-address-by-cep';
+import { InitialStateSignUp } from '~/pages/Authentication/SignUp';
 
+type AddressProps = {
+    loading: boolean
+    disabledInputs: {
+        state: boolean
+        city: boolean
+        neighborhood: boolean
+        street: boolean
+        complement: boolean
+    }
+}
+const AddressInputs = ({ loading, disabledInputs }: AddressProps) => {
 
-const Address = () => {
-    const [loading, setLoading] = useState(false)
-    const { values, setFieldValue } = useFormikContext()
-    const zipCode = (values as any).address.zipCode
+    const { setFieldValue } = useFormikContext<InitialStateSignUp>()
 
-    const setAddress = useCallback(
-        (data: any) => {
-            setFieldValue('address.state', data.uf)
-            setFieldValue('address.city', data.localidade)
-            setFieldValue('address.neighborhood', data.bairro)
-            setFieldValue('address.street', data.logradouro)
-            setFieldValue('address.complement', data.complemento)
-        },
-        [setFieldValue],
-    )
-
-
-    useEffect(() => {
-
-        if (zipCode.replace(/\D/g, '').length === 8) {
-            setLoading(true)
-            fetchAddressByCep(zipCode)
-                .then((data) => {
-                    setAddress(data)
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-                .finally(() => {
-                    setTimeout(() => {
-                        setLoading(false)
-                    }, 1000)
-                })
-        }
-    }, [zipCode, setAddress])
-
+    const onChangeNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target
+        setFieldValue('address.number', value)
+    }
 
     return (
         <>
+
             <FieldControl
                 className="form-control"
                 type="text"
-                label="CEP"
-                name="address.zipCode"
-                placeholder="Digite o CEP"
-                component={MaskedInput as any}
-                mask={"99999-999"}
+                label="Estado"
+                name="address.state"
+                disabled={disabledInputs.state || loading}
+                placeholder={loading ? 'Carregando...' : 'Digite o nome do estado'}
                 required
             />
-            {loading && <Loader loading={loading} />}
-            {!loading && (
-                <div>
-                    <FieldControl
-                        className="form-control"
-                        type="text"
-                        label="Estado"
-                        name="address.state"
-                        placeholder="Digite o nome do estado"
-                        required
-                    />
-                    <FieldControl
-                        className="form-control"
-                        type="text"
-                        label="Cidade"
-                        name="address.city"
-                        placeholder="Digite o nome da cidade"
-                        required
-                    />
-                    <FieldControl
-                        className="form-control"
-                        type="text"
-                        label="Bairro"
-                        name="address.neighborhood"
-                        placeholder="Digite o nome do bairro"
-                        required
-                    />
+            <FieldControl
+                className="form-control"
+                type="text"
+                label="Cidade"
+                name="address.city"
+                disabled={disabledInputs.city || loading}
+                placeholder={loading ? 'Carregando...' : 'Digite o nome da cidade'}
+                required
+            />
+            <FieldControl
+                className="form-control"
+                type="text"
+                label="Bairro"
+                name="address.neighborhood"
+                disabled={disabledInputs.neighborhood || loading}
+                placeholder={loading ? 'Carregando...' : 'Digite o nome do bairro'}
+                required
+            />
 
-                    <FieldControl
-                        label='Rua'
-                        name="address.street"
-                        aria-label="street"
-                        className="form-control w-70"
-                        placeholder="Rua"
-                        required
-                        disabledError
-                    >
+            <FieldControl
+                label='Rua'
+                name="address.street"
+                aria-label="street"
+                className="form-control w-70"
+                disabled={disabledInputs.street || loading}
+                placeholder={loading ? 'Carregando...' : 'Digite o nome da rua'}
+                required
+                disabledError
+            >
+                <Form.Control
+                    type="text"
+                    name="address.number"
+                    aria-label="number"
+                    placeholder="N"
+                    required
+                    onChange={onChangeNumber}
+                    className='ms-1 w-20'
+                    style={{
+                        maxWidth: '20%'
+                    }}
+                />
+            </FieldControl>
 
-                        <Form.Control
-                            type="text"
-                            name="address.number"
-                            aria-label="number"
-                            placeholder="Número"
-                            required
-                            className='ms-1 w-20'
-                            style={{
-                                maxWidth: '20%'
-                            }}
-
-                        />
-
-                    </FieldControl>
-
-                    <FieldControl
-                        className="form-control"
-                        type="text"
-                        label="Complemento"
-                        name="address.complement"
-                        placeholder="Digite o complemento (opcional)"
-                    />
-                </div>
-            )}
+            <FieldControl
+                className="form-control"
+                type="text"
+                label="Complemento"
+                name="address.complement"
+                disabled={disabledInputs.complement || loading}
+                placeholder={loading ? 'Carregando...' : "Digite o complemento (opcional)"}
+            />
 
         </>
     )
 }
 
-export default Address
+export default AddressInputs
---
+++ source/pages/Authentication/components/organism/steps/step-address.tsx
@@ -0,0 +1,96 @@
+
+
+import type { InitialStateSignUp } from '../../../SignUp';
+
+import { useFormikContext } from 'formik';
+import Container from 'react-bootstrap/Container';
+import Address from '../../molecules/address/address';
+
+import { useCallback, useMemo, useState } from 'react';
+
+import MaskedInput from 'react-input-mask';
+import validateAddress from '~/validations/address';
+
+import ErrMessage from '~/Components/atoms/err-message';
+import CountrySelect from '~/Components/molecules/country-select';
+import FieldControl from '~/Components/molecules/field-control/field-control';
+import { IAddress } from '~/helpers/fetch-address-by-cep';
+import useFetchAddress from '~/hooks/use-fetch-address';
+import { StepProps } from './types';
+
+const StepSignUpAddress = ({ nextStep, prevStep, ...rest }: StepProps) => {
+
+    const [disabledInputs, setDisabledInputs] = useState({ state: false, city: false, neighborhood: false, street: false, complement: false })
+
+    const { values, setFieldValue, setFieldError } = useFormikContext<InitialStateSignUp>()
+    const { address } = values
+    const { zipCode } = address
+
+    const updateAddressFields = useCallback(
+        ({ uf, localidade, bairro, logradouro, complemento }: IAddress) => {
+            setFieldValue('address.state', uf || '')
+
+            setFieldValue('address.city', localidade || '')
+
+            setFieldValue('address.neighborhood', bairro || '')
+
+            setFieldValue('address.street', logradouro || '')
+
+            setFieldValue('address.complement', complemento || '')
+
+            setDisabledInputs({
+                state: !!uf,
+                city: !!localidade,
+                neighborhood: !!bairro,
+                street: !!logradouro,
+                complement: !!complemento,
+            })
+        },
+        [setFieldValue],
+    )
+    const { cepInvalid, loading } = useFetchAddress({ onChangeAddress: updateAddressFields, zipCode })
+
+    const requiredFieldsFilled = useMemo((): boolean => {
+        return validateAddress.isValidSync(address) && !cepInvalid
+    }, [address, cepInvalid])
+
+    return (
+        <div className="p-lg-5 p-4">
+            <div>
+                <h5 className="text-primary">Criar Conta</h5>
+                <p className="text-muted">Crie uma conta PawKeeprs gratuita agora e aproveite.</p>
+            </div>
+
+            <Container className="d-flex flex-column mt-4">
+                <CountrySelect />
+                <FieldControl
+                    className="form-control"
+                    type="text"
+                    label="CEP"
+                    name="address.zipCode"
+                    placeholder="Digite o CEP"
+                    component={MaskedInput as any}
+                    mask={"99999-999"}
+                    required
+                />
+                {cepInvalid && <ErrMessage message={'CEP não encontrado'} />}
+                <Address loading={loading} disabledInputs={disabledInputs} />
+
+                <div className="mt-4 d-flex justify-content-center">
+                    <button className="btn btn-success w-40 m-1" type="button" onClick={prevStep}>Anterior</button>
+                    <button
+                        className="btn btn-success w-40 m-1"
+                        type="button"
+                        onClick={nextStep}
+                        disabled={!requiredFieldsFilled || loading}
+                    >
+                        Próximo
+                    </button>
+                </div>
+            </Container>
+
+        </div>
+    )
+}
+
+export default StepSignUpAddress
