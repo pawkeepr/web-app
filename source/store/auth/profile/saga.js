28387f74d942bc8f88e43ed428c6ec87b3fb59d7
murilomontino <murilomontinojr@hotmail.com>
2023-03-16 14:23:53 -0300

    🧹 chore: correções de imports, e delete firebase helpers

--- source/store/auth/profile/saga.js
+++ source/store/auth/profile/saga.js
@@ -1,28 +1,18 @@
-import { takeEvery, fork, put, all, call } from "redux-saga/effects";
+import { all, call, fork, put, takeEvery } from "redux-saga/effects";
 
 // Login Redux States
 import { EDIT_PROFILE } from "./actionTypes";
-import { profileSuccess, profileError } from "./actions";
+import { profileError, profileSuccess } from "./actions";
 //Include Both Helper File with needed methods
-import { getFirebaseBackend } from "../../../helpers/firebase_helper";
 import {
   postFakeProfile,
   postJwtProfile,
 } from "../../../helpers/fakebackend_helper";
 
-const fireBaseBackend = getFirebaseBackend();
-
 function* editProfile({ payload: { user } }) {
 
   try {
-    if (process.env.REACT_APP_DEFAULTAUTH === "firebase") {
-      const response = yield call(
-        fireBaseBackend.editProfileAPI,
-        user.username,
-        user.idx
-      );
-      yield put(profileSuccess(response));
-    } else if (process.env.REACT_APP_DEFAULTAUTH === "jwt") {
+    if (process.env.REACT_APP_DEFAULTAUTH === "jwt") {
       const response = yield call(postJwtProfile, "/post-jwt-profile", {
         username: user.username,
         idx: user.idx,
