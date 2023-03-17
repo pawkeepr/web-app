5fbcccd344780f39f34b4c4f5ed5d38a33badec9
murilomontino <murilomontinojr@hotmail.com>
2023-03-17 09:57:12 -0300

    ♻️ refactor(profiledropdown): conversão de reactstrap para react-bootstrap e jsx -> tsx

--- source/Components/Common/ProfileDropdown.tsx
+++ source/Components/Common/ProfileDropdown.tsx
@@ -1,14 +1,27 @@
 import Image from 'next/image';
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap';
+import React, { useEffect, useState } from 'react';
+
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownItem from 'react-bootstrap/DropdownItem';
+import DropdownMenu from 'react-bootstrap/DropdownMenu';
+import DropdownToggle from 'react-bootstrap/DropdownToggle';
 
 //import images
 import avatar1 from "~/assets/images/users/avatar-1.jpg";
+import { useAppSelector } from '~/store/hooks';
+
+const CustomToggle = ({ onClick }) => (
+    <span className="btn d-flex align-items-center" onClick={onClick}>
+        <Image className="rounded-circle header-profile-user" src={avatar1} alt="Header Avatar" />
+        <span className="text-start ms-xl-2">
+            <span className="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{"Admin"}</span>
+        </span>
+    </span>
+)
 
 const ProfileDropdown = () => {
 
-    const { user } = useSelector(state => ({
+    const { user } = useAppSelector(state => ({
         user: state.Profile.user,
     }));
 
@@ -16,7 +29,7 @@ const ProfileDropdown = () => {
 
     useEffect(() => {
         if (sessionStorage.getItem("authUser")) {
-            const obj = JSON.parse(sessionStorage.getItem("authUser"));
+            const obj = JSON.parse(sessionStorage.getItem("authUser") || '');
             setUserName(user.first_name || obj.data.first_name || "Admin");
         }
     }, [userName, user]);
@@ -27,25 +40,20 @@ const ProfileDropdown = () => {
     const toggleProfileDropdown = () => {
         setIsProfileDropdown(!isProfileDropdown);
     };
+
+    const DropdownToggleButton = () => <CustomToggle onClick={toggleProfileDropdown} />;
+
     return (
         <React.Fragment>
-            <Dropdown isOpen={isProfileDropdown} toggle={toggleProfileDropdown} className="ms-sm-3 header-item topbar-user">
-                <DropdownToggle tag="button" type="button" className="btn">
-                    <span className="d-flex align-items-center">
-                        <Image className="rounded-circle header-profile-user" src={avatar1}
-                            alt="Header Avatar" />
-                        <span className="text-start ms-xl-2">
-                            <span className="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{userName}</span>
-                            <span className="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">Founder</span>
-                        </span>
-                    </span>
-                </DropdownToggle>
+            <Dropdown show={isProfileDropdown} onToggle={toggleProfileDropdown} className="ms-sm-3 header-item topbar-user">
+                <DropdownToggle variant="link" id="dropdown-basic" className="btn" as={DropdownToggleButton} />
+
                 <DropdownMenu className="dropdown-menu-end">
 
                     <h6 className="dropdown-header">Welcome {userName}!</h6>
                     <DropdownItem href="/profile"><i className="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i>
                         <span className="align-middle">Profile</span></DropdownItem>
-                    <DropdownItem href="/apps-chat"><i
+                    {/* <DropdownItem href="/apps-chat"><i
                         className="mdi mdi-message-text-outline text-muted fs-16 align-middle me-1"></i> <span
                             className="align-middle">Messages</span></DropdownItem>
                     <DropdownItem href="#"><i
@@ -63,7 +71,7 @@ const ProfileDropdown = () => {
                             className="mdi mdi-cog-outline text-muted fs-16 align-middle me-1"></i> <span
                                 className="align-middle">Settings</span></DropdownItem>
                     <DropdownItem href="/auth-lockscreen-basic"><i
-                        className="mdi mdi-lock text-muted fs-16 align-middle me-1"></i> <span className="align-middle">Lock screen</span></DropdownItem>
+                        className="mdi mdi-lock text-muted fs-16 align-middle me-1"></i> <span className="align-middle">Lock screen</span></DropdownItem> */}
                     <DropdownItem href="/logout"><i
                         className="mdi mdi-logout text-muted fs-16 align-middle me-1"></i> <span
                             className="align-middle" data-key="t-logout">Logout</span></DropdownItem>
