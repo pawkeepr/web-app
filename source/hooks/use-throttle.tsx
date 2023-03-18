1aff3f84f86ed78826444e94a26972770ad0408c
murilomontino <murilomontinojr@hotmail.com>
2023-03-18 12:23:04 -0300

    ✨ feat(use-throttle): limita a execução da função por um período de tempo

---
+++ source/hooks/use-throttle.tsx
@@ -0,0 +1,22 @@
+import { useRef } from 'react';
+
+function useThrottle<T extends (...args: any[]) => any>(
+    func: T,
+    delay: number,
+): T {
+    const timeoutRef = useRef<NodeJS.Timeout>();
+
+    function throttledFunc(this: any, ...args: any[]) {
+        if (!timeoutRef.current) {
+            timeoutRef.current = setTimeout(() => {
+                timeoutRef.current = undefined;
+            }, delay);
+
+            return func.apply(this, args);
+        }
+    }
+
+    return throttledFunc as T;
+}
+
+export default useThrottle;
