2b3df04bf0b44b5dca8e65360822b3308ce7e556
murilomontino <murilomontinojr@hotmail.com>
2023-03-21 17:52:34 -0300

    🧪 test(auth-context): testes unitários verificando o comportamento do remember e visible

--- source/contexts/auth-context.spec.tsx
+++ source/contexts/auth-context.spec.tsx
@@ -1,4 +1,5 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
 import { afterEach, describe, expect, it, vi } from 'vitest';
 import RouterProvider from '~/__mocks__/next-router';
 import useAuth from '~/hooks/use-auth';
@@ -15,28 +16,64 @@ const TestWrapper = ({ children }: { children: React.ReactNode }) => {
     );
 };
 
+const Component = () => {
+    const {
+        isAuthenticated,
+        visiblePassword,
+        rememberMe,
+        onToggleVisiblePassword,
+        onToggleRememberMe,
+    } = useAuth();
+
+    return (
+        <>
+            <button onClick={onToggleVisiblePassword}>Toggle Password Visibility</button>
+            <input type="checkbox" data-testid="visibility" checked={visiblePassword} />
+            <button onClick={onToggleRememberMe}>Toggle Remember Me</button>
+            <input type="checkbox" data-testid="rememberMe" checked={rememberMe} />
+            {/* Add other data-testid elements for user, password, username, visiblePassword, rememberMe, and signIn */}
+        </>
+    );
+};
+
 describe('AuthContext (Unit)', () => {
 
     afterEach(() => {
         vi.clearAllMocks()
     })
 
-    it('should sign in user', async () => {
-        const wrapper = ({ children }: { children: React.ReactNode }) => <TestWrapper>{children}</TestWrapper>
+    it("should toggle remember me", async () => {
+        render(
+            <TestWrapper>
+                <Component />
+            </TestWrapper>
+        );
 
-        const { result, waitForNextUpdate } = renderHook(() => useAuth(), { wrapper });
+        const button = screen.getByText("Toggle Remember Me");
+        const rememberMeElement = screen.getByTestId("rememberMe");
 
-        result.current.onToggleRememberMe();
-        result.current.onToggleVisiblePassword();
-        result.current.onToggleVisiblePassword();
-        result.current.signIn({
-            username: 'testuser',
-            password: 'testpassword',
-        });
+        await userEvent.click(button);
+        expect(rememberMeElement).toBeChecked();
 
-        await waitForNextUpdate({ timeout: 15000 });
+        await userEvent.click(button);
+        expect(rememberMeElement).not.toBeChecked();
+    })
 
-        expect(result.current.isAuthenticated).toBe(true);
-        expect(result.current.user.name).toEqual('John Doe');
-    });
+    it("should toggle visibility password", async () => {
+        render(
+            <TestWrapper>
+                <Component />
+            </TestWrapper>
+        );
+
+        const button = screen.getByText("Toggle Password Visibility");
+        const passwordVisibility = screen.getByTestId("visibility");
+
+        await userEvent.click(button);
+        expect(passwordVisibility).toBeChecked();
+
+        await userEvent.click(button);
+        expect(passwordVisibility).not.toBeChecked();
+    })
+
 });
