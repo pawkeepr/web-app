6353db832be0a26367be73c6e2a8335afe065ba5
murilomontino <murilomontinojr@hotmail.com>
2023-04-04 10:29:37 -0300

    ♻️ refactor(dashboardpage/section): refatorado para utilização do componente default de layout

--- source/pages/DashboardPage/Section.tsx
+++ source/pages/DashboardPage/Section.tsx
@@ -1,21 +1,15 @@
 'use client'
 
-import React, { useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Nav from 'react-bootstrap/Nav';
-import NavItem from 'react-bootstrap/NavItem';
-import NavLink from 'react-bootstrap/NavLink';
-import Row from 'react-bootstrap/Row';
 import TabContainer from 'react-bootstrap/TabContainer';
 import TabContent from 'react-bootstrap/TabContent';
 import TabPane from 'react-bootstrap/TabPane';
 
 //import images
 
-import MyImage from '~/Components/atoms/my-image/my-image';
 import { useAppSelector } from '~/store/hooks';
+import DefaultLayout from '../_layouts/default/default';
 import OverviewTab from './components/organisms/tabs/OverviewTab';
 import PetsTab from './components/organisms/tabs/PetsTab';
 import TutorsTab from './components/organisms/tabs/TutorsTab';
@@ -37,104 +31,49 @@ const Section = () => {
         return profile?.firstName + " " + profile?.lastName
     }, [profile]);
 
+    const items = [
+        {
+            label: 'Principal',
+            onClick: toggleTab('1'),
+            href: '#Principal'
+        },
+        {
+            label: 'Consultas',
+            onClick: toggleTab('2'),
+            href: '#Consultas'
+        },
+        {
+            label: 'Pets',
+            onClick: toggleTab('3'),
+            href: '#Pets'
+        },
+        {
+            label: 'Tutores',
+            onClick: toggleTab('4'),
+            href: '#Tutores'
+        }
+    ]
+
     return (
-        <React.Fragment>
-            <Row>
-                <Col lg={12}>
-                    <Card className="mt-n4 mx-n4">
-                        <div className="bg-soft-warning">
-                            <Card className="p-4 pb-0">
-                                <Row className="mb-1">
-                                    <div className="col-md">
-                                        <Row className="align-items-center g-3">
-                                            <div className="col-md-auto">
-                                                <div className="avatar-md">
-                                                    <div className="avatar-title bg-white rounded-circle">
-                                                        <MyImage
-                                                            src={profile?.avatar}
-                                                            alt={`Avatar de ${name}`}
-                                                            className="avatar-xs rounded-full"
-                                                            fill
-                                                        />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-md">
-                                                <div>
-                                                    <h4 className="fw-bold">{name}</h4>
-                                                    <div className="hstack gap-3 flex-wrap">
-                                                        <div><i className="ri-building-line align-bottom me-1"></i>{profile?.company}</div>
-                                                        <div className="vr"></div>
-                                                        <div>Criado : <span className="fw-medium">{profile?.created_at}</span></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </Row>
-                                    </div>
+        <DefaultLayout navItems={items}>
+            <TabContainer activeKey={activeTab}>
+                <TabContent className="text-muted">
+                    <TabPane eventKey="1">
+                        <OverviewTab />
+                    </TabPane>
+                    <TabPane eventKey="2">
+                        <VeterinaryAppointmentsTab />
+                    </TabPane>
+                    <TabPane eventKey="3">
+                        <PetsTab />
+                    </TabPane>
+                    <TabPane eventKey="4">
+                        <TutorsTab />
+                    </TabPane>
+                </TabContent>
+            </TabContainer>
+        </DefaultLayout>
 
-                                </Row>
-
-                                <Nav className="nav-tabs-custom border-bottom-0" role="tablist">
-                                    <NavItem>
-                                        <NavLink
-                                            className="fw-bold"
-                                            onClick={toggleTab('1')}
-                                            href="#Principal">
-                                            Principal
-                                        </NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink
-                                            className="fw-bold"
-                                            onClick={toggleTab('2')}
-                                            href="#Consultas">
-                                            Consultas
-                                        </NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink
-                                            className="fw-bold"
-                                            onClick={toggleTab('3')}
-                                            href="#Pets">
-                                            Pets
-                                        </NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink
-                                            className="fw-bold"
-                                            onClick={toggleTab('4')}
-                                            href="#Tutors">
-                                            Tutores
-                                        </NavLink>
-                                    </NavItem>
-
-                                </Nav>
-                            </Card>
-                        </div>
-                    </Card>
-                </Col>
-            </Row>
-            <Row>
-                <Col lg={12}>
-                    <TabContainer activeKey={activeTab}>
-                        <TabContent className="text-muted">
-                            <TabPane eventKey="1">
-                                <OverviewTab />
-                            </TabPane>
-                            <TabPane eventKey="2">
-                                <VeterinaryAppointmentsTab />
-                            </TabPane>
-                            <TabPane eventKey="3">
-                                <PetsTab />
-                            </TabPane>
-                            <TabPane eventKey="4">
-                                <TutorsTab />
-                            </TabPane>
-                        </TabContent>
-                    </TabContainer>
-                </Col>
-            </Row>
-        </React.Fragment>
     );
 };
 
