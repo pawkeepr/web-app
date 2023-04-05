4915d7aab3b00473fecad034465ce2533bf07e8e
murilomontino <murilomontinojr@hotmail.com>
2023-04-05 13:49:25 -0300

    ♻️ refactor(veterinaryappointmentstab): mudança de botão para nova consulta red para new/consult

---
+++ source/routes/index.ts
@@ -0,0 +1,16 @@
+const routes = {
+    dashboard: {
+        root: '/dashboard',
+        new: {
+            appointments: '/dashboard/new/appointments',
+        },
+    },
+    signIn: '/sign-in',
+    signUp: '/sign-up',
+    forgotPassword: '/forgot-password',
+    resetPassword: '/reset-password',
+    profile: '/profile',
+    settings: '/settings',
+}
+
+export default routes
