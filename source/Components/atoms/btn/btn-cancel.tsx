fb066fcba7d3b1a46c352ffdecefdb801072deee
murilomontino <murilomontinojr@hotmail.com>
2023-05-11 09:21:01 -0300

    🐛 fix(btn): correções de cor e efeitos nos btns: hover, bg, text

--- source/Components/atoms/btn/btn-cancel.tsx
+++ source/Components/atoms/btn/btn-cancel.tsx
@@ -3,7 +3,15 @@ import { BtnProps } from './types'
 const BtnCancel = ({ onClick, label = 'Cancelar', className, ...rest }: BtnProps) => {
     return (
         <button
-            className={`btn text-gray-500 hover:text-gray-400 w-40 m-1 previous ${className}`}
+            className={`
+                py-2 rounded-lg 
+                text-gray-500
+                dark:!text-gray-400
+                dark:hover:!text-gray-100
+                hover:text-gray-400 
+                w-40 m-1 
+                previous ${className}
+            `}
             type="button"
             onClick={onClick}
             {...rest}
