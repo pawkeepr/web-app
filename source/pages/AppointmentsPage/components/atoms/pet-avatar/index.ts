e1e9f7f040342a905c19a2ee13c0008118f564cf
murilomontino <murilomontinojr@hotmail.com>
2023-05-12 00:15:11 -0300

    ✨ feat(petavatar): componente que mostra pet e seu nome

---
+++ source/pages/AppointmentsPage/components/atoms/pet-avatar/index.ts
@@ -0,0 +1,1 @@
+export { default } from './pet-avatar';
---
+++ source/pages/AppointmentsPage/components/atoms/pet-avatar/pet-avatar.tsx
@@ -0,0 +1,16 @@
+import { BtnAvatar } from '~/Components/atoms/btn'
+
+type AvatarPetProps = {
+    name: string
+}
+
+const AvatarPet = ({ name }: AvatarPetProps) => {
+    return (
+        <div>
+            <BtnAvatar alt='Avatar do Pet' name="pet.avatar" disabled size={40} />
+            <h5 className="text-center text-muted">{name}</h5>
+        </div>
+    )
+}
+
+export default AvatarPet
