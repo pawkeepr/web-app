9ef1333f15c315d31c72426cbd2cef7d760038c9
murilomontino <murilomontinojr@hotmail.com>
2023-05-26 01:07:59 -0300

    🐛 fix: correção no layout

--- source/contexts/layout-context.tsx
+++ source/contexts/layout-context.tsx
@@ -7,7 +7,7 @@ type LayoutProps = {
     children: React.ReactNode;
 }
 
-const routesAuth = ['/sign-in', '/sign-up', '/forget-password', '/reset-password', '/logout']
+const routesAuth = ['/sign-in', '/sign-up', '/forgot-password', '/reset-password', '/logout']
 
 const LayoutAuth = ({ children }: LayoutProps) => {
     return (
