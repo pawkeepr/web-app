fcf37c622699033fd991bc1e6879fb62504f0efe
Douglas Sales <douglas.sales0611@gmail.com>
2023-05-27 03:56:46 -0300

    ♻️ refactor: corrije problema de layout da página no mobile

--- source/Components/Common/LightDark.tsx
+++ source/Components/Common/LightDark.tsx
@@ -1,19 +1,18 @@
-
 //constants
 
 import useChangeLayoutMode from "~/hooks/use-change-layout-mode";
 
 const LightDark = () => {
-
-    const { onHandleChangeLayout } = useChangeLayoutMode()
+    const { onHandleChangeLayout } = useChangeLayoutMode();
 
     return (
         <div className="ms-1 header-item d-none d-sm-flex">
             <button
                 onClick={onHandleChangeLayout}
                 type="button"
-                className="btn btn-icon btn-topbar btn-ghost-secondary rounded-circle light-dark-mode text-white">
-                <i className='bx bx-moon fs-22'></i>
+                className="btn btn-icon btn-topbar btn-ghost-secondary rounded-circle light-dark-mode text-white"
+            >
+                <i className="bx bx-moon fs-22"></i>
             </button>
         </div>
     );
