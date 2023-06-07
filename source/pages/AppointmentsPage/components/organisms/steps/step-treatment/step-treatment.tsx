2a1e2a2eaa77b18d977762a3a4a60cc70dc7d796
Keven Teles <glevenkeven@gmail.com>
2023-06-07 00:19:22 -0300

    ✨ feat: percorrendo lista de exames e vacinas e layout mobile estruturado

--- source/pages/AppointmentsPage/components/organisms/steps/step-treatment/step-treatment.tsx
+++ source/pages/AppointmentsPage/components/organisms/steps/step-treatment/step-treatment.tsx
@@ -1,10 +1,10 @@
-'use client'
-
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import { Formik , Form , Field , ErrorMessage } from 'formik' ;       
 import {useState } from 'react';
 import ControlSwitch from '../../../molecules/switch/switch';
+
+
 //Import images
 
 import { StepProps } from '../types';
@@ -17,10 +17,40 @@ import AvatarPet from '../../../atoms/pet-avatar/pet-avatar';
 
 const StepTreatment = ({ toggleTab, activeTab }: StepProps) => {
 
+
     const { values } = useFormikContext<InitialValues>()
-    const [enabled, setEnabled] = useState<Boolean>(false)
+ 
 
-
+    const vacinas = [
+        {
+          id: 1,
+          name:'Vermefugo'
+        },
+        {
+          id: 2,
+          name:'Febre'
+        },
+        {
+          id: 3,
+          name:'Raiva'
+        },
+      ]
+    
+      const exames = [
+        {
+          id: 1,
+          name:'Raio X'
+        },
+        {
+          id: 2,
+          name:'Exame de Sangue'
+        },
+        {
+          id: 3,
+          name:'Check-up'
+        },
+      ]
+    
 
     return (
         <>
@@ -29,14 +59,79 @@ const StepTreatment = ({ toggleTab, activeTab }: StepProps) => {
              initialValues={{ marca: '', inicio:''}}>
                 <form>
                     <div>
-                        <h5 className='text-center'>Tratamento</h5>
+                        <h4 className='text-center'>Tratamento</h4>
                     </div>
 
                     <div>
                         <div className='mt-2'>
-                            <span>Aplicar medicação?</span>
-                            <ControlSwitch/>
-                            <div className='grid grid-cols-2'>
+                            <div className='flex justify-between'>
+                                <span className='mt-2'>Aplicar medicação?</span>
+                                <ControlSwitch className='mt-2 w-16 h-7'/>
+                            </div>
+                            
+                            {/* <div className='flex gap-2 flex-col'>
+                                <span>Uso Contínuo?</span>
+                                <ControlSwitch/>
+                            </div> */}
+                            <div className='grid grid-cols-2 gap-2'>
+                                <div className='flex flex-col'>
+                                    <label>Marca</label>
+                                    <Field className='text-black rounded-md' name='marca' type='text'/>
+                                </div>
+                                <div className='flex flex-col'>
+                                    <label>Início</label>
+                                    <Field className='rounded-md' name='inicio' type='text'/>
+                                </div>
+                                <div className='flex flex-col'>
+                                    <label>Marca</label>
+                                    <Field className='text-black rounded-md' name='marca' type='text'/>
+                                </div>
+                                <div className='flex flex-col'>
+                                    <label>Início</label>
+                                    <Field className='rounded-md' name='inicio' type='text'/>
+                                </div>
+                                <div className='flex flex-col'>
+                                    <label>Marca</label>
+                                    <Field className='text-black rounded-md' name='marca' type='text'/>
+                                </div>
+                                <div className='flex flex-col'>
+                                    <label>Início</label>
+                                    <Field className='rounded-md' name='inicio' type='text'/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='mt-2'>
+                        <div className='flex justify-between'>
+                                <span className='mt-2'>Aplicar vacina?</span>
+                                <ControlSwitch className='mt-2 w-16 h-7'/>
+                            </div>
+                            <div className='mt-2'>
+                                {vacinas.map((vacina)=>(
+                                    <div>
+                                        <p>{vacina.name}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                        <div className='mt-2'>
+                        <div className='flex justify-between'>
+                                <span className='mt-2'>Aplicar exame?</span>
+                                <ControlSwitch className='mt-2 w-16 h-7'/>
+                            </div>
+                            <div className='mt-2 flex flex-col'>
+                                {exames.map((exame)=>(
+                                    <div className=''>
+                                        <p>{exame.name}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                        <div>
+                            <div className='flex justify-between'>
+                                <span className='mt-2'>Possui doença?</span>
+                                <ControlSwitch className='mt-2 w-16 h-7'/>
+                            </div>
+                            <div className='grid grid-cols-2 gap-2'>
                                 <div className='flex flex-col'>
                                     <label>Marca</label>
                                     <Field className='text-black rounded-md' name='marca' type='text'/>
@@ -48,26 +143,31 @@ const StepTreatment = ({ toggleTab, activeTab }: StepProps) => {
                             </div>
                         </div>
                         <div>
-                            <span>Aplicar vacina?</span>
-                            <ControlSwitch/>
+                            <div className='flex justify-between'>
+                                <span className='mt-2'>Aplicar nutrição alimentar?</span>
+                                <ControlSwitch className='mt-2 w-16 h-7'/>
+                            </div>
+                            <div className='grid grid-cols-2 gap-2'>
+                                <div className='flex flex-col'>
+                                    <label>Marca</label>
+                                    <Field className='text-black rounded-md' name='marca' type='text'/>
+                                </div>
+                                <div className='flex flex-col'>
+                                    <label>Início</label>
+                                    <Field className='rounded-md' name='inicio' type='text'/>
+                                </div>
+                            </div>
                         </div>
-                        <div>
-                            <span>Aplicar exame?</span>
-                            <ControlSwitch/>
-                        </div>
-                        <div>
-                            <span>Possui doença?</span>
-                            <ControlSwitch/>
-                        </div>
-                        <div>
-                            <span>Aplicar Nutrição Alimentar?</span>
-                            <ControlSwitch/>
+
+                        <div className='mt-4'>
+                            <span>Informações Obrigatórias</span>
+                                <div className='flex flex-col mt-2'>
+                                    <label>Peso</label>
+                                    <Field className='rounded-md' name='inicio' type='text'/>
+                                </div>
                         </div>
                     </div>
 
-                    <div>
-                        <button type='submit'>Cancelar Consulta</button>
-                    </div>
                 </form>
 
             </Formik>
