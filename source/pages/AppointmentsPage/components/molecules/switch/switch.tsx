cbced53ceeedd9820b3b697ede2c4a3bfe294464
Keven Teles <glevenkeven@gmail.com>
2023-06-07 00:18:11 -0300

    ✨ feat: componente switch recebendo altura por props, de acordo com a div pai

--- source/pages/AppointmentsPage/components/molecules/switch/switch.tsx
+++ source/pages/AppointmentsPage/components/molecules/switch/switch.tsx
@@ -1,23 +1,24 @@
-'use client'
+
 
 import { useState } from 'react'
 import { Switch } from '@headlessui/react'
 
+type SwitchProps ={
+  className: string
+}
 
+const ControlSwitch = ({className}: SwitchProps)=>{
 
-const ControlSwitch = ()=>{
-
-
-  const [enabled, setEnabled] = useState<Boolean>(false)
+  const [enabled, setEnabled] = useState<boolean>(false)
 
   return(
     <>
-    <div>
+    <div className={className}>
       <Switch
         checked={enabled}
         onChange={setEnabled}
-        className={`${enabled ? 'bg-teal-900' : 'bg-teal-700'}
-          relative inline-flex h-[28px] w-[64px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+        className={`${enabled ? 'bg-teal-900' : 'bg-teal-600'}
+          relative inline-flex h-full w-full shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
       >
         <span className="sr-only">Use setting</span>
         <span
