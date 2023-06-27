86e7fa1813b0e5977528fde4bf189a1a350f46b1
Keven Teles <glevenkeven@gmail.com>
2023-06-27 01:29:15 -0300

    💎 style: alterações no background dos componentes

--- source/pages/AppointmentsPage/components/molecules/switch/switch.tsx
+++ source/pages/AppointmentsPage/components/molecules/switch/switch.tsx
@@ -23,7 +23,7 @@ const ControlSwitch = ({ className, children, label, onClick }: SwitchProps) => 
             onClick={onClick}
             checked={enabled}
             onChange={setEnabled}
-            className={`${enabled ? 'bg-teal-900' : 'bg-teal-600'}
+            className={`${enabled ? 'bg-primary-700' : 'bg-primary-600'}
             relative inline-flex h-full w-full shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
           >
             <span className="sr-only">Use setting</span>
