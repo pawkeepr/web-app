1eb90c6cb3d26f9ee8542345d4c300c1a84c088f
Murilo Montino <51464383+murilomontino@users.noreply.github.com>
2023-07-23 00:49:59 +0000

    ♻️ refactor(dashboard): remoção de css bootstrap e refatorção de layout

--- source/pages/_layouts/dashboard/dashboard.tsx
+++ source/pages/_layouts/dashboard/dashboard.tsx
@@ -2,19 +2,11 @@
 
 import React, { useMemo } from "react";
 
-import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import Nav from "react-bootstrap/Nav";
 import NavItem from "react-bootstrap/NavItem";
-import NavLink from "react-bootstrap/NavLink";
-import Row from "react-bootstrap/Row";
 
 import { useRouter } from "next/navigation";
 
-//import images
-
 import HeaderTitle from "~/Components/atoms/header-title";
-import MyImage from "~/Components/atoms/my-image/my-image";
 import ModalConfirm from "~/Components/modals/modal-confirm/modal-confirm";
 import { useAppSelector } from "~/store/hooks";
 
@@ -79,99 +71,10 @@ const DashboardLayouts = ({
     }, [profile]);
 
     return (
-        <div className="page-content">
+        <div className="px-24 mobile:px-4 py-12 mt-8">
             <HeaderTitle title={title} />
-            <div className="container container-fluid">
-                <React.Fragment>
-                    <Row>
-                        <Col lg={12}>
-                            <Card className="mt-1 mx-1 mobile:hidden">
-                                <Card className="p-4 pb-0">
-                                    <Row className="items-center gap-4">
-                                        {/* <div className="col-md-auto flex justify-center align-middle self-center pb-2">
-                                            <div className="avatar-md w-32 h-32">
-                                                <div className="avatar-title bg-white rounded-circle relative mt-2">
-                                                    <MyImage
-                                                        src={
-                                                            profile?.avatar ||
-                                                            ""
-                                                        }
-                                                        alt={`Avatar de ${name}`}
-                                                        className="avatar-xs rounded-full"
-                                                        fill
-                                                    />
-                                                </div>
-                                            </div>
-                                        </div> */}
-                                        <div className="col-md">
-                                            <h4 className="text-base font-bold mobile:text-center">
-                                                Seja bem vindo, {name} !
-                                            </h4>
-                                            {/* <div className="hstack gap-3 flex-wrap mobile:flex mobile:items-center mobile:justify-center">
-                                                <div>
-                                                    <i className="ri-building-line align-bottom me-1"></i>
-                                                    {profile?.company}
-                                                </div>
-                                                <div className="vr mobile:hidden"></div>
-                                                <div>
-                                                    Criado :{" "}
-                                                    <span className="fw-medium">
-                                                        {profile?.created_at}
-                                                    </span>
-                                                </div>
-                                            </div> */}
-                                        </div>
-                                    </Row>
-                                </Card>
-                            </Card>
-                        </Col>
-                        <Col lg={12}>
-                            <Card className="mx-1 block mobile:flex mobile:items-center mobile:justify-center">
-                                <Nav
-                                    className="nav-tabs-custom border-bottom-0"
-                                    role="tablist"
-                                >
-                                    {items.map(
-                                        (
-                                            {
-                                                label,
-                                                onClick,
-                                                Element,
-                                                ...rest
-                                            },
-                                            index
-                                        ) => (
-                                            <NavItem key={index}>
-                                                {Element && (
-                                                    <NavLink
-                                                        className="fw-bold"
-                                                        {...rest}
-                                                    >
-                                                        {Element(onClick)}
-                                                    </NavLink>
-                                                )}
-
-                                                {!Element && (
-                                                    <NavLink
-                                                        className="fw-bold"
-                                                        onClick={onClick}
-                                                        {...rest}
-                                                    >
-                                                        {label}
-                                                    </NavLink>
-                                                )}
-                                            </NavItem>
-                                        )
-                                    )}
-                                </Nav>
-                            </Card>
-                        </Col>
-                    </Row>
-
-                    <Row>
-                        <Col lg={12}>{children}</Col>
-                    </Row>
-                </React.Fragment>
+            <div className="">
+                {children}
             </div>
         </div>
     );
