a86873a58cbc707affd9a242f0b10685871b888c
Jainefranciellem <jainefraciellen@gmail.com>
2023-08-03 15:12:32 -0300

    ✨ feat(btn-label): deixa botão branco com a borda dourada para seguir padrão de design da empresa (122)
    122

--- source/Components/atoms/btn/btn-label.tsx
+++ source/Components/atoms/btn/btn-label.tsx
@@ -6,7 +6,7 @@ const BtnLabel = ({ onClick, label = 'Sucesso', className, children, disabled, .
 
 	return (
 		<button
-			className={`btn-label !max-w-fit-content ${className}`}
+			className={`btn-label border-x border-y mobile:p-4 md:p-2 rounded-md pl-2 border-secondary-600 !max-w-fit-content ${className}`}
 			type="button"
 			onClick={onClick}
 			disabled={disabled}
