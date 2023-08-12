262bb5eeb4bee4f4c5a425b7bcbe6d76d1b62ad5
murilomontino <murilomontinojr@hotmail.com>
2023-08-12 18:37:06 -0300

    🐛 fix: correção de endpoints user tutor e user vet

--- source/services/helpers/urls.ts
+++ source/services/helpers/urls.ts
@@ -1,29 +1,29 @@
 // Appointment Vet
 export const APPOINTMENT_GET_ALL = () => '/api-appointment/fetch-all-appointment-vet'
-export const APPOINTMENT_UPDATE = () => `/api-appointment/update-appointment`
+export const APPOINTMENT_UPDATE = () => '/api-appointment/update-appointment'
 export const APPOINTMENT_CREATE = () => '/api-appointment/create-appointment'
-export const APPOINTMENT_GET_BY_ID = () => `/api-appointment/search-appointment`
+export const APPOINTMENT_GET_BY_ID = () => '/api-appointment/search-appointment'
 
 // Schedule Vet
 export const SCHEDULED_CREATE = () => '/api-appointment/create-schedule'
 
 // Profile Vet
 
-export const VET_CREATE_PROFILE = () => '/create-user-vet'
-export const VET_GET_PROFILE = () => `/search-user-vet`
-export const VET_UPDATE_PROFILE = () => `/update-user-vet`
+export const VET_CREATE_PROFILE = () => '/api-user/create-user-vet'
+export const VET_GET_PROFILE = () => '/api-user/search-user-vet'
+export const VET_UPDATE_PROFILE = () => '/api-user/update-user-vet'
 
 // Profile Tutor
 
-export const TUTOR_CREATE_PROFILE = () => '/create-user-tutor'
-export const TUTOR_GET_PROFILE = () => `/search-user-tutor`
-export const TUTOR_UPDATE_PROFILE = () => `/update-user-tutor`
+export const TUTOR_CREATE_PROFILE = () => '/api-user-tutor/create-user-tutor'
+export const TUTOR_GET_PROFILE = () => '/api-user-tutor/search-user-tutor'
+export const TUTOR_UPDATE_PROFILE = () => '/api-user-tutor/update-user-tutor'
 
 // Pet
 
-export const PET_FETCH_ALL = () => `/api-pet/fetch-all-pets-tutor`
+export const PET_FETCH_ALL = () => '/api-pet/fetch-all-pets-tutor'
 export const PET_CREATE_PROFILE = () => '/api-pet/create-pet'
-export const PET_GET_PROFILE = () => `/api-pet/search-pet`
+export const PET_GET_PROFILE = () => '/api-pet/search-pet'
 export const PET_UPDATE_PROFILE = () => '/api-pet/update-pet'
 export const PET_UPDATE_HEALTH = () => '/api-pet/update-pet-health'
 
