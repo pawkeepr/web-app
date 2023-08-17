cdfe230a88b4218f2ce23bed0573012bf4f0773e
natalia Farias Santos <102639784+fariasnatalia@users.noreply.github.com>
2023-08-17 11:03:20 -0300

    Feat:[Step-Pet] adiciona sub-step segundo tutor

--- source/pages/AppointmentsPage/components/organisms/steps/step-pet.tsx
+++ source/pages/AppointmentsPage/components/organisms/steps/step-pet.tsx
@@ -8,6 +8,7 @@ import FieldDocument from "~/Components/molecules/field-document/field-document"
 import { useFormikContext } from "formik";
 import { BtnAvatar, BtnSuccess } from "~/Components/atoms/btn";
 import { StepProps } from "./types";
+import ControlSwitch from "../../molecules/switch/switch";
 
 import { InitialValues } from "../../../Appointments";
 
@@ -69,16 +70,57 @@ const StepPet = ({ toggleTab, activeTab }: StepProps) => {
     return (
         <>
             <div className="p-1 m-2 mb-4">
-                <h5>Pet</h5>
-                <p className="text-muted">Todas as Informações sobre o PET</p>
+                <h5 className="font-bold text-center">Pet</h5>
             </div>
-
+            <div className="text-align: left mb-4">Preencha as Informações do PET</div>
             <div>
                 <Row className="g-3">
                     {/* <div className="flex flex-row gap-2 items-center justify-center m-2 p-1">
                         <AvatarPet name={values.pet?.name || ''} />
                         <BtnAvatar alt='Avatar de Tutor' name="tutor.avatar" disabled size={24} />
                     </div> */}
+                      <Row className="mt-2">
+                        <div className="w-full lg:w-1/3 px-3 mb-6">
+                            <ListBoxTailwind
+                                items={species}
+                                option={specie}
+                                value={values.pet?.species}
+                                onChangeOption={onChangeSpecie}
+                                required
+                                disabled={isPending || !!values.pet?.id}
+                                name="species"
+                                placeholder="Ex: Cachorro, Gato, etc..."
+                                label="Espécie"
+                            />
+                        </div>
+
+                        <div className="w-full lg:w-1/3 px-3 mb-6">
+                            <ListBoxTailwind
+                                items={specie.breedType as any}
+                                value={values.pet?.breed}
+                                disabled={!!values.pet?.id}
+                                required
+                                name="breed"
+                                label="Raça"
+                                placeholder="Ex: Vira-lata, Poodle, etc..."
+                            />
+                        </div>
+
+                        <div className="w-full lg:w-1/3 px-3 mb-6">
+                            <ListBoxTailwind
+                                items={specie.bloodType as any}
+                                value={values.pet?.bloodType}
+                                disabled={!!values.pet?.id}
+                                name="bloodType"
+                                label="Tipo Sanguíneo"
+                                placeholder="Ex: A, B, etc..."
+                            />
+                        </div>
+                    </Row>
+                    <div className="p-1 m-2 mb-4">
+                        <h5 className="font-bold text-center">Tutor</h5>
+                    </div>
+                    <div className="text-align: left  mb-2">Preencha as Informações do Tutor</div>
                     <Col sm={3}>
                         <FieldDocument
                             label="CPF"
@@ -127,52 +169,73 @@ const StepPet = ({ toggleTab, activeTab }: StepProps) => {
                             required
                         />
                     </Col>
-
-                    <Row className="mt-2">
-                        <div className="w-full lg:w-1/3 px-3 mb-6">
-                            <ListBoxTailwind
-                                items={species}
-                                option={specie}
-                                value={values.pet?.species}
-                                onChangeOption={onChangeSpecie}
-                                required
-                                disabled={isPending || !!values.pet?.id}
-                                name="species"
-                                placeholder="Ex: Cachorro, Gato, etc..."
-                                label="Espécie"
-                            />
-                        </div>
-
-                        <div className="w-full lg:w-1/3 px-3 mb-6">
-                            <ListBoxTailwind
-                                items={specie.breedType as any}
-                                value={values.pet?.breed}
-                                disabled={!!values.pet?.id}
-                                required
-                                name="breed"
-                                label="Raça"
-                                placeholder="Ex: Vira-lata, Poodle, etc..."
-                            />
-                        </div>
-
-                        <div className="w-full lg:w-1/3 px-3 mb-6">
-                            <ListBoxTailwind
-                                items={specie.bloodType as any}
-                                value={values.pet?.bloodType}
-                                disabled={!!values.pet?.id}
-                                name="bloodType"
-                                label="Tipo Sanguíneo"
-                                placeholder="Ex: A, B, etc..."
-                            />
-                        </div>
-                    </Row>
                 </Row>
-
                 <Row className="g-3">
                     <StepTutor disabled={tutorExists} />
                 </Row>
+                <Row className="mt-2">
+                <ControlSwitch
+                    label="O pet possui um segundo Tutor?"
+                    className="mt-2 mb-4 lg:w-16 lg:h-7 w-[3.72rem] h-6"
+                    >
+                    <div className="text-align: left mb-2">Preencha as Informações do segundo Tutor</div>     
+                    <Col sm={3}>
+                        <FieldDocument
+                            label="CPF"
+                            divClassName="my-1"
+                            name="tutor.document"
+                            aria-label="document"
+                            className="form-control"
+                            onlyCPF
+                            disabled={isPending || tutorExists}
+                            placeholder="CPF"
+                            component={MaskedInput as any}
+                            required
+                        />
+                    </Col>
+                    <Col sm={5}>
+                        <FieldControl
+                            initialFocus
+                            divClassName="my-1"
+                            label="Nome Completo"
+                            name="tutor.name"
+                            disabled={isPending || tutorExists}
+                            aria-label="name"
+                            className="form-control"
+                            placeholder="Digite o nome do Tutor"
+                            required
+                            disabledError
+                            onChange={onlyWords}
+                        />
+                    </Col>
+                    <Col sm={4}>
+                        <FieldControl
+                            className="form-control"
+                            divClassName="my-1"
+                            type="text"
+                            label="Telefone/Celular"
+                            name="tutor.phone"
+                            disabled={isPending || tutorExists}
+                            placeholder={
+                                isPending
+                                    ? "Carregando..."
+                                    : "Digite o seu Número de Telefone"
+                            }
+                            component={MaskedInput as any}
+                            mask={"(99) 99999-9999"}
+                            maskChar={null}
+                            required
+                        />
+                    </Col>
+                    <Row className="g-3">
+                    <StepTutor disabled={tutorExists} />
+                    </Row>
+                    </ControlSwitch>
+                    </Row>
+                
+                
+                
             </div>
-
             <div className="flex align-items-center justify-end gap-3 mt-4">
                 <BtnSuccess
                     type="button"
