fb3f8c4e628fede000e1923988b8c17c8ab8596a
murilomontino <murilomontinojr@hotmail.com>
2023-08-22 11:27:41 -0300

    ♻️ refactor: adição de twMerge para label costumizaveis

--- source/Components/atoms/label/label.tsx
+++ source/Components/atoms/label/label.tsx
@@ -1,5 +1,7 @@
 import { ClassAttributes, LabelHTMLAttributes } from "react"
+import { twMerge } from "tailwind-merge"
 import withControl from "~/Components/helpers/with-control"
+
 
 type LabelProps = {
     id?: string
@@ -11,14 +13,20 @@ type LabelProps = {
     ClassAttributes<HTMLLabelElement> &
     LabelHTMLAttributes<HTMLLabelElement>
 
-const Label = ({ id, label, required, separator, ...props }: LabelProps) => {
+const Label = ({
+    id,
+    label,
+    required,
+    separator = '',
+    className,
+    ...props }: LabelProps) => {
     if (!label) return null;
 
     return (
         <label
             {...props}
             htmlFor={id || props.name}
-            className="mb-0 text-xs font-semibold text-gray-500 gap-1"
+            className={twMerge("text-xs font-semibold text-gray-500 gap-1", className)}
             data-testid={`label-${id}`}
         >
             {label.trim() ? (label + separator) : ''}
