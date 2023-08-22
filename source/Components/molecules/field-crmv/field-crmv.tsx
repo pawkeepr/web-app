e5f309923e9d039951f70fbd3da434ea7e57cd8f
murilomontino <murilomontinojr@hotmail.com>
2023-08-22 10:41:48 -0300

    ✨ feat: novo componente para documentos crmv

---
+++ source/Components/molecules/field-crmv/field-crmv.tsx
@@ -0,0 +1,28 @@
+
+
+import { InputControlProps } from '~/Components/molecules/field-control';
+
+import FieldMasked from '../field-masked';
+
+type FieldDocumentProps<T> = Omit<InputControlProps<T>, 'ref'>
+
+const FieldCRMV = <T,>({ ...props }: FieldDocumentProps<T>) => {
+
+
+
+    return (
+        <FieldMasked
+            {...props}
+            name={props.name}
+            showMask={true}
+            mask={'--______'}
+            className='uppercase'
+            replacement={{
+                _: /\d/,
+                '-': /[A-Za-z]/ // letras maiusculas
+            }}
+        />
+    );
+};
+
+export default FieldCRMV;
