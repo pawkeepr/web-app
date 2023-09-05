816e1fe5c7a270b8ef68da04a180fb8b538f3d61
murilomontino <murilomontinojr@hotmail.com>
2023-09-05 15:27:36 -0300

    🐛 fix: correção de colors na tela de logout

--- source/pages/Authentication/LogoutPage.tsx
+++ source/pages/Authentication/LogoutPage.tsx
@@ -1,16 +1,16 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 'use client';
 
-import { useEffect } from 'react';
 
 
 
-import { resetLoading, signOutUser } from '~/store/auth/login/actions';
 import { useAppDispatch, useAppSelector } from '~/store/hooks';
 
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import { BtnLink } from '~/Components/atoms/btn';
+import { resetLoading, signOutUser } from '~/store/auth/login/actions';
 import AuthLayout from '../_layouts/auth/auth_layout';
-
 
 const LogoutPage = () => {
     const dispatch = useAppDispatch();
@@ -29,17 +29,18 @@ const LogoutPage = () => {
                 router.push('/sign-in')
             }, 1000);
         }
-    }, [dispatch, isAuthenticated, router]);
+
+    }, [isAuthenticated]);
 
     return (
         <AuthLayout title='Sair'>
 
             <div className='flex flex-col items-center justify-center'>
                 {/* icon de um emoji chorando */}
-                <div className="mt-4 pt-2">
+                <div className="mt-4 pt-2 text-center">
                     <h4>Você acabou de sair 😭</h4>
-                    <p className="text-muted">Obrigado por usar <span className="fw-bold">PawKeeprs</span>, não esqueça de voltar</p>
-                    <BtnLink href='sign-in' className="!w-full !bg-primary-500" message='Entrar' />
+                    <p className="text-gray-400">Obrigado por usar a <span className="font-semibold text-gray-500">PawKeeprs</span>, não se esqueça de voltar</p>
+                    <BtnLink href='sign-in' className="!w-full !bg-primary-500 !text-white" message='Entrar' />
                 </div>
             </div>
 
