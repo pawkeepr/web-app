2406bdc537a64196f260416dc86980e3419c2831
Jainefranciellem <jainefraciellen@gmail.com>
2023-10-11 15:24:50 -0300

    ♻️ refactor: deixa inputs obrigatórios ao selecionar o toggle - TSK 202

--- source/pages/AppointmentsPage/components/molecules/health-insurance/health-insurance.tsx
+++ source/pages/AppointmentsPage/components/molecules/health-insurance/health-insurance.tsx
@@ -13,28 +13,31 @@ export default function HealthInsurance() {
                 label="O pet possui plano de saúde?"
                 className="mt-2 mb-4 lg:w-16 lg:h-7 w-[3.72rem] h-6"
             >
-            <div className="text-align: left mb-2">Preencha as Informações do plano de saúde do Pet</div>
-                <div className="text-secondary-500 mt-2">Os campos abaixo são opcionais!</div>
-                
+            <div className="flex justify-center mb-2">Preencha as Informações do plano de saúde do Pet</div>
+                <span className="flex justify-center items-center text-secondary-500 font-semibold">Obrigatório(*)</span>
                 <div className="grid grid-cols-2 mobile:grid-cols-1 gap-3 m-2 p-2">
                     <FieldControl
-                        label="Nome do plano"a
+                        label="Nome do plano"
+                        required
                         name="health_insurance"
                         placeholder="Digite o nome do plano"
                     />
                     <FieldControl
                         label="Tipo do plano"
+                        required
                         name="palno_type"
                         placeholder="Digite o tipo do plano"
                     />
                     <FieldControl
                         label="Número da carteirinha"
+                        required
                         name="card_number"
                         placeholder="Digite o número da carteirinha"
 
                     />
                     <FieldControl
                         label="Validade da carteirinha"
+                        required
                         name="wallet_validity"
                         placeholder="Digite a data de validade da carteirinha"
                         type="date"
