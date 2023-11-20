17f8c4f99b83232b90c1834531574250e9a7c8ed
Jainefranciellem <jainefraciellen@gmail.com>
2023-11-20 09:02:08 -0300

    ♻️ refactor: refatora cards

--- source/pages/DashboardPage/components/organisms/card-scheduled.tsx
+++ source/pages/DashboardPage/components/organisms/card-scheduled.tsx
@@ -1,4 +1,5 @@
 import { RadioGroup } from "@headlessui/react";
+import { useState } from "react";
 import MyImage from "~/Components/atoms/my-image";
 import ViewAppointment from "~/Components/modals/view-appointment/modal-view-appointment";
 import ravena from "~/assets/images/ravena.jpeg";
@@ -11,87 +12,115 @@ type CardScheduledProps = {
 
 const CardScheduled = ({ checked, appointment }: CardScheduledProps) => {
     return (
-        <div className="flex flex-col w-full">
-            <div className="flex mobile:gap-3 justify-around items-center">
-                <MyImage
-                    src={ravena}
-                    alt="Picture of the author"
-                    width={150}
-                    height={150}
-                    className="h-32 mt-3 w-32 rounded-full"
-                />
-
-                <div className="flex flex-col items-center">
-                    <RadioGroup.Label
-                        as="p"
-                        className={`font-medium  ${checked ? 'text-white' : 'text-gray-900'
-                            }`}
-                    >
-                        {'Informações Do Agendamento:'}
-                    </RadioGroup.Label>
-                    <div className="">
-                        <RadioGroup.Description
-                            as="span"
-                            className={`inline ${checked ? 'text-sky-100' : 'text-gray-500'
+            <div className="space-y-10 w-full">
+                <RadioGroup.Option
+                  key={appointment?.name_tutor}
+                  value={appointment}
+                  className={({ active }) =>
+                    `${
+                      active
+                        ? 'ring-2 ring-white/20 ring-offset-2'
+                        : ''
+                    }
+                    ${checked ? 'bg-primary-500 bg-opacity-60 text-white' : 'bg-white'}
+                      relative flex cursor-pointer rounded-lg px-2 py-2 shadow-md focus:outline-none`
+                  }
+                >
+                  {({ active }) => (
+                    <>
+                      <div className="flex flex-col w-full">
+                        <div className="flex mobile:gap-3 justify-around items-center">
+                            <MyImage
+                                src={ravena}
+                                alt="Picture of the author"
+                                width={150}
+                                height={150}
+                                className="h-32 mt-3 w-32 rounded-full"
+                            />
+    
+                          <div className="flex flex-col items-center">
+                            <RadioGroup.Label
+                              as="p"
+                              className={`font-medium  ${
+                                checked ? 'text-white' : 'text-gray-900'
+                              }`}
+                            >
+                              {'Informações Do Agendamento:'}
+                            </RadioGroup.Label>
+                            <div className="">
+                                <RadioGroup.Description
+                                as="span"
+                                className={`inline ${
+                                    checked ? 'text-sky-100' : 'text-gray-500'
                                 }`}
-                        >
-                            <div className="p-2">
-                                <p className="text-gray-700 md:hidden">Nome do Tutor: {appointment.tutor_data.name}</p>
-
-                                <p className="text-gray-700 md:hidden">Contato:{appointment.tutor_data.phone}</p>
+                                >
+                               <div className="p-2">
+                                    <p className="text-gray-700 md:hidden">Nome do pet: { appointment?.pet_data.name_pet }</p>
+                                    <p className="text-gray-700">Data: {appointment?.dates_consults.date_consultation}</p>
+                                    <p className="text-gray-700">Horário: { appointment?.dates_consults.time_consultation}</p>
+                                    <p className="text-gray-700 md:hidden">Contato:{ appointment?.tutor_data.phone}</p>
+                                </div>
+                                </RadioGroup.Description>
                             </div>
-                        </RadioGroup.Description>
-                    </div>
-
-                </div>
-                <div className="flex flex-col mobile:hidden items-center">
-                    <RadioGroup.Label
-                        as="p"
-                        className={`font-medium  ${checked ? 'text-white' : 'text-gray-900'
-                            }`}
-                    >
-                        {'Informações Do Pet:'}
-                    </RadioGroup.Label>
-                    <RadioGroup.Description
-                        as="span"
-                        className={`inline ${checked ? 'text-sky-100' : 'text-gray-500'
-                            }`}
-                    >
-                        <div className="p-2 ">
-                            <p className="text-gray-700">Nome do pet: {appointment.pet_data.name_pet}</p>
-                            <p className="text-gray-700">Especie: {appointment.pet_data.specie}</p>
-                            <p className="text-gray-700">Sexo: {appointment.pet_data.sex}</p>
+                            
+                          </div>
+                          <div className="flex flex-col mobile:hidden items-center">
+                            <RadioGroup.Label
+                              as="p"
+                              className={`font-medium  ${
+                                checked ? 'text-white' : 'text-gray-900'
+                              }`}
+                            >
+                              {'Informações Do Pet:'}
+                            </RadioGroup.Label>
+                            <RadioGroup.Description
+                              as="span"
+                              className={`inline ${
+                                checked ? 'text-sky-100' : 'text-gray-500'
+                              }`}
+                            >   
+                              <div className="p-2 ">
+                                    <p className="text-gray-700">Nome do pet: {appointment?.pet_data.name_pet}</p>
+                                    <p className="text-gray-700">Especie: { appointment?.pet_data.specie}</p>
+                                    <p className="text-gray-700">Sexo: {appointment?.pet_data.sex}</p>
+                                    <p className="text-gray-700">Microchip: 1294</p>
+                                </div>
+                            </RadioGroup.Description>
+                          </div>
+                          <div className="flex mobile:hidden flex-col">
+                            <RadioGroup.Label
+                              as="p"
+                              className={`font-medium  ${
+                                checked ? 'text-white' : 'text-gray-900'
+                              }`}
+                            >
+                              {'Informações Do Tutor:'}
+                            </RadioGroup.Label>
+                            <RadioGroup.Description
+                              as="span"
+                              className={`inline ${
+                                checked ? 'text-sky-100' : 'text-gray-500'
+                              }`}
+                            >
+                              <div className="p-2">
+                                    <p className="text-gray-700">Nome: {appointment?.tutor_data.email}</p>
+                                    <p className="text-gray-700">Contato: {appointment?.tutor_data.phone}</p>
+                                </div>
+                            </RadioGroup.Description>
+                            
+                          </div>
                         </div>
-                    </RadioGroup.Description>
-                </div>
-                <div className="flex mobile:hidden flex-col">
-                    <RadioGroup.Label
-                        as="p"
-                        className={`font-medium  ${checked ? 'text-white' : 'text-gray-900'
-                            }`}
-                    >
-                        {'Informações Do Tutor:'}
-                    </RadioGroup.Label>
-                    <RadioGroup.Description
-                        as="span"
-                        className={`inline ${checked ? 'text-sky-100' : 'text-gray-500'
-                            }`}
-                    >
-                        <div className="p-2">
-                            <p className="text-gray-700">Email: {appointment.tutor_data.email}</p>
-                            <p className="text-gray-700">Contato: {appointment.tutor_data.phone}</p>
-                        </div>
-                    </RadioGroup.Description>
-
-                </div>
+                        {checked && (
+                          <div className="flex justify-end text-white">
+                            <ViewAppointment props={appointment} />
+                          </div>
+                        )}
+                      </div>
+                    </>
+                  )}
+                </RadioGroup.Option>
             </div>
-            {checked && (
-                <div className="flex justify-end text-white">
-                    <ViewAppointment props={appointment} />
-                </div>
-            )}
-        </div>
-    )
+    );
 }
 
 export default CardScheduled
