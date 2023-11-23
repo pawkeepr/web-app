e2c5ccc1d60b65b3910f3ca5db7af2db3943c735
murilomontino <murilomontinojr@hotmail.com>
2023-11-23 13:20:39 -0300

    refactor: card-pet component to display pet information

--- source/Components/modals/scheduled-appointment/components/card-pet/index.tsx
+++ source/Components/modals/scheduled-appointment/components/card-pet/index.tsx
@@ -1,3 +1,4 @@
+import { KeyOfSpecies, Species } from "~/store/slices/pets/speciesType"
 import { PetData } from "~/types/pet-v2"
 
 type CardPetProps = {
@@ -8,18 +9,14 @@ const CardPet = ({ pet }: CardPetProps) => {
 
     return (
         <section className="flex flex-col justify-center mb-2">
-            <div className="gap-2 flex-wrap flex flex-row w-full justify-between">
+            <div className="gap-2 flex-wrap flex flex-col w-full justify-between">
                 <p className="text-gray-500 flex justify-between">
-                    <strong className="mr-2">Nome do Pet:</strong>
-                    {pet.name_pet}
-                </p>
-                <p className="text-gray-500 flex justify-between">
-                    <strong className="mr-2">Espécie:</strong>
-                    {pet.specie as string}
-                </p>
-                <p className="text-gray-500 flex justify-between">
-                    <strong className="mr-2">Raça:</strong>
-                    {pet.race as string}
+                    <strong className="mr-2">Pet:</strong>
+                    <span>
+                        {
+                            `${pet.name_pet}, ${Species[(pet.specie as Species) as unknown as KeyOfSpecies]}, ${pet.race as string}`
+                        }
+                    </span>
                 </p>
             </div>
         </section>
