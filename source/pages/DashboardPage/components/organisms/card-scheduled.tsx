199ccc0427c98dd5e17007de7d078774a9eb65f0
Jainefranciellem <jainefraciellen@gmail.com>
2023-11-24 17:10:58 -0300

    ♻️ refactor: deixa só as informações que vão ser preenchidas no card do pet - TSK 274

--- source/pages/DashboardPage/components/organisms/card-scheduled.tsx
+++ source/pages/DashboardPage/components/organisms/card-scheduled.tsx
@@ -37,32 +37,6 @@ const CardScheduled = ({ checked, appointment }: CardScheduledProps) => {
                                 className="h-32 mt-3 w-32 rounded-full"
                             />
     
-                          <div className="flex flex-col items-center">
-                            <RadioGroup.Label
-                              as="p"
-                              className={`font-medium  ${
-                                checked ? 'text-white' : 'text-gray-900'
-                              }`}
-                            >
-                              {'Informações Do Agendamento:'}
-                            </RadioGroup.Label>
-                            <div className="">
-                                <RadioGroup.Description
-                                as="span"
-                                className={`inline ${
-                                    checked ? 'text-sky-100' : 'text-gray-500'
-                                }`}
-                                >
-                               <div className="p-2">
-                                    <p className="text-gray-700 md:hidden">Nome do pet: { appointment?.pet_data.name_pet }</p>
-                                    <p className="text-gray-700">Data: {appointment?.dates_consults?.date_consultation}</p>
-                                    <p className="text-gray-700">Horário: { appointment?.dates_consults?.time_consultation}</p>
-                                    <p className="text-gray-700 md:hidden">Contato:{ appointment?.tutor_data?.phone}</p>
-                                </div>
-                                </RadioGroup.Description>
-                            </div>
-                            
-                          </div>
                           <div className="flex flex-col mobile:hidden items-center">
                             <RadioGroup.Label
                               as="p"
@@ -82,7 +56,6 @@ const CardScheduled = ({ checked, appointment }: CardScheduledProps) => {
                                     <p className="text-gray-700">Nome do pet: {appointment?.pet_data?.name_pet}</p>
                                     <p className="text-gray-700">Especie: { appointment?.pet_data?.specie}</p>
                                     <p className="text-gray-700">Sexo: {appointment?.pet_data?.sex}</p>
-                                    <p className="text-gray-700">Microchip: {appointment.pet_data?.microchip}</p>
                                 </div>
                             </RadioGroup.Description>
                           </div>
@@ -103,7 +76,7 @@ const CardScheduled = ({ checked, appointment }: CardScheduledProps) => {
                             >
                               <div className="p-2">
                                     <p className="text-gray-700">Nome: {appointment?.tutor_data?.name}</p>
-                                    <p className="text-gray-700">Contato: {appointment?.contact_tutor?.phone}</p>
+                                    <p className="text-gray-700">Email: {appointment?.tutor_data.email}</p>
                                 </div>
                             </RadioGroup.Description>
                             
