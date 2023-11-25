644400ee3feaf8d626734f0e27d26273bb370ab1
murilomontino <murilomontinojr@hotmail.com>
2023-11-25 13:59:20 -0300

    refactor: box-buttons and card-scheduled components

--- source/pages/DashboardPage/components/organisms/organisms/box-buttons/box-buttons.tsx
+++ source/pages/DashboardPage/components/organisms/organisms/box-buttons/box-buttons.tsx
@@ -10,10 +10,24 @@ const BoxButtons = ({
     isLoading = false,
 }: BoxButtonsProps) => {
     return (
-        <div className="gap-2 justify-center flex w-full">
-            <BtnLabel condition={!isLoading} label={'Cancelar Consulta'} onClick={() => { }} className='text-gray-600' />
-            <BtnCancel condition={!isLoading} label={'Reagendar Consulta'} onClick={() => { }} className='text-white' />
-            <BtnPrimary label={'Iniciar Consulta'} onClick={() => { }} />
+        <div className="gap-1 justify-end flex w-full mobile:grid mobile:grid-cols-1 flex-wrap">
+            <BtnLabel
+                condition={!isLoading}
+                label='Cancelar Consulta'
+                onClick={() => { }}
+                className='text-red-500 border-none mobile:col-span-1'
+            />
+            <BtnCancel
+                condition={!isLoading}
+                label='Reagendar Consulta'
+                onClick={() => { }}
+                className='border-none mobile:!w-full mobile:col-span-1'
+            />
+            <BtnPrimary
+                label='Iniciar Consulta'
+                className='border-none mobile:!w-full mobile:col-span-1'
+                onClick={() => { }}
+            />
         </div>
     )
 }
--- source/pages/DashboardPage/components/organisms/steps/step-scheduled-all.tsx
+++ source/pages/DashboardPage/components/organisms/steps/step-scheduled-all.tsx
@@ -1,21 +1,27 @@
 import { RadioGroup } from "@headlessui/react";
-import { useState } from "react";
-import { useAppointmentScheduled } from "~/store/hooks/appointments";
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "~/store/hooks";
+import { getAll } from "~/store/slices/appointment-vet/actions";
 import CardScheduled from "../organisms/card-scheduled";
 
 
 const StepAll = () => {
+    const data = useAppSelector(state => state.AppointmentVet.all_scheduled);
+    const dispatch = useAppDispatch()
 
-    const { activeData } = useAppointmentScheduled()
+    useEffect(() => {
+        dispatch(getAll());
+    }, [dispatch]);
 
     const [selected, setSelected] = useState(null)
+
 
     return (
         <RadioGroup value={selected} onChange={setSelected}>
             <RadioGroup.Label className="sr-only ">Server size</RadioGroup.Label>
             <div className="space-y-10 w-full">
-                {activeData.map((appointment) => (
-                    <CardScheduled key={appointment.id} checked={selected === appointment} appointment={appointment} />
+                {data.map((appointment) => (
+                    <CardScheduled key={appointment.id} appointment={appointment} />
                 ))}
             </div>
         </RadioGroup>
