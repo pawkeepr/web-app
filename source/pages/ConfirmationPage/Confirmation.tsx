f99219fbc63c63f16e4a6aa219a15e475a9ec7ef
Jainefranciellem <jainefraciellen@gmail.com>
2023-11-27 18:43:49 -0300

    ✨ feat: cria botões da página de confirmação - TSK 285

--- source/pages/ConfirmationPage/Confirmation.tsx
+++ source/pages/ConfirmationPage/Confirmation.tsx
@@ -4,28 +4,17 @@ import DefaultLayout from "../_layouts/dashboard/dashboard";
 import { useAppDispatch } from "~/store/hooks";
 import { resetLoading } from "~/store/actions";
 import { useEffect } from "react";
+import { BtnPrimary } from "~/Components/atoms/btn";
 
 
 
 const ConfirmationPage = () => {
     return (
         <DefaultLayout title="Confirmation">
-            <div className="flex flex-col justify-center items-center gap-3 lg:mt-5">
-                <p className="text-sm font-bold text-secondary-500">
-                    Seja Bem-vindo(a)!
-                </p>
-            </div>
-            <div className="mobile:!mt-0 mobile:p-0">
-                <div className="flex justify-center item-center">
-                    <div
-                        className="spinner-border text-primary-500 w-40 h-40 my-4"
-                        role="status"
-                    >
-                        <span className="visually-hidden">
-                            Loading...
-                        </span>
-                    </div>
-                </div>
+            <div className="flex flex-col justify-center items-center gap-7 lg:mt-52">
+                <BtnPrimary label="Confirmar Consulta" onClick={() => {}} />
+                <BtnPrimary label="Cancelar Consulta" onClick={() => {}} />
+                <BtnPrimary label="Remarcar Consulta" onClick={() => {}} />
             </div>
         </DefaultLayout>
     );
