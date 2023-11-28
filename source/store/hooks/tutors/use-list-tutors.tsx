1fc6c674f6a7d4e69dfa953ffa73bf643165916b
murilomontino <murilomontinojr@hotmail.com>
2023-11-28 16:57:43 -0300

    feat: new hook to list tutors in clinic

---
+++ source/store/hooks/tutors/use-list-tutors.tsx
@@ -0,0 +1,19 @@
+import useAppQuery from '~/hooks/use-app-query'
+import { getAllTutorsOfClinic } from '~/services/helpers'
+import { IPetV2Data } from '~/types/pet-v2'
+
+export const NAME = 'list-tutors'
+const TIME = 1000 * 60 * 1 // 1 min
+
+const useListTutors = () => {
+    const superKeys = [NAME]
+
+    return useAppQuery<IPetV2Data[]>(superKeys, getAllTutorsOfClinic, {
+        initialData: [],
+        keepPreviousData: true,
+        cacheTime: TIME, // 1 min,
+        // staleTime: TIME // 1 min
+    })
+}
+
+export default useListTutors
