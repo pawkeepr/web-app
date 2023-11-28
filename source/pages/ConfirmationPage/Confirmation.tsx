27e82170e6c04d61e5e2a33cd0f4d8a4d01bc1a1
Jainefranciellem <jainefraciellen@gmail.com>
2023-11-28 08:34:01 -0300

    💎 style: muda o tamanho dos botões da page cofnirmation_scheduled

--- source/pages/ConfirmationPage/Confirmation.tsx
+++ source/pages/ConfirmationPage/Confirmation.tsx
@@ -12,9 +12,9 @@ const ConfirmationPage = () => {
     return (
         <DefaultLayout title="Confirmation">
             <div className="flex flex-col justify-center items-center gap-7 lg:mt-52">
-                <BtnPrimary label="Confirmar Consulta" onClick={() => {}} />
-                <BtnPrimary label="Cancelar Consulta" onClick={() => {}} />
-                <BtnPrimary label="Remarcar Consulta" onClick={() => {}} />
+                <BtnPrimary label="Confirmar Consulta" className="w-2/4" onClick={() => {}} />
+                <BtnPrimary label="Cancelar Consulta" className="w-2/4" onClick={() => {}} />
+                <BtnPrimary label="Remarcar Consulta" className="w-2/4" onClick={() => {}} />
             </div>
         </DefaultLayout>
     );
