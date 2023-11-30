09a4ef32fec5243078b945c560c432a0af67498c
murilomontino <murilomontinojr@hotmail.com>
2023-11-29 21:19:29 -0300

    refactor: card-pet component to display pet information and tutor name

--- source/pages/AppointmentsPage/components/molecules/card-pet/index.tsx
+++ source/pages/AppointmentsPage/components/molecules/card-pet/index.tsx
@@ -2,7 +2,6 @@ import { useFormikContext } from "formik"
 import { InitialValues } from "~/pages/AppointmentsPage/Appointments"
 import { Gender, KeyOfGender, KeyOfSpecies, Species } from "~/store/slices/pets/speciesType"
 import { calcAge } from "~/utils/calc-age"
-import PetAvatar from "../../atoms/pet-avatar"
 
 const CardPet = () => {
 
@@ -10,15 +9,14 @@ const CardPet = () => {
 
     return (
         <section className=" flex flex-col justify-start p-4">
-            <PetAvatar name={values.pet_data?.name_pet as string} />
             <div className="gap-2 flex-wrap flex mt-2">
-                <p className="text-gray-500">
-                    <strong className="mr-2">Espécie:</strong>
-                    {Species[values.pet_data?.specie as KeyOfSpecies]}
-                </p>
-                <p className="text-gray-500">
-                    <strong className="mr-2">Raça:</strong>
-                    {values.pet_data?.race}
+                <p className="text-gray-500 flex justify-start">
+                    <strong className="mr-2">Pet:</strong>
+                    <span>
+                        {
+                            `${values.pet_data?.name_pet}, ${Species[(values.pet_data?.specie as Species) as unknown as KeyOfSpecies]}, ${values.pet_data?.race as string}`
+                        }
+                    </span>
                 </p>
                 <p className="text-gray-500">
                     <strong className="mr-2">Idade:</strong>
@@ -27,6 +25,12 @@ const CardPet = () => {
                 <p className="text-gray-500">
                     <strong className="mr-2">Sexo:</strong>
                     {Gender[values.pet_data?.sex as KeyOfGender]}
+                </p>
+            </div>
+            <div className="gap-2 flex-wrap flex mt-2">
+                <p className="text-gray-500">
+                    <strong className="mr-2">Tutor:</strong>
+                    {values.name_tutor}
                 </p>
             </div>
         </section>
