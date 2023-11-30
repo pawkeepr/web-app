a78c6c21e8634da2f36d4a8c3ea13f9a34a3f727
murilomontino <murilomontinojr@hotmail.com>
2023-11-29 21:19:51 -0300

    refactor: step-treatment component and add physical activity field

--- source/pages/AppointmentsPage/components/organisms/steps/step-treatment/step-treatment.tsx
+++ source/pages/AppointmentsPage/components/organisms/steps/step-treatment/step-treatment.tsx
@@ -13,17 +13,23 @@ import Medicines from "../../../molecules/medicines";
 import Nutritions from "../../../molecules/nutritions";
 import Vaccines from "../../../molecules/vaccines";
 
+const physical_activity = ['Caminhadas', 'Corridas', 'Natação', 'Passeios', 'Cabo-de-guerra', 'Varetinhas', 'Bolinhas']
+
+const options_physical_activity = physical_activity.map((item) => ({
+    value: item,
+    label: item,
+    color: 'rgb(255 200 107);',
+}));
+
+const tests = ['Teste 1', 'Teste 2', 'Teste 3', 'Teste 4', 'Teste 5']
+
+const options = tests.map((item) => ({
+    value: item,
+    label: item,
+    color: 'rgb(255 200 107);',
+}));
+
 const StepTreatment = ({ toggleTab, activeTab }: StepProps) => {
-    const tests = ['Teste 1', 'Teste 2', 'Teste 3', 'Teste 4', 'Teste 5']
-
-    const options = tests.map((item) => ({
-        value: item,
-        label: item,
-        color: 'rgb(255 200 107);',
-    }));
-
-
-
     return (
         <section className="card card-body shadow-lg">
             <h4 className="text-center font-sans font-semibold text-base capitalize">
@@ -31,6 +37,19 @@ const StepTreatment = ({ toggleTab, activeTab }: StepProps) => {
                 <br />
                 <span className="text-xs font-bold text-secondary-500">Obrigatório (*)</span>
             </h4>
+
+            <ControlSwitchDiv
+                name="activities_carry"
+                label="O pet está praticando atividade física ?"
+            >
+                <FieldControlSelect
+                    label="Selecione uma ou mais opções:"
+                    placeholder="Selecione uma ou mais atividades"
+                    isMulti
+                    name="activities_carry"
+                    options={options_physical_activity}
+                />
+            </ControlSwitchDiv>
 
             <ControlSwitchDiv
                 name="fast_test"
