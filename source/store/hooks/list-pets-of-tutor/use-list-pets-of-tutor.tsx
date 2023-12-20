4cbc86d7977397d83323b0fd8aa5c8efa9e72112
murilomontino <murilomontinojr@hotmail.com>
2023-12-20 14:59:42 -0300

    refactor: add Pet entity and IPet import in use-list-pets-of-tutor.tsx

--- source/store/hooks/list-pets-of-tutor/use-list-pets-of-tutor.tsx
+++ source/store/hooks/list-pets-of-tutor/use-list-pets-of-tutor.tsx
@@ -1,4 +1,6 @@
+import { Pet } from '~/entities/Pet'
 import { createPet, getAllPets } from '~/services/helpers'
+import { IPet } from '~/types/pet'
 import { IPetV2 } from '~/types/pet-v2'
 import useAppStore from '../use-app-store'
 
@@ -9,9 +11,10 @@ const usePetsByTutor = (
 ) => {
     const superKeys = [NAME, document]
 
-    return useAppStore<IPetV2[]>({
+    return useAppStore<IPetV2, IPet>({
         get: getAllPets.bind(null, document),
         add: createPet,
+        entity: Pet,
         keys: superKeys,
         name: NAME,
         enabled: !!document,
