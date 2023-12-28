17b909c7fb1ed4e6fac6a5b41aa4e5183bc09782
Murilo Montino <murilomontinojr@hotmail.com>
2023-12-28 20:16:42 -0300

    refactor: usePetsByTutor to support different strategies

--- source/store/hooks/list-pets-of-tutor/use-list-pets-of-tutor.tsx
+++ source/store/hooks/list-pets-of-tutor/use-list-pets-of-tutor.tsx
@@ -1,22 +1,40 @@
+import { AbstractEntity } from '~/entities/BuilderEntity'
 import { Pet } from '~/entities/Pet'
-import { createPet, getAllPets } from '~/services/helpers'
+import { PetSimplified } from '~/entities/PetSimplified'
+import { createPet, createPetSimplified, getAllPets } from '~/services/helpers'
 import { IPet } from '~/types/pet'
 import { IPetV2 } from '~/types/pet-v2'
 import useAppStore from '../use-app-store'
 
 export const NAME = 'list-pets-of-tutor'
 
+const createStrategy = new Map([
+    ['simple', createPetSimplified],
+    ['full', createPet],
+])
+
+const entityStrategy = new Map<string, AbstractEntity>([
+    ['simple', PetSimplified],
+    ['full', Pet],
+])
+
 const usePetsByTutor = (
-    document: string
+    document: string,
+    strategy: 'simple' | 'full' = 'full',
+    handleCloseModal?: () => void
 ) => {
     const superKeys = [NAME, document]
 
+    const create = createStrategy.get(strategy) || createPet
+    const entity = entityStrategy.get(strategy) || Pet
+
     return useAppStore<IPetV2, IPet>({
         get: getAllPets.bind(null, document),
-        add: createPet,
-        entity: Pet,
+        add: create,
+        entity: entity,
         keys: superKeys,
         name: NAME,
+        handleCloseModal,
         enabled: !!document,
     })
 }
