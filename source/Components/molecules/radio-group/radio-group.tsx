fd7af0268d5292d43e8335d19e7dc617c1506107
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-08 20:52:56 -0300

    refactor: radio-group component

--- source/Components/molecules/radio-group/radio-group.tsx
+++ source/Components/molecules/radio-group/radio-group.tsx
@@ -1,35 +1,61 @@
-
-import { useField } from 'formik'
-import Checkbox from '~/Components/atoms/checkbox'
-import Label from '~/Components/atoms/label'
+import { Field } from 'formik';
+import { checkbox } from '~/Components/atoms/checkbox';
+import Label from '~/Components/atoms/label';
 
 interface RadioGroupProps<T> extends React.HTMLAttributes<HTMLDivElement> {
-    items: (T & { name: string, value: any })[]
-    name: string
+    items: (T & { name: string; value: unknown })[];
+    name: string;
+    title: string;
+    required?: boolean;
+    separator?: string;
 }
 
-export default function RadioGroup<T>({ items = [], name, className, ...rest }: RadioGroupProps<T>) {
-
-    const [field, meta] = useField(name)
-
+export default function RadioGroup<T>({
+    items = [],
+    name,
+    title,
+    className,
+    required,
+    separator = ':',
+    ...rest
+}: RadioGroupProps<T>) {
     return (
-
-        <div className="mx-auto w-full relative flex justify-center items-center flex-wrap gap-2">
-            {
-                items.map((item, index) => (
-                    <div className="flex flex-row items-center justify-center" key={index}>
-                        <Checkbox
-                            id={item.name}
-                            type="radio"
-                            radio
-                            required
-                            {...field}
-                        />
-                        <Label htmlFor={item.name} label={item.name} />
+        <div className="col-span-full">
+            <Label
+                label={title}
+                required={required}
+                id={name as string}
+                separator={separator as string}
+            />
+            <div
+                className="justify-center items-center flex mobile:flex-col mobile:items-start col-span-full"
+                role="group"
+                aria-labelledby={name as string}
+            >
+                {items.map((item, index) => (
+                    <div
+                        className="flex flex-row items-center justify-center"
+                        key={item.name + index}
+                    >
+                        <Label
+                            required={required}
+                            id={item.name as string}
+                            label={item.name}
+                        >
+                            <Field
+                                className={checkbox({
+                                    className,
+                                    radio: true,
+                                    ...rest,
+                                })}
+                                name={name}
+                                type="radio"
+                                value={item.value}
+                            />
+                        </Label>
                     </div>
-                ))
-            }
+                ))}
+            </div>
         </div>
-
-    )
+    );
 }
