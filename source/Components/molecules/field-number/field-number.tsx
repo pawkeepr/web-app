cf8ddad2850982eeb22ca8a401977954f20a32d5
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-08 12:54:11 -0300

    fix: FieldNumber component

--- source/Components/molecules/field-number/field-number.tsx
+++ source/Components/molecules/field-number/field-number.tsx
@@ -1,24 +1,33 @@
-
-import { InputNumberFormat, InputNumberFormatProps } from '@react-input/number-format';
-import FieldControl, { InputControlProps } from '~/Components/molecules/field-control';
+import {
+    InputNumberFormat,
+    InputNumberFormatProps,
+} from '@react-input/number-format';
+import FieldControl, {
+    InputControlProps,
+} from '~/Components/molecules/field-control';
 
 import { input } from '~/Components/atoms/input';
 
-const FieldNumber = ({
+const FieldNumber = <Ctx,>({
     placeholder,
     name,
+    locales = 'en-US',
+    maximumFractionDigits = 2,
+    groupDisplay = false,
     ...props
-}: InputControlProps<InputNumberFormatProps>) => {
+}: InputControlProps<InputNumberFormatProps, Ctx>) => {
     return (
         <FieldControl
             {...props}
-            maximumFractionDigits={2}
-            placeholder={placeholder || "Digite aqui..."}
+            placeholder={placeholder || 'Digite aqui...'}
             name={name}
-            component={InputNumberFormat as any}
+            locales={locales}
+            groupDisplay={groupDisplay}
+            maximumFractionDigits={maximumFractionDigits}
+            component={InputNumberFormat}
             className={input({ className: props.className })}
         />
-    )
-}
+    );
+};
 
-export default FieldNumber
+export default FieldNumber;
