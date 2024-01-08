0c4968d9d02b3469c38d600495568654c9d20877
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-08 12:53:14 -0300

    refactor: field-control types.ts [key:string]: unknown.

--- source/Components/molecules/field-control/types.ts
+++ source/Components/molecules/field-control/types.ts
@@ -1,17 +1,19 @@
-import { FieldHookConfig } from "formik";
-import { ObjPaths } from "~/types/helpers";
+import { FieldHookConfig } from 'formik';
+import { ObjPaths } from '~/types/helpers';
 
-export type InputControlProps<T, Ctx = any> = FieldHookConfig<string> & T & {
-    [key: string]: any
-    endIcon?: React.ReactNode;
-    label?: string;
-    ctx?: Ctx;
-    name: ObjPaths<Ctx>;
-    input?: Omit<React.ElementType, 'name'> | 'input';
-    startIcon?: React.ReactNode;
-    required?: boolean;
-    pattern?: string;
-    mask?: string | Array<string | RegExp>;
-    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    divClassName?: string;
-};
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export type InputControlProps<T, Ctx = any> = FieldHookConfig<string> &
+    T & {
+        [key: string]: unknown;
+        endIcon?: React.ReactNode;
+        label?: string;
+        ctx?: Ctx;
+        name: ObjPaths<Ctx>;
+        input?: Omit<React.ElementType, 'name'> | 'input';
+        startIcon?: React.ReactNode;
+        required?: boolean;
+        pattern?: string;
+        mask?: string | Array<string | RegExp>;
+        onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+        divClassName?: string;
+    };
