9202f608ae66dec26b57b91346e71e9ef5766b63
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-08 09:50:03 -0300

    chore: add biome.json

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,45 +1,37 @@
-
-import '~/globals.scss';
-import '~/tailwind.css';
+import "~/globals.scss";
+import "~/tailwind.css";
 
 import Provider from "~/store";
 
-import type { AppProps } from 'next/app';
+import type { AppProps } from "next/app";
 
 import { AuthProvider } from "~/contexts/auth-context";
 import LayoutProvider from "~/contexts/layout-context";
 
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import '~/aws';
-import ErrorBoundary from '~/contexts/error-boundary';
-import fakebackend from '~/helpers/AuthType/fakeBackend';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "~/aws";
+import ErrorBoundary from "~/contexts/error-boundary";
 
-import VLibras from '@djpfs/react-vlibras';
-
-const isProd = process.env.NODE_ENV === 'production';
-
-isProd && fakebackend();
+import VLibras from "@djpfs/react-vlibras";
 
 function App({ Component, pageProps }: AppProps) {
-
-
-    return (
-        <ErrorBoundary>
-            <VLibras forceOnload={true} />
-            <Provider>
-                <AuthProvider>
-                    <LayoutProvider>
-                        <ToastContainer
-                            theme='colored'
-                            className="!w-full p-4 bg-dark bg-opacity-50 !h-screen  flex flex-1 items-center justify-center absolute top-0"
-                        />
-                        <Component {...pageProps} />
-                    </LayoutProvider>
-                </AuthProvider>
-            </Provider>
-        </ErrorBoundary>
-    )
+	return (
+		<ErrorBoundary>
+			<VLibras forceOnload={true} />
+			<Provider>
+				<AuthProvider>
+					<LayoutProvider>
+						<ToastContainer
+							theme="colored"
+							className="!w-full p-4 bg-dark bg-opacity-50 !h-screen  flex flex-1 items-center justify-center absolute top-0"
+						/>
+						<Component {...pageProps} />
+					</LayoutProvider>
+				</AuthProvider>
+			</Provider>
+		</ErrorBoundary>
+	);
 }
 
 export default App;
--- setupMocks.ts
+++ setupMocks.ts
@@ -1,3 +1,3 @@
-import '~/__mocks__/auth-helpers';
-import '~/__mocks__/next-get-config';
+import "~/__mocks__/auth-helpers";
+import "~/__mocks__/next-get-config";
 
