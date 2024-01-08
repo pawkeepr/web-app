f29bb3b65a5253bfc47d3ac6e826476a614456f4
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-08 12:52:33 -0300

    fix: import paths in field-control.tsx

--- source/Components/molecules/field-control/field-control.tsx
+++ source/Components/molecules/field-control/field-control.tsx
@@ -1,13 +1,13 @@
-import { useField } from "formik";
+import { useField } from 'formik';
 
-
-import type { InputControlProps } from "./types";
+import type { InputControlProps } from './types';
 
 import cn from 'classnames';
 import { twMerge } from 'tailwind-merge';
-import Input from "~/Components/atoms/input/input";
-import Label from "~/Components/atoms/label";
+import Input from '~/Components/atoms/input/input';
+import Label from '~/Components/atoms/label';
 
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 const FieldControl = <T, Ctx = any>({
     label,
     children,
@@ -23,21 +23,25 @@ const FieldControl = <T, Ctx = any>({
     divClassName,
     ...props
 }: InputControlProps<T, Ctx>) => {
-
     const [inputProps, meta] = useField(props);
     const id = props.name || props.id;
 
-    const InputComponent = component as any;
+    const InputComponent = component as JSX.ElementType;
 
-    const onChange = (e: any) => {
+    const onChange = (e) => {
         props.onChange?.(e);
         inputProps.onChange(e);
     };
 
     return (
-        <div className={twMerge("w-full", divClassName)}>
-            <Label label={label} required={required} id={id} separator={separator} />
-            <div className='relative'>
+        <div className={twMerge('w-full', divClassName)}>
+            <Label
+                label={label}
+                required={required}
+                id={id as string}
+                separator={separator as string}
+            />
+            <div className="relative">
                 {startIcon && (
                     <div className="absolute inset-y-0 flex items-center pl-1 text-sm text-gray-400 pointer-events-none left-1">
                         {startIcon}
@@ -47,31 +51,26 @@ const FieldControl = <T, Ctx = any>({
                     id={id}
                     required={required}
                     data-testid={`input-${id}`}
-                    className={
-                        twMerge(
-                            cn(
-                                {
-                                    'border-secondary-500': required,
-                                    'bg-slate-100': props.disabled,
-                                    '!pl-8': startIcon,
-                                    '!pr-8': endIcon,
-                                },
-
-                            ), className)
-                    }
+                    className={twMerge(
+                        cn({
+                            'border-secondary-500': required,
+                            'bg-slate-100': props.disabled,
+                            '!pl-8': startIcon,
+                            '!pr-8': endIcon,
+                        }),
+                        className,
+                    )}
                     {...inputProps}
                     {...props}
                     onChange={onChange}
                 />
                 {endIcon && (
-                    <div className={`absolute top-1/2 transform -translate-y-1/2 right-0 mr-2`}>
+                    <div className="absolute top-1/2 transform -translate-y-1/2 right-0 mr-2">
                         {endIcon}
                     </div>
                 )}
             </div>
-            {!meta.error && (
-                <div className="pb-2" />
-            )}
+            {!meta.error && <div className="pb-2" />}
 
             {meta.error && (
                 <div className="w-full text-xs text-center text-secondary-500 font-semibold">
