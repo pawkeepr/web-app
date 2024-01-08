49afd5b743a05ac5b5941cd2c95a3915cb17bf14
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-08 17:42:59 -0300

    refactor: FieldTextArea export

--- source/Components/molecules/field-text-area/index.ts
+++ source/Components/molecules/field-text-area/index.ts
@@ -1,8 +1,3 @@
-import { InputControlProps } from '../field-control';
 import FieldTextArea from './field-text-area';
 
-type FieldTextAreaAssign = <T, Ctx>(
-    props: InputControlProps<T, Ctx>,
-) => JSX.Element;
-
-export default FieldTextArea as FieldTextAreaAssign;
+export default FieldTextArea;
