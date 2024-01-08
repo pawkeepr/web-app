6f8d46bec931ab9ccc533e883e6b09d72d3b6962
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-08 12:56:07 -0300

    refactor: step-treatment component

--- source/pages/AppointmentsPage/components/organisms/steps/step-treatment/step-treatment.tsx
+++ source/pages/AppointmentsPage/components/organisms/steps/step-treatment/step-treatment.tsx
@@ -1,91 +1,101 @@
-import { BtnCancel, BtnPrimary } from "~/Components/atoms/btn";
+import { BtnCancel, BtnPrimary } from '~/Components/atoms/btn';
 
 import { FieldArray, useFormikContext } from 'formik';
-import CardInput from "~/Components/organism/card-input";
-import { ItemTreatment, OptionSelect } from "~/store/slices/appointment-vet/types";
-import { StepProps } from "~/types/helpers";
+import CardInput from '~/Components/organism/card-input';
+import { OptionSelect } from '~/store/slices/appointment-vet/types';
+import { VeterinaryConsultation } from '~/types/appointment';
+import { StepProps } from '~/types/helpers';
+
+type CtxStepTreatment = Pick<VeterinaryConsultation, 'treatments'>;
 
 const items: OptionSelect[] = [
     {
         value: 'activities_carry',
-        label: 'Recomendações de atividades físicas'
+        label: 'Recomendações de atividades físicas',
     },
     {
         value: 'fast_test',
-        label: 'Testes rápidos'
+        label: 'Testes rápidos',
     },
     {
         value: 'medicine',
-        label: 'Medicação'
+        label: 'Medicação',
     },
     {
         value: 'vaccine',
-        label: 'Vacina'
+        label: 'Vacina',
     },
     {
         value: 'exam',
-        label: 'Exame'
+        label: 'Exame',
     },
     {
         value: 'nutrition',
-        label: 'Nutrição Alimentar'
-    }
-]
+        label: 'Nutrição Alimentar',
+    },
+];
 
 const StepTreatment = ({ toggleTab, activeTab }: StepProps) => {
-    const { values } = useFormikContext<{ treatments: ItemTreatment[] }>();
+    const { values } = useFormikContext<CtxStepTreatment>();
 
     return (
         <section className="card card-body shadow-lg">
             <h4 className="text-center font-sans font-semibold text-base capitalize">
                 Informações de Tratamento
                 <br />
-                <span className="text-xs font-bold text-secondary-500">Obrigatório (*)</span>
+                <span className="text-xs font-bold text-secondary-500">
+                    Obrigatório (*)
+                </span>
             </h4>
-            <FieldArray
-                name="treatments"
-            >
-                {
-                    ({ push, remove }) => (
-                        <>
-                            {
-                                values.treatments.map((treatment, index) => (
-                                    <div key={index} className="w-full bg-secondary rounded-md text-xs py-1 px-2" >
-                                        <div className="w-full flex flex-row bg-secondary px-2 rounded-sm border-dashed border border-primary">
-                                            <div className="grid grid-cols-6 w-full">
-                                                <h6 className="col-span-1 font-mono font-semibold  capitalize">
-                                                    {(treatment.type as OptionSelect).label}
-                                                </h6>
-                                                <h6 className="col-span-2 font-mono font-semibold  capitalize">
-                                                    {treatment.name}
-                                                </h6>
+            <FieldArray name="treatments">
+                {({ push, remove }) => (
+                    <>
+                        {values.treatments?.questions_treatment?.map(
+                            (treatment, index) => (
+                                <div
+                                    key={`treatment-${index}`}
+                                    className="w-full bg-secondary rounded-md text-xs py-1 px-2"
+                                >
+                                    <div className="w-full flex flex-row bg-secondary px-2 rounded-sm border-dashed border border-primary">
+                                        <div className="grid grid-cols-6 w-full">
+                                            <h6 className="col-span-1 font-mono font-semibold  capitalize">
+                                                {
+                                                    (
+                                                        treatment.type_treatment as OptionSelect
+                                                    ).label
+                                                }
+                                            </h6>
+                                            <h6 className="col-span-2 font-mono font-semibold  capitalize">
+                                                {treatment.name_treatment}
+                                            </h6>
 
-                                                <p className="col-span-3 font-mono  capitalize">
-                                                    {treatment.notes}
-                                                </p>
-                                            </div>
-                                            <button
-                                                type="button"
-                                                className="text-red-500"
-                                                onClick={() => remove(index)}
-                                            >
-                                                X
-                                            </button>
+                                            <p className="col-span-3 font-mono  capitalize">
+                                                {treatment.notes_treatment}
+                                            </p>
                                         </div>
+                                        <button
+                                            type="button"
+                                            className="text-red-500"
+                                            onClick={() => remove(index)}
+                                        >
+                                            X
+                                        </button>
                                     </div>
-                                ))
-                            }
-                            <CardInput
-                                items={items}
-                                handleSubmit={async (data, formikHelpers) => {
-                                    await new Promise((resolve) => setTimeout(resolve, 300))
-                                    push(data)
-                                    formikHelpers.resetForm()
-                                }}
-                            />
-                        </>
-                    )
-                }
+                                </div>
+                            ),
+                        )}
+                        <CardInput
+                            items={items}
+                            handleSubmit={async (data, formikHelpers) => {
+                                await new Promise((resolve) =>
+                                    setTimeout(resolve, 300),
+                                );
+                                push(data);
+                                formikHelpers.resetForm();
+                            }}
+                        />
+                    </>
+                )}
             </FieldArray>
 
             <div className="flex items-center justify-center">
