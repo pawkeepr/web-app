9dbc242a4dcc967d72640518180b89f22b3dadc1
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-09 09:12:21 -0300

    feat: add Portuguese (Brazil) translation

--- source/i18n.ts
+++ source/i18n.ts
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 'use client';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import i18n from 'i18next';
 import detector from 'i18next-browser-languagedetector';
@@ -11,6 +11,7 @@ import translationENG from './locales/en.json';
 import translationFR from './locales/fr.json';
 import translationGr from './locales/gr.json';
 import translationIT from './locales/it.json';
+import translationPT_BR from './locales/pt-BR.json';
 import translationRS from './locales/ru.json';
 import translationSP from './locales/sp.json';
 
@@ -37,17 +38,24 @@ const resources = {
     fr: {
         translation: translationFR,
     },
-};
+    'pt-BR': {
+        translation: translationPT_BR,
+    },
+} as const;
+
+type Language = keyof typeof resources;
 
 const useLanguage = () => {
-    const [language, setLanguage] = useState('en');
+    const [language, setLanguage] = useState<Language>('pt-BR');
 
     useEffect(() => {
         if (!language) {
             localStorage.setItem('I18N_LANGUAGE', 'en');
         }
 
-        setLanguage(localStorage.getItem('I18N_LANGUAGE') || 'en');
+        setLanguage(
+            (localStorage.getItem('I18N_LANGUAGE') as Language) || 'en',
+        );
     }, [language]);
 
     return i18n
