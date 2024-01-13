cb92ce587975022b2d8290dc0b2be3d6ebdfa80e
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-13 11:10:33 -0300

    fix: update StepDocument component and fix initial values

--- source/Components/modals/list-pets-modal/components/steps/step-document.tsx
+++ source/Components/modals/list-pets-modal/components/steps/step-document.tsx
@@ -3,25 +3,27 @@ import { useMemo } from 'react'
 import BoxButtons from '~/Components/molecules/box-buttons/box-buttons'
 import FieldDocument from '~/Components/molecules/field-document'
 import useFormikContextSafe from '~/hooks/use-formik-context-safe'
-import { CtxSimplifiedPedFields, StepProps } from '../../types'
+import type { CtxSimplifiedPeTFields, StepProps } from '../../types'
 
 const StepDocument = ({ nextStep, onChangeDocument }: StepProps) => {
-    const { values } = useFormikContextSafe<CtxSimplifiedPedFields>()
+    const { values } = useFormikContextSafe<CtxSimplifiedPeTFields>()
 
     const validateDocument = useMemo(() => {
-        const document = values.cpf_tutor
+        const document = values.ownerEmergencyContact?.cpf_cnpj
         return cpf.isValid(document) || cnpj.isValid(document)
-    }, [values.cpf_tutor])
+    }, [values.ownerEmergencyContact])
 
     return (
         <div className="mt-3 p-1 gap-2 ">
             <FieldDocument
-                ctx={{} as CtxSimplifiedPedFields}
+                ctx={values}
                 required
-                name="cpf_tutor"
+                name="ownerEmergencyContact.cpf_cnpj"
                 className="w-full flex-1 mt-2"
                 placeholder="CPF/CNPJ"
-                onChange={(e) => onChangeDocument?.(e.target.value)}
+                onChange={(e: { target: { value: string } }) =>
+                    onChangeDocument?.(e.target.value)
+                }
             />
             <BoxButtons
                 isValid={validateDocument}
