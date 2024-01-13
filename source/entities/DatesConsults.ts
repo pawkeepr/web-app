26167f8dad6f9ae71a79a5e61441ae919b8a10d6
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-13 11:08:47 -0300

    refactor: DateConsults class to use type imports

--- source/entities/DatesConsults.ts
+++ source/entities/DatesConsults.ts
@@ -1,38 +1,20 @@
-import {
-    IAppointmentVet,
-    IDatesConsultsAppointment,
-    IStatusAppointment,
-} from '~/store/slices/appointment-vet/types'
+import type { VeterinaryConsultation } from '~/types/appointment'
 
-type DateConsultsProps = Pick<
-    IAppointmentVet,
-    'dates_consults' | 'id' | 'appointment_status'
->
+type DateConsultsProps = Pick<VeterinaryConsultation, 'dates_consults' | 'id'>
 
 class DateConsults implements DateConsultsProps {
     id: string | null
-    appointment_status: IStatusAppointment
-    dates_consults: IDatesConsultsAppointment
+    dates_consults: VeterinaryConsultation['dates_consults']
 
     private constructor() {
         this.id = null
         this.dates_consults = {
             additional_remarks: '',
             date_consultation: '',
-            date_next_consultation: '',
             reason_consultation: '',
             time_consultation: '',
-            time_next_consultation: '',
             type_consultation: '',
         }
-
-        this.appointment_status = {
-            canceled: 'no',
-            confirmed: 'no',
-            done: 'no',
-            reason_canceled: 'no',
-            scheduled: 'yes',
-        } as IStatusAppointment
     }
 
     defineId(id: string): this {
@@ -40,14 +22,10 @@ class DateConsults implements DateConsultsProps {
         return this
     }
 
-    defineDateConsultas(date_consultation: IDatesConsultsAppointment): this {
+    defineDateConsultas(
+        date_consultation: VeterinaryConsultation['dates_consults'],
+    ): this {
         this.dates_consults = date_consultation
-        return this
-    }
-
-    defineAppointmentStatus(appointment_status: IStatusAppointment): this {
-        if (!appointment_status) return this
-        this.appointment_status = appointment_status
         return this
     }
 
@@ -57,7 +35,6 @@ class DateConsults implements DateConsultsProps {
         return entity
             .defineId(props.id as string)
             .defineDateConsultas(props.dates_consults)
-            .defineAppointmentStatus(props.appointment_status)
     }
 }
 
