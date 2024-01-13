9a4cd94efd5989144e2ef118b07e0c67421ab4cb
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-13 15:55:28 -0300

    refactor: Add box-buttons component and update card-scheduled component

--- source/Components/organism/box-buttons/box-buttons.tsx
+++ source/Components/organism/box-buttons/box-buttons.tsx
@@ -4,15 +4,16 @@ import { useCallback } from 'react'
 import { BtnCancel, BtnConfirm, BtnLabel, BtnPrimary } from '~/Components/atoms/btn'
 import withLoading from '~/Components/helpers/with-loading'
 import { ModalPlus, usePlusModal } from '~/hooks/use-plus-modal'
-import { IAppointmentVet } from '~/store/slices/appointment-vet/types'
+import type { VeterinaryConsultation } from '~/types/appointment'
 
 type BoxButtonsProps = {
     isLoading?: boolean
-    item: IAppointmentVet
+    item: VeterinaryConsultation
 }
 
 const BoxButtons = ({ isLoading = false, item }: BoxButtonsProps) => {
     const router = useRouter()
+
     const { setItem, open, close } = usePlusModal()
 
     const onClickCancel = useCallback(() => {
@@ -35,33 +36,48 @@ const BoxButtons = ({ isLoading = false, item }: BoxButtonsProps) => {
 
     const startAppointment = useCallback(() => {
         router.push(
-            `/dashboard/appointments?appointment_id=${item.id}&document=${item.cpf_tutor}&pet=${item.id_pet}`,
+            `/dashboard/appointments?appointment_id=${item.id}&document=${item.tutor_pet_vet?.tutor?.cpf_cnpj}&pet=${item.tutor_pet_vet?.pet?.id_pet}`,
         )
     }, [item])
 
     return (
         <div className="gap-1 justify-end flex w-full mobile:grid mobile:grid-cols-1 flex-wrap">
             <BtnLabel
-                condition={!isLoading}
+                condition={
+                    !isLoading &&
+                    item.appointment_status?.canceled === 'no' &&
+                    item.appointment_status?.done === 'no'
+                }
                 label="Cancelar Consulta"
                 onClick={onClickCancel}
                 className="text-red-500 border-none mobile:col-span-1"
             />
 
             <BtnCancel
-                condition={!isLoading}
+                condition={!isLoading && item.appointment_status?.done === 'no'}
                 label="Reagendar Consulta"
                 onClick={onClickReScheduled}
                 className="border-none mobile:!w-full mobile:col-span-1 text-gray-500"
             />
 
             <BtnConfirm
+                condition={
+                    !isLoading &&
+                    item.appointment_status?.confirmed === 'no' &&
+                    item.appointment_status?.canceled === 'no' &&
+                    item.appointment_status?.done === 'no'
+                }
                 label="Confirmar Consulta"
                 className="border-none mobile:!w-full mobile:col-span-1 text-gray-200"
                 onClick={onClickConfirmed}
             />
 
             <BtnPrimary
+                condition={
+                    !isLoading &&
+                    item.appointment_status?.canceled === 'no' &&
+                    item.appointment_status?.done === 'no'
+                }
                 label="Iniciar Consulta"
                 className="border-none mobile:!w-full mobile:col-span-1"
                 onClick={startAppointment}
