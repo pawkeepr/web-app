9b7737ddd6e77c40f77912a90baec4bb2faefe8a
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-14 17:31:39 -0300

    feat: modal card de infor para pets em consultas

--- source/Components/organism/box-buttons/modal-box-buttons.tsx
+++ source/Components/organism/box-buttons/modal-box-buttons.tsx
@@ -1,98 +1,143 @@
-import { Dialog, Transition } from '@headlessui/react'
-import { Fragment } from 'react'
+import { forwardRef, useImperativeHandle, useMemo } from 'react'
 import { BtnConfirm } from '~/Components/atoms/btn'
-import withControl from '~/Components/helpers/with-control'
 import useModal from '~/hooks/use-modal'
+import AvatarPet from '~/pages/AppointmentsPage/components/atoms/pet-avatar/pet-avatar'
+import { Gender, Species } from '~/store/slices/pets/speciesType'
 import type { VeterinaryConsultation } from '~/types/appointment'
+import { calcAge } from '~/utils/calc-age'
+import { getNameTutor } from '~/utils/get-name-tutors'
+import Modal from '../modal'
 import BoxButtons from './box-buttons'
 
 type ModalBoxButtonsProps = {
     item: VeterinaryConsultation
+    children?: null | ((props: { showModal: () => void }) => JSX.ElementType)
 }
 
-const ModalBoxButtons = ({ item }: ModalBoxButtonsProps) => {
-    const { closeModal, open, showModal } = useModal()
+const ModalBoxButtons = forwardRef(
+    ({ item, children }: ModalBoxButtonsProps, ref) => {
+        const { closeModal, open, showModal } = useModal()
 
-    return (
-        <>
-            <BtnConfirm
-                label="Ver Mais"
-                className="border-none mobile:!w-full mobile:col-span-full text-gray-200"
-                onClick={showModal}
-            />
+        useImperativeHandle(
+            ref,
+            () => {
+                return {
+                    showModal,
+                    closeModal,
+                    open,
+                }
+            },
+            [open, closeModal, showModal],
+        )
 
-            <Transition appear show={open} as={Fragment}>
-                <Dialog as="div" className="relative z-10" onClose={closeModal}>
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <div className="fixed inset-0 bg-black bg-opacity-25" />
-                    </Transition.Child>
+        const pet = useMemo(
+            () => ({
+                ...item.tutor_pet_vet.pet,
+                specie: Species[
+                    item.tutor_pet_vet?.pet?.specie as keyof typeof Species
+                ],
+                gender: Gender[item.tutor_pet_vet?.pet?.sex as Gender],
+                name_tutor: getNameTutor(item.tutor_pet_vet?.tutor),
+                contact: item.tutor_pet_vet?.tutor?.contact,
+            }),
+            [item.tutor_pet_vet],
+        )
 
-                    <div className="fixed inset-0 overflow-y-auto">
-                        <div className="flex min-h-full items-center justify-center p-4 text-center">
-                            <Transition.Child
-                                as={Fragment}
-                                enter="ease-out duration-300"
-                                enterFrom="opacity-0 scale-95"
-                                enterTo="opacity-100 scale-100"
-                                leave="ease-in duration-200"
-                                leaveFrom="opacity-100 scale-100"
-                                leaveTo="opacity-0 scale-95"
-                            >
-                                <Dialog.Panel
-                                    className="
-                                    w-full 
-                                    max-w-md 
-                                    transform 
-                                    overflow-hidden 
-                                    rounded-2xl 
-                                    bg-white 
-                                    p-6 
-                                    text-left 
-                                    align-middle 
-                                    shadow-xl 
-                                    transition-all
-                                    dark:!bg-dark-500
-                                    dark:!text-gray-200
-                                    !font-sans
-                                "
-                                >
-                                    <Dialog.Title
-                                        as="h2"
-                                        className="text-xl font-semibold leading-6 text-gray-900 dark:!text-gray-200 text-center"
-                                    >
-                                        Title
-                                    </Dialog.Title>
+        return (
+            <>
+                {children?.({ showModal })}
+                {!children && (
+                    <BtnConfirm
+                        label="Ver Mais"
+                        className="border-none mobile:!w-full mobile:col-span-full text-gray-500 bg-transparent"
+                        onClick={showModal}
+                    />
+                )}
 
-                                    <Dialog.Description
-                                        as="p"
-                                        className="text-sm font-bold text-secondary-500 dark:!text-secondary-500 text-center"
-                                    >
-                                        Subtitle
-                                    </Dialog.Description>
+                <Modal
+                    onOpen={() => showModal}
+                    onClose={() => closeModal()}
+                    modal
+                    nested
+                    open={open}
+                    lockScroll
+                    className="pb-0 w-[750px]"
+                >
+                    <div className="flex flex-col justify-between items-center w-full h-[90%] p-4">
+                        <AvatarPet
+                            name={pet?.name_pet}
+                            alt={`Avatar do Pet ${pet?.name_pet}`}
+                            title={(props) => (
+                                <h1 className="text-center text-muted text-xl font-sans">
+                                    {props.name}
+                                </h1>
+                            )}
+                        />
+                        <section className="w-full grid grid-cols-2 mt-4">
+                            <div className="col-span-1">
+                                <div className="text-gray-500 mb-2">
+                                    <h3 className="font-bold">Pet:</h3>
+                                    <p>{`${pet?.name_pet}, ${pet?.specie}, ${pet?.race}`}</p>
+                                </div>
 
-                                    <div className="mt-3 p-1">
-                                        <p className="text-sm text-gray-500 dark:!text-gray-300 leading-6">
-                                            Message
-                                        </p>
+                                <div className="text-gray-500 mb-2">
+                                    <h3 className="font-bold">Idade do Pet:</h3>
+                                    <p>{calcAge(pet?.date_birth)} Anos</p>
+                                </div>
+
+                                <div className="text-gray-500 mb-2">
+                                    <h3 className="font-bold">Sexo do Pet:</h3>
+                                    <p>{pet?.gender}</p>
+                                </div>
+                                {item.appointment_status?.canceled === 'no' && (
+                                    <div className="text-gray-500 mb-2">
+                                        <h3 className="font-bold">Data Marcada:</h3>
+                                        <p>{`${
+                                            Intl.DateTimeFormat('pt-BR', {}).format(
+                                                new Date(
+                                                    item.dates_consults
+                                                        .date_consultation,
+                                                ),
+                                            ) || 'Não informado'
+                                        } às ${
+                                            item.dates_consults.time_consultation
+                                        }`}</p>
                                     </div>
+                                )}
+                            </div>
+                            <div className="col-span-1">
+                                <div className="text-gray-500 mb-2">
+                                    <h3 className="font-bold">Tutor:</h3>
+                                    <p>{pet?.name_tutor}</p>
+                                </div>
 
-                                    <BoxButtons item={item} />
-                                </Dialog.Panel>
-                            </Transition.Child>
+                                <div className="text-gray-500 mb-2">
+                                    <h3 className="font-bold">Telefone:</h3>
+                                    <p>{pet?.contact?.phone}</p>
+                                </div>
+
+                                <div className="text-gray-500 mb-2">
+                                    <h3 className="font-bold">Whatsapp:</h3>
+                                    <p>
+                                        {pet?.contact?.whatsapp || 'Não informado'}
+                                    </p>
+                                </div>
+
+                                <div className="text-gray-500 mb-2">
+                                    <h3 className="font-bold">Email:</h3>
+                                    <p>{pet?.contact?.email}</p>
+                                </div>
+                            </div>
+                        </section>
+
+                        <div className="w-full mt-4 self-end">
+                            <BoxButtons item={item} />
                         </div>
                     </div>
-                </Dialog>
-            </Transition>
-        </>
-    )
-}
+                </Modal>
+            </>
+        )
+    },
+)
 
-export default withControl(ModalBoxButtons)
+export default ModalBoxButtons
