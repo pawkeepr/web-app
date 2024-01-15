34e3bd39cef834617bbff20c4e846d5720acb859
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-15 20:18:04 -0300

    feat: adiciona página de confirmação de cliente e atualiza layout de confirmação

--- source/pages/ConfirmationPage/Confirmation.tsx
+++ source/pages/ConfirmationPage/Confirmation.tsx
@@ -1,32 +1,39 @@
 'use client'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import { BtnPrimary } from '~/Components/atoms/btn'
-import { resetLoading } from '~/store/actions'
-import { useAppDispatch } from '~/store/hooks'
-import DefaultLayout from '../_layouts/dashboard/dashboard'
+import { BtnCancel, BtnLink, BtnPrimary } from '~/Components/atoms/btn'
+import AuthLayout from '../_layouts/auth/auth_layout'
 
 const ConfirmationPage = () => {
     return (
-        <DefaultLayout title="Confirmation">
-            <div className="flex flex-col justify-center items-center gap-7 lg:mt-52">
+        <AuthLayout title="Activation Profile">
+            <div className="flex flex-col justify-center items-center gap-3 lg:mt-5">
+                <p className="text-sm font-bold text-secondary-500">
+                    Seja Bem-vindo(a)!
+                </p>
+                <h1 className="font-sans text-xl font-bold mb-4">
+                    Confirmar Agendamento
+                </h1>
+            </div>
+
+            <div className="flex flex-row justify-center items-center">
                 <BtnPrimary
                     label="Confirmar Consulta"
                     className="w-2/4"
                     onClick={() => {}}
                 />
-                <BtnPrimary
+                <BtnCancel
                     label="Cancelar Consulta"
                     className="w-2/4"
                     onClick={() => {}}
                 />
-                <BtnPrimary
-                    label="Remarcar Consulta"
-                    className="w-2/4"
-                    onClick={() => {}}
-                />
             </div>
-        </DefaultLayout>
+            <div className="w-full flex flex-col justify-center items-center ">
+                <p className="-mb-2 text-gray-400 font-normal">
+                    Você não tem uma conta? Crie agora mesmo e obtenha todos os
+                    benefícios da plataforma
+                </p>
+                <BtnLink message="Criar Conta" href="/sign-up" />
+            </div>
+        </AuthLayout>
     )
 }
 
