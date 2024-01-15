69c87d84c2af3217d7dd7fbcc1a223cb39a19027
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-15 11:25:56 -0300

    refactor: refinamento do card de consultas

--- source/Components/organism/card-scheduled/card-scheduled.tsx
+++ source/Components/organism/card-scheduled/card-scheduled.tsx
@@ -1,9 +1,16 @@
-import { useRef, type ForwardRefExoticComponent, type RefAttributes } from 'react'
+import {
+    useMemo,
+    useRef,
+    type ForwardRefExoticComponent,
+    type RefAttributes,
+} from 'react'
+import { FaWhatsapp } from 'react-icons/fa'
 import { tv } from 'tailwind-variants'
 import MyImage from '~/Components/atoms/my-image'
 import ravena from '~/assets/images/ravena.jpeg'
 import type { IHookModal } from '~/hooks/use-modal'
 import useResizeMobile from '~/hooks/use-resize-mobile'
+import { Gender, Species } from '~/store/slices/pets/speciesType'
 import type { VeterinaryConsultation } from '~/types/appointment'
 import { getNameTutor } from '~/utils/get-name-tutors'
 import BoxButtons from '../box-buttons'
@@ -21,7 +28,7 @@ type CardScheduledProps = {
 
 const card = tv({
     base: `
-        bg-white relative flex flex-col rounded-lg px-2 py-2 shadow-md focus:outline-none
+        bg-white relative flex flex-row rounded-lg px-2 py-2 shadow-md focus:outline-none
     `,
     variants: {
         checked: {
@@ -44,8 +51,7 @@ const card = tv({
             no: '',
         },
         isMobile: {
-            true: 'hover:bg-gray-100 hover:bg-opacity-50 cursor-pointer',
-            false: 'mobile:grid mobile:grid-cols-2',
+            true: 'hover:bg-gray-100 hover:bg-opacity-50 cursor-pointer flex-col',
         },
     },
 })
@@ -60,6 +66,24 @@ const CardScheduled = ({
     const BoxButtons = boxButtons
     const name = getNameTutor(appointment?.tutor_pet_vet.tutor)
     const { isMobile } = useResizeMobile()
+
+    const pet = useMemo(
+        () => ({
+            ...appointment.tutor_pet_vet.pet,
+            specie: Species[
+                appointment.tutor_pet_vet?.pet?.specie as keyof typeof Species
+            ],
+            gender: Gender[appointment.tutor_pet_vet?.pet?.sex as Gender],
+        }),
+        [appointment],
+    )
+
+    const formatedDateAndHours = useMemo(() => {
+        const date = appointment?.dates_consults?.date_consultation
+        const hour = appointment?.dates_consults?.time_consultation
+        const dateAndHour = `${date} - ${hour}`
+        return dateAndHour
+    }, [appointment])
 
     return (
         <article
@@ -82,98 +106,74 @@ const CardScheduled = ({
                 canceled: appointment.appointment_status?.canceled,
             })}
         >
-            <div className="mb-2">
-                <div className="flex flex-col w-full">
-                    <div className="flex mobile:gap-3 justify-around items-center">
-                        <MyImage
-                            src={ravena}
-                            alt="Picture of the author"
-                            width={150}
-                            height={150}
-                            className="h-32 mt-3 w-32 rounded-full"
-                        />
-
-                        <div className="flex flex-col items-center">
-                            {'Informações Do Agendamento:'}
-                            <div className="">
-                                <div className="p-2">
-                                    <p className="text-gray-700 md:hidden">
-                                        Nome do pet:{' '}
-                                        {appointment?.tutor_pet_vet.pet?.name_pet}
-                                    </p>
-                                    <p className="text-gray-700">
-                                        Data:{' '}
-                                        {
-                                            appointment?.dates_consults
-                                                .date_consultation
-                                        }
-                                    </p>
-                                    <p className="text-gray-700">
-                                        Horário:{' '}
-                                        {
-                                            appointment?.dates_consults
-                                                .time_consultation
-                                        }
-                                    </p>
-                                    <p className="text-gray-700 md:hidden">
-                                        Contato:
-                                        {
-                                            appointment?.tutor_pet_vet.tutor
-                                                ?.contact?.phone
-                                        }
-                                    </p>
-                                </div>
-                            </div>
+            <picture className="mobile:w-full flex flex-1 justify-center items-center">
+                <MyImage
+                    src={ravena}
+                    alt="Picture of the author"
+                    width={150}
+                    height={150}
+                    className="h-32 mt-3 w-32 rounded-full"
+                />
+            </picture>
+            <div className="flex flex-col flex-[4] mobile:flex-1 w-full">
+                <div className="flex mobile:gap-3 justify-around items-center">
+                    <section>
+                        <div className="text-gray-500 mb-2">
+                            <h3 className="font-bold mb-1">Pet:</h3>
+                            <p>{`${pet?.name_pet}, ${pet?.specie}, ${pet?.race}, ${pet?.sex}`}</p>
                         </div>
-                        <div className="flex flex-col mobile:hidden items-center">
-                            {'Informações Do Pet:'}
-
-                            <div className="p-2 ">
-                                <p className="text-gray-700">
-                                    Nome do pet:{' '}
-                                    {appointment?.tutor_pet_vet.pet?.name_pet}
-                                </p>
-                                <p className="text-gray-700">
-                                    Especie:{' '}
-                                    {
-                                        appointment?.tutor_pet_vet.pet
-                                            ?.specie as string
-                                    }
-                                </p>
-                                <p className="text-gray-700">
-                                    Sexo:{' '}
-                                    {appointment?.tutor_pet_vet.pet?.sex as string}
-                                </p>
-                                <p className="text-gray-700">Microchip: 1294</p>
+                        {pet?.microchip && (
+                            <div className="text-gray-500 mb-2 mobile:hidden">
+                                <h3 className="font-bold mb-1">
+                                    Microchip do Pet:
+                                </h3>
+                                <p>{pet?.microchip}</p>
                             </div>
+                        )}
+                        <div className="text-gray-500 mb-2">
+                            <h3 className="font-bold mb-1">Nome do Tutor:</h3>
+                            <p>{name}</p>
                         </div>
-                        <div className="flex mobile:hidden flex-col">
-                            {'Informações Do Tutor:'}
+                        <div className="text-gray-500 mb-2">
+                            <h3 className="font-bold mb-1">Data da Consulta:</h3>
+                            <p>{formatedDateAndHours}</p>
+                        </div>
+                    </section>
+                    <section>
+                        <div className="text-gray-500 mb-2 mobile:hidden">
+                            <h3 className="font-bold mb-1">Email do Tutor:</h3>
+                            <p>
+                                {appointment?.tutor_pet_vet.tutor?.contact?.email}
+                            </p>
+                        </div>
 
-                            <div className="p-2">
-                                <p className="text-gray-700">Nome: {name}</p>
-                                <p className="text-gray-700">
-                                    Email:{' '}
+                        <div className="text-gray-500 mb-2 mobile:hidden">
+                            <h3 className="font-bold mb-1">Telefone do Tutor:</h3>
+                            <p>
+                                {appointment?.tutor_pet_vet.tutor?.contact?.phone}
+                            </p>
+                        </div>
+                        {appointment?.tutor_pet_vet.tutor?.contact?.whatsapp && (
+                            <div className="text-gray-500 mb-2 gap-2 mobile:hidden">
+                                <h3 className="font-bold mb-1">
+                                    WhatsApp do Tutor:{' '}
+                                </h3>
+                                <p className="flex gap-2">
+                                    <FaWhatsapp className="text-green-600 text-xl" />
+
                                     {
                                         appointment?.tutor_pet_vet.tutor?.contact
-                                            ?.email
-                                    }
-                                </p>
-                                <p className="text-gray-700">
-                                    Contato:{' '}
-                                    {
-                                        appointment?.tutor_pet_vet.tutor?.contact
-                                            ?.phone
+                                            ?.whatsapp
                                     }
                                 </p>
                             </div>
-                        </div>
-                    </div>
+                        )}
+                    </section>
                 </div>
-            </div>
 
-            {BoxButtons && !isMobile && <BoxButtons item={appointment} />}
-            {isMobile && <ModalBoxButtons item={appointment} ref={ref} />}
+                {BoxButtons && !isMobile && <BoxButtons item={appointment} />}
+                {isMobile && <ModalBoxButtons item={appointment} ref={ref} />}
+            </div>
         </article>
     )
 }
