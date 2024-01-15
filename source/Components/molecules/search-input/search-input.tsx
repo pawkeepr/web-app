f9dd1f449e4031b08df535f5e485cb96da656892
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-15 12:43:40 -0300

    refactor: adiciona ícone de busca e ajusta estilo do input de pesquisa

--- source/Components/molecules/search-input/search-input.tsx
+++ source/Components/molecules/search-input/search-input.tsx
@@ -1,15 +1,17 @@
+import { FaSearch } from 'react-icons/fa'
 import Input from '~/Components/atoms/input'
 
 type SearchInputProps = {
     value?: string
+    placeholder?: string
 } & React.HTMLAttributes<HTMLInputElement>
 
 const SearchInput = ({ ...rest }: SearchInputProps) => {
     return (
         <div className="flex w-full">
             <div className="search-box w-full">
-                <Input type="text" {...rest} />
-                <i className="ri-search-line search-icon" />
+                <FaSearch className="search-icon" />
+                <Input type="text" className="px-8" {...rest} />
             </div>
         </div>
     )
