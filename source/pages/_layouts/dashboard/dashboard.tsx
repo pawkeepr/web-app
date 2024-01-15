ef9f1d388915e72117435ca75640835faeab8c96
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-15 16:06:03 -0300

    refactor: removido componente FieldDocumentAppointment do layout do dashboard

--- source/pages/_layouts/dashboard/dashboard.tsx
+++ source/pages/_layouts/dashboard/dashboard.tsx
@@ -3,7 +3,6 @@
 import type React from 'react'
 
 import HeaderTitle from '~/Components/atoms/header-title'
-import FieldDocumentAppointment from '~/Components/molecules/field-document-appointment'
 import ContextModalPlus from '~/hooks/use-plus-modal'
 
 type DashboardLayoutsProps = {
@@ -18,11 +17,6 @@ const DashboardLayouts = ({
     return (
         <main className="min-h-screen">
             <HeaderTitle title={title} />
-            <div className="flex justify-end items-center">
-                <div className="flex-row flex items-center justify-end ">
-                    <FieldDocumentAppointment />
-                </div>
-            </div>
             {children}
             <ContextModalPlus />
         </main>
