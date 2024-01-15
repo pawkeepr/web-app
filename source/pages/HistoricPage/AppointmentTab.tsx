39aeeabbdfec1ba8482b469d04da019977be090e
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-15 12:51:16 -0300

    refactor: remover componentes de pesquisa e agendamento de consulta

--- source/pages/HistoricPage/AppointmentTab.tsx
+++ source/pages/HistoricPage/AppointmentTab.tsx
@@ -1,40 +1,15 @@
-import type React from 'react'
-import { useCallback, useDeferredValue, useEffect, useState } from 'react'
-import ScheduledNewAppointment from '~/Components/modals/scheduled-appointment'
 import FieldDocumentAppointment from '~/Components/molecules/field-document-appointment'
-import SearchInput from '~/Components/molecules/search-input'
 import DefaultLayout from '../_layouts/dashboard/dashboard'
 
 import ContextModalPlus from '~/hooks/use-plus-modal'
 import HorizontalTabs from './components/organisms/templates/Horizontal-List'
 
 const AppointmentsTabs = <T,>() => {
-    const [search, setSearch] = useState('')
-    const [filteredItems, setFilteredItems] = useState<T[]>([] as T[])
-    const deferredItems = useDeferredValue(filteredItems)
-
-    useEffect(() => {
-        setFilteredItems([])
-    }, [])
-
-    const handleSearch = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-        const search = e.target.value.toLowerCase()
-        setSearch(search)
-    }, [])
-
     return (
         <DefaultLayout title="Dashboard">
             <div className="flex justify-end items-center">
-                <SearchInput
-                    value={search}
-                    onChange={handleSearch}
-                    placeholder="Busque a Consulta..."
-                    className="rounded-md w-1/2"
-                />
-
                 <div className="w-1/2 flex-row flex items-center ">
                     <FieldDocumentAppointment />
-                    <ScheduledNewAppointment selectedTabInitial={0} />
                 </div>
             </div>
             <HorizontalTabs />
