1b81099cd72e2c82cb8fbc5a6eec35a266e822c4
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-15 13:12:04 -0300

    refactor: adiciona campo de agendamento de documentos e modal de contexto

--- source/pages/_layouts/dashboard/dashboard.tsx
+++ source/pages/_layouts/dashboard/dashboard.tsx
@@ -3,6 +3,8 @@
 import type React from 'react'
 
 import HeaderTitle from '~/Components/atoms/header-title'
+import FieldDocumentAppointment from '~/Components/molecules/field-document-appointment'
+import ContextModalPlus from '~/hooks/use-plus-modal'
 
 type DashboardLayoutsProps = {
     children: React.ReactNode
@@ -16,7 +18,13 @@ const DashboardLayouts = ({
     return (
         <main className="min-h-screen">
             <HeaderTitle title={title} />
+            <div className="flex justify-end items-center">
+                <div className="flex-row flex items-center justify-end ">
+                    <FieldDocumentAppointment />
+                </div>
+            </div>
             {children}
+            <ContextModalPlus />
         </main>
     )
 }
