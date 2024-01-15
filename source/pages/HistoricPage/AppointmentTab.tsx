b2cd36356fb7a4987d43eabcd8b03d7d91ae31c2
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-15 13:12:22 -0300

    refactor: removido importações desnecessárias de FieldDocumentAppointment e ContextModalPlus

--- source/pages/HistoricPage/AppointmentTab.tsx
+++ source/pages/HistoricPage/AppointmentTab.tsx
@@ -1,19 +1,11 @@
-import FieldDocumentAppointment from '~/Components/molecules/field-document-appointment'
 import DefaultLayout from '../_layouts/dashboard/dashboard'
 
-import ContextModalPlus from '~/hooks/use-plus-modal'
 import HorizontalTabs from './components/organisms/templates/Horizontal-List'
 
 const AppointmentsTabs = <T,>() => {
     return (
         <DefaultLayout title="Dashboard">
-            <div className="flex justify-end items-center">
-                <div className="w-1/2 flex-row flex items-center ">
-                    <FieldDocumentAppointment />
-                </div>
-            </div>
             <HorizontalTabs />
-            <ContextModalPlus />
         </DefaultLayout>
     )
 }
