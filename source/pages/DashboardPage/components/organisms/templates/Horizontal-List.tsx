300f6b6432f3885e665d67fbba4aff91afed5e49
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-15 16:06:23 -0300

    refactor: adiciona campo de busca e agendamento de documentos

--- source/pages/DashboardPage/components/organisms/templates/Horizontal-List.tsx
+++ source/pages/DashboardPage/components/organisms/templates/Horizontal-List.tsx
@@ -4,6 +4,8 @@
 import { Tab } from '@headlessui/react'
 import cn from 'classnames'
 import { useEffect, useState } from 'react'
+import FieldDocumentAppointment from '~/Components/molecules/field-document-appointment'
+import SearchInput from '~/Components/molecules/search-input'
 import useResizeMobile from '~/hooks/use-resize-mobile'
 import StepScheduledAll from '../steps/step-scheduled-all'
 
@@ -30,41 +32,45 @@ const HorizontalTabs = () => {
     }, [isMobile])
 
     return (
-        <div className="w-full mt-2">
-            <Tab.Group>
-                <Tab.List className="flex  rounded-sm bg-transparent p-1">
-                    {categories.map((category) => (
-                        <Tab
-                            key={category.id}
-                            className={({ selected }) =>
-                                cn(
-                                    'w-full rounded-sm py-2.5 text-sm leading-5 font-bold text-white uppercase',
-                                    'ring-white/60 ring-offset-2 focus:outline-none focus:ring-2 hover:cursor-default',
-                                    selected
-                                        ? 'bg-white !text-gray-500 shadow'
-                                        : 'text-blue-100 hover:bg-white/[0.12]  hover:text-white',
-                                )
-                            }
-                        >
-                            {category.title}
-                        </Tab>
-                    ))}
-                </Tab.List>
-                <Tab.Panels className="mt-2">
-                    {categories.map((item) => (
-                        <Tab.Panel
-                            key={item.id}
-                            className={cn(
-                                'rounded-xl bg-white p-3',
-                                'ring-white/60 ring-offset-2 focus:outline-none focus:ring-2',
-                            )}
-                        >
-                            {item.tab}
-                        </Tab.Panel>
-                    ))}
-                </Tab.Panels>
-            </Tab.Group>
-        </div>
+        <Tab.Group>
+            <Tab.List className="flex rounded-sm bg-transparent items-center justify-center">
+                {categories.map((category) => (
+                    <Tab
+                        key={category.id}
+                        className={({ selected }) =>
+                            cn(
+                                'w-full rounded-md py-1 text-sm leading-5 font-bold text-white uppercase h-10',
+                                'hover:cursor-default',
+                                selected
+                                    ? 'bg-transparent !text-gray-500'
+                                    : 'text-blue-100 hover:bg-white/[0.12]  hover:text-white',
+                            )
+                        }
+                    >
+                        {category.title}
+                    </Tab>
+                ))}
+            </Tab.List>
+            <div className="mb-2 w-full mobile:my-2 flex h-fit justify-end items-center">
+                <div className="h-fit w-full">
+                    <SearchInput placeholder="Digite sua busca" />
+                </div>
+                <FieldDocumentAppointment />
+            </div>
+            <Tab.Panels>
+                {categories.map((item) => (
+                    <Tab.Panel
+                        key={item.id}
+                        className={cn(
+                            'rounded-xl bg-white p-3',
+                            'ring-white/60 ring-offset-2 focus:outline-none focus:ring-2',
+                        )}
+                    >
+                        {item.tab}
+                    </Tab.Panel>
+                ))}
+            </Tab.Panels>
+        </Tab.Group>
     )
 }
 
