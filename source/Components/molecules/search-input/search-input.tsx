9c8e2c746474c21fc39398e9ba62957afc38f9c1
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-16 01:06:32 -0300

    feat: adiciona funcionalidade de pesquisa com ícone de fechar

--- source/Components/molecules/search-input/search-input.tsx
+++ source/Components/molecules/search-input/search-input.tsx
@@ -1,4 +1,6 @@
+import { XMarkIcon } from '@heroicons/react/24/solid'
 import { FaSearch } from 'react-icons/fa'
+import { create } from 'zustand'
 import Input from '~/Components/atoms/input'
 
 type SearchInputProps = {
@@ -6,13 +8,42 @@ type SearchInputProps = {
     placeholder?: string
 } & React.HTMLAttributes<HTMLInputElement>
 
+type IHookUseSearch = {
+    search: string
+    onChangeSearch: (search: string) => void
+}
+
+export const useSearch = create<IHookUseSearch>((set) => ({
+    search: '',
+    onChangeSearch: (search: string) => set({ search }),
+}))
+
 const SearchInput = ({ ...rest }: SearchInputProps) => {
+    const { onChangeSearch, search } = useSearch()
+
+    const handleChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        onChangeSearch(e.target.value)
+    }
+
     return (
         <div className="w-full border border-secondary rounded-lg relative">
             <div className="absolute h-10 left-0 flex items-center justify-center w-10">
                 <FaSearch className="text-gray-400" />
             </div>
-            <Input type="text" className="px-8 rounded-lg" {...rest} />
+            <Input
+                type="text"
+                className="px-8 rounded-lg"
+                value={search}
+                onChange={handleChangeSearch}
+                {...rest}
+            />
+            <button
+                type="button"
+                onClick={() => onChangeSearch('')}
+                className="absolute h-10 right-0 items-center justify-center w-10 "
+            >
+                <XMarkIcon className="text-red-300 w-6 h-6" />
+            </button>
         </div>
     )
 }
