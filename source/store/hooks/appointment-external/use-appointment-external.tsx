7f0f1d63b7770c85e48de05d988a7820254e4e30
murilomontino <murilomontinojr@hotmail.com>
2024-01-16 19:52:25 -0300

    feat: atualizações no hook useAppointmentExternal para lidar com modos de consulta externa

--- source/store/hooks/appointment-external/use-appointment-external.tsx
+++ source/store/hooks/appointment-external/use-appointment-external.tsx
@@ -1,28 +1,47 @@
 import { Appointments } from '~/entities/Appointments'
 import {
-    createAppointmentVet,
-    finishedAppointmentVet,
-    getAppointmentVet,
+    canceledAppointmentExternal,
+    confirmedAppointmentExternal,
+    getAppointmentExternalByID,
+    type FnAxiosAppointmentByIdExternal,
 } from '~/services/helpers'
 import type { VeterinaryConsultation } from '~/types/appointment'
 import useAppStore from '../use-app-store'
 
-export const NAME = 'appointments-vet'
+export const NAME = 'appointments-external'
+
+const ModeExternal = {
+    CONFIRMED: 'confirmed',
+    CANCELED: 'canceled',
+} as const
+
+type ModeExternal = (typeof ModeExternal)[keyof typeof ModeExternal]
 
 type IHookUseAppointmentExternal = {
     id?: string
+    mode: ModeExternal
 }
 
-const useAppointmentExternal = ({ id }: IHookUseAppointmentExternal) => {
+const strategyModeExternal = new Map<ModeExternal, FnAxiosAppointmentByIdExternal>([
+    [ModeExternal.CONFIRMED, confirmedAppointmentExternal],
+    [ModeExternal.CANCELED, canceledAppointmentExternal],
+])
+
+const useAppointmentExternal = ({ id, mode }: IHookUseAppointmentExternal) => {
     const superKeys = [NAME]
+
+    if (!mode) {
+        throw new Error('Mode is required')
+    }
+
+    const update = strategyModeExternal.get(mode as ModeExternal)
 
     return useAppStore<VeterinaryConsultation, VeterinaryConsultation>({
         keys: superKeys,
         name: NAME,
-        get: getAppointmentVet.bind(null, id as string),
+        get: getAppointmentExternalByID.bind(null, id as string),
         entity: Appointments,
-        add: createAppointmentVet,
-        update: finishedAppointmentVet,
+        update,
     })
 }
 
