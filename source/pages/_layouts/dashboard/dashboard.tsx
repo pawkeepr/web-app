347f647d407f9f8f786d680ae623e94aaa771f0a
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-16 01:23:55 -0300

    refactor: atualizações no layout do painel de controle

--- source/pages/_layouts/dashboard/dashboard.tsx
+++ source/pages/_layouts/dashboard/dashboard.tsx
@@ -6,21 +6,31 @@ import HeaderTitle from '~/Components/atoms/header-title'
 import BlockSearchAndInputDocument from '~/Components/molecules/block-search-and-input-document'
 import ContextModalPlus from '~/hooks/use-plus-modal'
 
+type DashboardLayoutsSearch =
+    | {
+          searchBlock: true
+          name: 'appointments' | 'veterinary' | 'tutor' | 'pet' | 'historic'
+      }
+    | {
+          searchBlock?: false
+          name?: never
+      }
+
 type DashboardLayoutsProps = {
     children: React.ReactNode
     title?: string
-    searchBlock?: boolean
-}
+} & DashboardLayoutsSearch
 
 const DashboardLayouts = ({
     children,
     title = 'Dashboard',
     searchBlock = true,
+    name,
 }: DashboardLayoutsProps) => {
     return (
         <main className="min-h-screen">
             <HeaderTitle title={title} />
-            {searchBlock && <BlockSearchAndInputDocument />}
+            {searchBlock && <BlockSearchAndInputDocument name={name as string} />}
             {children}
             <ContextModalPlus />
         </main>
