f90d09f00ed053023f807ad737bab310c1407185
murilomontino <murilomontinojr@hotmail.com>
2024-01-17 17:15:04 -0300

    refactor: atualiza estilos de input para adicionar transição de sombra, remover borda ao focar e adicionar contorno primário.

--- source/Components/atoms/input/input.tsx
+++ source/Components/atoms/input/input.tsx
@@ -6,16 +6,17 @@ export const input = tv({
     base: `
         w-full px-4 py-2 rounded-sm
         transition-all duration-100 ease-in-out
-        focus:outline-none transition-shadow
+        transition-shadow 
         disabled:opacity-70 text-gray-600 disabled:bg-gray-200
         text-sm h-10 invalid:!shadow-outline-secondary-500 invalid:!text-secondary-500
-        focus:ring-2 focus:ring-primary-500
         font-sans dark:bg-[#292e33] dark:text-gray-200 
         invalid:border-secondary-500 border invalid:focus:border-gray-300
+        focus:!border-none 
+        outline-primary
     `,
     variants: {
         required: {
-            true: '!border-secondary-500 focus:!border-none',
+            true: '!border-secondary-500',
         },
         center: {
             true: 'text-center',
