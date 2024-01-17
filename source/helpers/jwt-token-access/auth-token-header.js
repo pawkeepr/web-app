07d42a81a29a828ed61d12eb0c8137194f2bbe00
murilomontino <murilomontinojr@hotmail.com>
2024-01-17 00:06:06 -0300

    removidas funções e arquivos não utilizados

--- source/helpers/jwt-token-access/auth-token-header.js
+++
@@ -1,8 +0,0 @@
-export default function authHeader() {
-    const obj = JSON.parse(sessionStorage.getItem('authUser'))
-
-    if (obj?.accessToken) {
-        return { Authorization: obj.accessToken }
-    }
-    return {}
-}
