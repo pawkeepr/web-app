6132240fd7cdb1f84177fae34986ebac164f6d41
murilomontino <murilomontinojr@hotmail.com>
2024-01-26 15:07:32 -0300

    refactor: atualizar componente FieldControlSelect

--- source/Components/molecules/field-control/field-control-select.tsx
+++ source/Components/molecules/field-control/field-control-select.tsx
@@ -1,21 +1,24 @@
-import { useCallback, useEffect } from "react";
-import type { Props } from "react-select";
-import Label from "~/Components/atoms/label";
-import Select from "~/Components/atoms/select";
-import useFormikContextSafe from "~/hooks/use-formik-context-safe";
-import type { ObjPaths } from "~/types/helpers";
-import type { InputControlProps, OptionSelect } from "./types";
+import { useCallback, useEffect } from 'react'
+import type { Props } from 'react-select'
+import Label from '~/Components/atoms/label'
+import Select from '~/Components/atoms/select'
+import useFormikContextSafe from '~/hooks/use-formik-context-safe'
+import type { ObjPaths } from '~/types/helpers'
+import type { InputControlProps, OptionSelect } from './types'
 
-type FieldSelectControl<Ctx> = Omit<InputControlProps<Props, Ctx>, "value"> & {
-    name: string;
-    deps?: unknown[];
-    onChangeValue?: (item: unknown) => void;
-    options?: OptionSelect[];
-    value?: OptionSelect | null;
-    required?: boolean;
-    label: string;
-    divClassName?: string;
-};
+type FieldSelectControl<Ctx> = Omit<
+    InputControlProps<Props, Ctx>,
+    'value' | 'disabled'
+> & {
+    name: string
+    deps?: unknown[]
+    onChangeValue?: (item: unknown) => void
+    options?: OptionSelect[]
+    value?: OptionSelect | null
+    required?: boolean
+    label: string
+    divClassName?: string
+}
 
 const FieldControlSelect = <Ctx,>({
     label,
@@ -26,47 +29,41 @@ const FieldControlSelect = <Ctx,>({
     onChangeValue = () => {},
     ...props
 }: FieldSelectControl<Ctx>) => {
-    const { values: defaultValues, setFieldValue } =
-        useFormikContextSafe<Ctx>();
-    const values = defaultValues as Ctx & { [key: string]: unknown };
+    const { values: defaultValues, setFieldValue } = useFormikContextSafe<Ctx>()
+    const values = defaultValues as Ctx & { [key: string]: unknown }
 
     useEffect(() => {
-        const item = options.find((option) => option.value === values[name]);
+        const item = options.find((option) => option.value === values[name])
 
-        if (!item) return;
+        if (!item) return
 
-        setFieldValue(name, item);
-    }, [values[name]]);
+        setFieldValue(name, item)
+    }, [values[name]])
 
     const onChange = useCallback(
         (option: unknown | OptionSelect) => {
-            onChangeValue?.(option);
-            setFieldValue(name as ObjPaths<Ctx>, option, true);
+            onChangeValue?.(option)
+            setFieldValue(name as ObjPaths<Ctx>, option, true)
         },
         // eslint-disable-next-line react-hooks/exhaustive-deps
-        [setFieldValue]
-    );
+        [setFieldValue],
+    )
 
     return (
         <div className={divClassName}>
-            <Label
-                label={label}
-                required={required}
-                id={name}
-                separator={":"}
-            />
+            <Label label={label} required={required} id={name} separator={':'} />
             <Select
                 {...props}
                 id={name}
                 required={required}
-                className={required ? "border-secondary-500" : ""}
+                className={required ? 'border-secondary-500' : ''}
                 options={options}
                 name={name}
                 onChange={onChange}
                 value={props.value ?? values?.[name]}
             />
         </div>
-    );
-};
+    )
+}
 
-export default FieldControlSelect;
+export default FieldControlSelect
