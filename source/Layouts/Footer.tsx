5d507a1d86acf0993e9f9ce0badad952136948b2
murilomontino <murilomontinojr@hotmail.com>
2024-01-26 15:28:35 -0300

    refactor: atualiza rodapé com links para Política de Privacidade e Termos de Uso

--- source/Layouts/Footer.tsx
+++ source/Layouts/Footer.tsx
@@ -10,15 +10,29 @@ const Footer = () => {
         <footer className="bg-transparent w-full my-2 mobile:hidden">
             <div className="flex items-center justify-between px-6 flex-row mobile:flex-col">
                 <div>
-                    <p className="text-sm text-gray-600 font-semibold">
+                    <p className="text-xs text-gray-600 font-semibold">
                         {new Date().getFullYear()} © PawKeepr Sistema Inteligente
                         para Pets Inova Simples LTDA. 49.173.821/0001-19
                     </p>
-                    <p className="text-sm mt-2 text-gray-600 font-semibold">
+                    <p className="text-xs mt-2 text-gray-600 font-semibold">
                         Rua Riachuelo, 1200, São José, 49015-160 Aracaju.
                     </p>
                 </div>
-
+                <div className="gap-2 p-2 h-full">
+                    <Link
+                        href="/privacy-policy"
+                        className="text-xs text-gray-600 font-semibold mx-1"
+                    >
+                        Política de Privacidade
+                    </Link>
+                    {' - '}
+                    <Link
+                        href="/service-terms"
+                        className="text-xs text-gray-600 font-semibold mx-1"
+                    >
+                        Termos de Uso
+                    </Link>
+                </div>
                 <div className="flex items-center">
                     <Link
                         href="#"
