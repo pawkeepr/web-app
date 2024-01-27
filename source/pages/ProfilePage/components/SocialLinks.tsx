517d56e3992ce6c59e38f37deb76f6f81517cdf4
murilomontino <murilomontinojr@hotmail.com>
2024-01-27 14:44:03 -0300

    refactor: atualizações nos links sociais da página de perfil

--- source/pages/ProfilePage/components/SocialLinks.tsx
+++ source/pages/ProfilePage/components/SocialLinks.tsx
@@ -1,19 +1,23 @@
 // path/filename: src/components/SocialLinks.js
 
 import {
-    FaEnvelope,
-    FaFacebook,
-    FaInstagram,
-    FaTiktok,
+    // FaEnvelope,
+    // FaFacebook,
+    // FaInstagram,
+    // FaTiktok,
     FaWhatsapp,
 } from 'react-icons/fa'
-import { Input } from 'reactstrap'
+import FieldControl from '~/Components/molecules/field-control'
 import CardContainer from './CardContainer'
 
-const SocialLinks = () => {
+type SocialLinksProps = {
+    whatsapp?: string
+}
+
+const SocialLinks = ({ whatsapp }: SocialLinksProps) => {
     return (
         <CardContainer>
-            <div className="flex items-center mb-3">
+            {/* <div className="flex items-center mb-3">
                 <FaFacebook className="text-blue-600 w-6 h-6 mr-3" />
                 <Input
                     type="text"
@@ -32,18 +36,17 @@ const SocialLinks = () => {
                     placeholder="Email Address"
                     defaultValue="dave@example.com"
                 />
-            </div>
+            </div> */}
             <div className="flex items-center mb-3">
-                <FaWhatsapp className="text-green-500 w-6 h-6 mr-3" />
-                <Input
-                    type="text"
-                    className="form-control"
-                    id="whatsappNumber"
-                    placeholder="WhatsApp Number"
-                    defaultValue="+1234567890"
+                <FieldControl
+                    startIcon={<FaWhatsapp className="text-green-500 w-5 h-5" />}
+                    ctx={{ whatsapp: '' }}
+                    name="whatsapp"
+                    mode="readonly"
+                    value={whatsapp || 'Não Informado'}
                 />
             </div>
-            <div className="flex items-center mb-3">
+            {/* <div className="flex items-center mb-3">
                 <FaInstagram className="text-pink-500 w-6 h-6 mr-3" />
                 <Input
                     type="text"
@@ -62,7 +65,7 @@ const SocialLinks = () => {
                     placeholder="TikTok Username"
                     defaultValue="@daveadame"
                 />
-            </div>
+            </div> */}
         </CardContainer>
     )
 }
