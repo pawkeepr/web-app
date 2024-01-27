f3c550ff01d4c1e9f0153040f43dc080eec2c8e8
murilomontino <murilomontinojr@hotmail.com>
2024-01-27 19:02:13 -0300

    feat: adicionado arquivo use-species.tsx para seleção de espécies

---
+++ source/Components/organism/selects/use-species.tsx
@@ -0,0 +1,21 @@
+import { create } from 'zustand'
+import { species, type SpeciesType } from '~/types/speciesType'
+
+type State = {
+    specie: SpeciesType
+    optionsSpecies: SpeciesType[]
+    onChangeSpecie: (species: SpeciesType) => void
+}
+
+export const useSpecies = create<State>((set) => ({
+    specie: {
+        label: 'Desconhecido',
+        value: 'unknown',
+        breedType: [],
+        bloodType: [],
+    },
+    optionsSpecies: species,
+    onChangeSpecie: (species: SpeciesType) => {
+        set((state) => ({ ...state, specie: species }))
+    },
+}))
