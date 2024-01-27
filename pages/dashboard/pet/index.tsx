18394fa82a409f05236b8458fd9a48515ff67a2c
murilomontino <murilomontinojr@hotmail.com>
2024-01-27 19:02:29 -0300

    refactor: adiciona hook useEffect para alterar o modo de edição do pet

--- pages/dashboard/pet/index.tsx
+++ pages/dashboard/pet/index.tsx
@@ -1,12 +1,19 @@
 import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
 import LayoutMain from '~/Layouts'
 import getServerSidePropsPagesPrivates from '~/helpers/get-server-side-props-pages-privates'
 import Page from '~/pages/NewPetPage'
+import { useModeEditablePet } from '~/pages/NewPetPage/use-zustand-hook'
 
 const PetsPage = () => {
     const search = useSearchParams()
     const document = search.get('document') || ''
     const { push } = useRouter()
+    const { onChangeMode } = useModeEditablePet()
+
+    useEffect(() => {
+        onChangeMode('editable')
+    }, [])
 
     if (!document) return push('/dashboard')
 
