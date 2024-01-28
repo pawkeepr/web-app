66adc14abf7bddc186de75825229a46be9d5f916
murilomontino <murilomontinojr@hotmail.com>
2024-01-28 11:37:09 -0300

    refactor: adição de modo editable e readonly em select

--- source/Components/atoms/select/select.tsx
+++ source/Components/atoms/select/select.tsx
@@ -1,8 +1,11 @@
-import ReactSelect, { Props } from 'react-select'
+import ReactSelect, { type Props } from 'react-select'
+import { tv } from 'tailwind-variants'
+import type { ModeInput } from '~/Components/molecules/field-control/field-control'
 
-import cn from 'classnames'
-
-export type SelectProps = Props
+export type SelectProps = Props & {
+    isSearchable?: boolean
+    mode?: ModeInput
+}
 
 export const colorStyles = {
     multiValue: (styles: any, { data }: any) => {
@@ -14,12 +17,52 @@ export const colorStyles = {
     },
 }
 
-const Select = ({ isSearchable = false, ...props }: Props) => {
+const control = tv({
+    base: 'focus:!outline-none transition-shadow h-10 px-2 w-full focus:!border-0 dark:bg-[#292e33] dark:text-gray-200',
+    variants: {
+        required: {
+            true: '!border-secondary-500 !border ',
+            false: '!border !border-gray-300',
+        },
+        mode: {
+            editable: '',
+            readonly:
+                'bg-transparent !border-none !pointer-events-none !focus:outline-none',
+        },
+    },
+})
+
+const dropdownIndicator = tv({
+    base: 'text-gray-400',
+    variants: {
+        mode: {
+            editable: '',
+            readonly: '!hidden',
+            view: '!hidden',
+        },
+    },
+})
+
+const option = tv({
+    base: 'py-2 hover:!bg-secondary-500 hover:text-neutral hover:cursor-pointer uppercase dark:bg-[#292e33] dark:text-gray-200 ',
+    variants: {
+        selected: {
+            true: '!bg-primary-500',
+            false: '',
+        },
+    },
+})
+
+const Select = ({
+    isSearchable = true,
+    mode = 'readonly',
+    ...props
+}: SelectProps) => {
     return (
         <ReactSelect
             placeholder={
                 <div className="text-gray-400">
-                    {props.placeholder || 'Clique aqui ...'}
+                    {props.placeholder || 'Selecione um opção'}
                 </div>
             }
             delimiter=","
@@ -36,22 +79,10 @@ const Select = ({ isSearchable = false, ...props }: Props) => {
             menuPosition="fixed"
             classNames={{
                 noOptionsMessage: () => 'Não há opções',
-                control: (state) =>
-                    cn(
-                        'focus:!outline-none transition-shadow h-10 px-2 w-full focus:!border-0 dark:bg-[#292e33] dark:text-gray-200 ',
-                        {
-                            '!border-secondary-500 !border ': props.required,
-                            '!border !border-gray-300': !props.required,
-                        },
-                    ),
+                control: () => control({ required: props.required, mode }),
                 indicatorSeparator: () => '!hidden',
-                option: (state) =>
-                    cn(
-                        'py-2 hover:!bg-secondary-500 hover:text-neutral hover:cursor-pointer uppercase dark:bg-[#292e33] dark:text-gray-200 ',
-                        {
-                            '!bg-primary-500': state.isSelected,
-                        },
-                    ),
+                option: (state) => option({ selected: state.isSelected }),
+                dropdownIndicator: () => dropdownIndicator({ mode }),
             }}
             {...props}
         />
