9cc9eb194dd9832cf7cd15eaae10dc86953adc29
murilomontino <murilomontinojr@hotmail.com>
2024-01-28 13:14:44 -0300

    refactor: atualizar componente ControlSwitchDiv com estilos do Tailwind CSS

--- source/Components/molecules/control-switch-div/switch.tsx
+++ source/Components/molecules/control-switch-div/switch.tsx
@@ -1,27 +1,66 @@
 import { Switch } from '@headlessui/react'
 import { useField } from 'formik'
+import { tv, type VariantProps } from 'tailwind-variants'
+import type { ObjPaths } from '~/types/helpers'
 
-type SwitchProps = {
+type SwitchProps<Ctx = undefined> = {
     className?: string
-    name: string
+    ctx?: Ctx extends undefined ? never : Ctx
+    name: Ctx extends undefined ? string : ObjPaths<Ctx>
     children?: React.ReactNode
     label: string
     onClick?: () => void
+    divClassName?: string
 }
 
-const ControlSwitch = ({
+const controlSwitch = tv({
+    base: `
+    relative inline-flex h-full w-full shrink-0 
+    cursor-pointer rounded-full border-2 border-transparent 
+    transition-colors duration-200 ease-in-out focus:outline-none 
+    focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75
+    `,
+    variants: {
+        checked: {
+            true: 'bg-opacity-100',
+            false: 'bg-opacity-50',
+        },
+        primary: {
+            true: 'bg-primary-500',
+            false: 'bg-secondary-400',
+        },
+        secondary: {
+            true: 'bg-secondary-500',
+            false: 'bg-primary-400',
+        },
+    },
+    defaultVariants: {
+        checked: false,
+        primary: false,
+        secondary: true,
+    },
+})
+
+const divSwitch = tv({
+    base: 'mb-2',
+})
+
+type ControlSwitchTailwind = VariantProps<typeof controlSwitch>
+
+const ControlSwitch = <Ctx,>({
     className,
     children,
     label,
     name,
+    divClassName,
     onClick,
-}: SwitchProps) => {
+}: SwitchProps<Ctx> & ControlSwitchTailwind) => {
     const [field, meta, helpers] = useField(name)
 
     return (
-        <div className="mb-2">
+        <div className={divSwitch({ className: divClassName })}>
             <div className="flex justify-between items-center gap-2 mb-2">
-                <span className="font-bold">{label}</span>
+                <span className="font-semibold text-gray-600">{label}</span>
                 <div className="w-16 max-h-max">
                     <Switch
                         onClick={onClick}
@@ -29,10 +68,10 @@ const ControlSwitch = ({
                         onChange={(e) => {
                             helpers.setValue(e)
                         }}
-                        className={`${
-                            field.value ? 'bg-secondary-500' : '  bg-secondary-600'
-                        }
-            relative inline-flex h-full w-full shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+                        className={controlSwitch({
+                            checked: field.value as boolean,
+                            className,
+                        })}
                     >
                         <span className="sr-only">Use setting</span>
                         <span
