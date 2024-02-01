56d5a91084080d6251e6d0490be8d10e854d8259
Jainefranciellem <jainefranciellen@gmail.com>
2024-02-01 18:58:35 -0300

    page tutor: configura endpoints para listagem de vets e pets do  tutor

--- source/pages/Modules/tutor/PetsAndVets/components/organisms/VetsTab.tsx
+++ source/pages/Modules/tutor/PetsAndVets/components/organisms/VetsTab.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import CardVets from "~/Components/organism/card-tutors";
 
 import ListTab from "~/Components/templates/ListTab";
-import useListVets from "~/store/hooks/list-tutors-by-clinic/use-list-tutors";
+import useListVets from "~/store/hooks/list-vets-by-tutor/use-list-vets";
+import useProfile from "~/store/hooks/profile/use-profile";
 import type { IMainResponsibleGuardian } from "~/types/pet-v2";
 
 const VetsTab = () => {
-    const { data: vets } = useListVets();
+    const { data } = useProfile();
+    const { vets } = useListVets(data?.user_information.cpf_cnpj);
 
     const cards = (vets: IMainResponsibleGuardian[]) =>
         vets?.map((vet) => <CardVets key={vet.cpf_cnpj} vet={vet} />);
