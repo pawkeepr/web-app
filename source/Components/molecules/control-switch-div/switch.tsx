ec6c76c70c1d999946b7d4be66584709afe9781c
murilomontino <murilomontinojr@hotmail.com>
2024-04-02 15:16:50 -0300

    feat: switch com três estados.

--- source/Components/molecules/control-switch-div/switch.tsx
+++ source/Components/molecules/control-switch-div/switch.tsx
@@ -1,6 +1,7 @@
 import { Switch } from '@headlessui/react'
 import cn from 'classnames'
 import { useField } from 'formik'
+import { type MouseEvent, useEffect } from 'react'
 import { type VariantProps, tv } from 'tailwind-variants'
 import type { ObjPaths } from '~/types/helpers'
 import { ModeInput } from '../field-control/field-control'
@@ -12,6 +13,7 @@ type SwitchProps<Ctx = undefined> = {
     children?: React.ReactNode
     label: string
     onClick?: () => void
+    initialValue?: boolean | null
     onChange?: (e: boolean) => void
     divClassName?: string
     legend?: boolean
@@ -33,10 +35,12 @@ const controlSwitch = tv({
         primary: {
             true: 'bg-primary-500',
             false: 'bg-secondary-400',
+            null: 'bg-gray-400',
         },
         secondary: {
             true: 'bg-secondary-500',
             false: 'bg-primary-400',
+            null: 'bg-gray-400',
         },
         mode: {
             [ModeInput.readonly]: 'hidden',
@@ -47,6 +51,21 @@ const controlSwitch = tv({
         checked: false,
         primary: false,
         secondary: true,
+    },
+})
+
+const pointerSwitch = tv({
+    base: `
+        pointer-events-none inline-block lg:h-[24px] 
+        lg:w-[24px] h-[20px] w-[20px] transform rounded-full 
+        bg-white shadow-lg ring-0 transition duration-200 ease-in-out
+    `,
+    variants: {
+        translateClass: {
+            true: 'translate-x-9',
+            false: 'translate-x-0',
+            null: 'translate-x-[1.2rem]',
+        },
     },
 })
 
@@ -63,16 +82,33 @@ const ControlSwitch = <Ctx,>({
     name,
     divClassName,
     onClick,
+    initialValue = false,
     legend = true,
     mode = ModeInput.editable,
     onChange = () => {},
 }: SwitchProps<Ctx> & ControlSwitchTailwind) => {
-    const [field, meta, helpers] = useField(name)
+    const [field, _meta, helpers] = useField(name)
+
+    useEffect(() => {
+        helpers.setValue(initialValue)
+    }, [])
 
     const handleChange = (e: boolean) => {
         helpers.setValue(e)
-        onChange?.(e)
+        onChange(e)
     }
+
+    const handleClick = (
+        event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>,
+    ) => {
+        const switchRect = event.currentTarget.getBoundingClientRect()
+        const clickPosition = event.clientX - switchRect.left
+        const newValue = clickPosition > switchRect.width / 2
+        handleChange(newValue)
+        onClick?.()
+    }
+
+    const title = field.value === null ? 'Ind.' : field.value ? 'Sim' : 'Não'
 
     return (
         <div className={divSwitch({ className: divClassName })}>
@@ -82,14 +118,15 @@ const ControlSwitch = <Ctx,>({
                     <div className="w-16 max-h-max relative">
                         <Switch
                             disabled={mode === ModeInput.readonly}
-                            onClick={onClick}
-                            checked={field.value}
-                            onChange={handleChange}
+                            onClick={(e) => handleClick(e)}
+                            checked={field.value === true}
                             className={controlSwitch({
                                 mode,
                                 checked: field.value as boolean,
-                                primary: field.value as boolean,
-                                secondary: !field.value as boolean,
+                                primary:
+                                    field.value === null ? 'null' : field.value,
+                                secondary:
+                                    field.value === null ? 'null' : !field.value,
                                 className,
                             })}
                         >
@@ -97,18 +134,15 @@ const ControlSwitch = <Ctx,>({
                             {/* Adjusted the translate class to center the circle */}
                             <span
                                 aria-hidden="true"
-                                className={`${
-                                    field.value ? 'translate-x-9' : 'translate-x-0'
-                                } pointer-events-none inline-block lg:h-[24px] lg:w-[24px] h-[20px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+                                className={pointerSwitch({
+                                    translateClass:
+                                        field.value === null ? 'null' : field.value,
+                                })}
                             />
                             {/* Text inside the switch */}
                         </Switch>
                     </div>
-                    {legend && (
-                        <span className={cn('text-gray-400')}>
-                            {field.value ? 'Sim' : 'Não'}
-                        </span>
-                    )}
+                    {legend && <span className={cn('text-gray-400')}>{title}</span>}
                 </div>
             </div>
             {field.value && children}
