937c60de634b278faeb8a66554aa761a0790e899
murilomontino <murilomontinojr@hotmail.com>
2024-04-04 09:00:34 -0300

    feat: adiciona hook de navegação por teclado ao componente Modal

--- source/Components/organism/modal/index.tsx
+++ source/Components/organism/modal/index.tsx
@@ -6,23 +6,29 @@ import XMarkIcon from '@heroicons/react/24/solid/XMarkIcon'
 
 import { twMerge } from 'tailwind-merge'
 import { BtnIcon } from '~/Components/atoms/btn'
+import useKeyboardNavigation from '~/hooks/use-keyboard-navigation'
 
 const Modal = ({
     className,
     ...props
 }: JSX.IntrinsicAttributes &
     PopupProps &
-    RefAttributes<PopupActions> & { maxHeight?: number; width?: number }) => (
-    <Popup
-        position="bottom center"
-        className="mt-2"
-        overlayStyle={{ background: 'rgba(0,0,0,0.6)' }}
-        closeOnEscape={false}
-        {...props}
-    >
-        <div
-            className={twMerge(
-                `
+    RefAttributes<PopupActions> & { maxHeight?: number; width?: number }) => {
+    useKeyboardNavigation({
+        Escape: () => props.onClose?.(),
+    })
+
+    return (
+        <Popup
+            position="bottom center"
+            className="mt-2"
+            overlayStyle={{ background: 'rgba(0,0,0,0.6)' }}
+            closeOnEscape={false}
+            {...props}
+        >
+            <div
+                className={twMerge(
+                    `
                 relative
                 z-50
                 popup-content
@@ -32,26 +38,27 @@ const Modal = ({
 	            overflow-visible  py-10 bg-gray-100 dark:bg-dark-500
 	            shadow-2xl rounded-md
                 `,
-                className,
-            )}
-        >
-            <div className="h-4 p-3 bg-primary absolute top-0 w-full right-0" />
+                    className,
+                )}
+            >
+                <div className="h-4 p-3 bg-primary absolute top-0 w-full right-0" />
 
-            <BtnIcon
-                type="button"
-                icon={
-                    <XMarkIcon className="w-8 h-8 text-gray-500 hover:!text-red-600 font-extrabold" />
-                }
-                className="absolute top-1 right-0 w-fit p-0 ! py-0 h-fit "
-                onClick={() => props.onClose?.()}
-                aria-label="Close modal"
-            />
+                <BtnIcon
+                    type="button"
+                    icon={
+                        <XMarkIcon className="w-8 h-8 text-gray-500 hover:!text-red-600 font-extrabold" />
+                    }
+                    className="absolute top-1 right-0 w-fit p-0 ! py-0 h-fit "
+                    onClick={() => props.onClose?.()}
+                    aria-label="Close modal"
+                />
 
-            <section className="mt-3 relative overflow-auto px-4 ">
-                {props.children}
-            </section>
-        </div>
-    </Popup>
-)
+                <section className="mt-3 relative overflow-auto px-4 ">
+                    {props.children}
+                </section>
+            </div>
+        </Popup>
+    )
+}
 
 export default Modal
