893461a644bf8e5f5849cfbc9fa31622354588ea
murilomontino <murilomontinojr@hotmail.com>
2024-04-08 12:35:24 -0300

    fix: atualiza componente VetsTab

--- source/pages/Modules/tutor/PetsAndVets/components/organisms/VetsTab.tsx
+++ source/pages/Modules/tutor/PetsAndVets/components/organisms/VetsTab.tsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import CardVets from '~/Components/organism/card-tutors'
 
-import ListTab from '~/Components/templates/ListTab'
 import useListVets from '~/store/hooks/list-vets-by-tutor/use-list-vets'
 import useProfile from '~/store/hooks/profile/use-profile'
-import type { IMainResponsibleGuardian } from '~/types/pet-v2'
 
 const VetsTab = () => {
     const { data } = useProfile()
-    const { vets } = useListVets(data?.user_information?.cpf_cnpj)
-
-    const cards = (vets: IMainResponsibleGuardian[]) =>
-        vets?.map((vet) => <CardVets key={vet.cpf_cnpj} vet={vet} />)
+    const { activeData: vets } = useListVets(data?.user_information?.cpf_cnpj as string)
 
     return (
         <React.Fragment>
-            <ListTab cards={cards} items={vets || []} />
+            {
+                vets?.map((vet) => <CardVets key={vet.cpf_cnpj} tutor={data} />)
+            }
         </React.Fragment>
     )
 }
