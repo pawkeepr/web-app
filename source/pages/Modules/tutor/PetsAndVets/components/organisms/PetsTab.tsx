c90830021d864fe0bca90692614983efe14f5c3c
murilomontino <murilomontinojr@hotmail.com>
2024-04-08 12:36:25 -0300

    fix: listagem de pets.

--- source/pages/Modules/tutor/PetsAndVets/components/organisms/PetsTab.tsx
+++ source/pages/Modules/tutor/PetsAndVets/components/organisms/PetsTab.tsx
@@ -1,28 +1,21 @@
-import { RadioGroup } from "@headlessui/react";
-import CardPets from "~/Components/organism/card-pets";
-import ListTab from "~/Components/templates/ListTab";
-import useListPets from "~/store/hooks/list-appointments-by-pet/use-list-appointments-by-pet";
-import type { IPetV2Data } from "~/types/pet-v2";
+import { RadioGroup } from '@headlessui/react'
+import CardPets from '~/Components/organism/card-pets'
+import useListPets from '~/store/hooks/list-appointments-by-pet/use-list-appointments-by-pet'
 
 const PetsTab = () => {
-    const {
-        data: pets,
-        isLoading,
-        isError,
-    } = useListPets({ mode: "scheduled" });
+    const { activeData: pets, isLoading, isError } = useListPets({ mode: 'scheduled' })
 
-    const cards = (pets: IPetV2Data[]) =>
-        pets?.map((pet) => <CardPets key={pet?.id_pet} pet={pet} />);
+    if (isLoading) return <div>Loading...</div>
 
-    if (isLoading) return <div>Loading...</div>;
-
-    if (isError) return <div>Error</div>;
+    if (isError) return <div>Error</div>
 
     return (
         <RadioGroup>
-            <ListTab items={pets || []} cards={cards} />
+            {pets?.map((pet) => (
+                <CardPets key={pet?.id_pet} pet={pet} />
+            ))}
         </RadioGroup>
-    );
-};
+    )
+}
 
-export default PetsTab;
+export default PetsTab
