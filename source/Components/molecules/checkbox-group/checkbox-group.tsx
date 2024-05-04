cae0fcceb819615fc2063e452fcad8eda925278a
murilomontino <murilomontinojr@hotmail.com>
2024-05-04 18:11:06 -0300

    Atualizações no componente CheckboxGroup***
    ***Descrição:*** Este commit contém atualizações no componente CheckboxGroup. Foram feitas as seguintes alterações:
    - Importação do tipo ObjPaths do módulo types/helpers.
    - Atualização dos tipos dos valores da propriedade "value" do tipo "any" para "unknown".
    - Atualização do tipo da propriedade "name" para que possa aceitar caminhos de objetos.
    - Remoção de propriedades não utilizadas.
    - Atualização do layout dos checkboxes e seus respectivos labels.
    
    ***Tradução:

--- source/Components/molecules/checkbox-group/checkbox-group.tsx
+++ source/Components/molecules/checkbox-group/checkbox-group.tsx
@@ -3,33 +3,32 @@ import { useState } from 'react'
 import Checkbox from '~/Components/atoms/checkbox'
 
 import Label from '~/Components/atoms/label'
+import type { ObjPaths } from '~/types/helpers'
 
 type Item = {
     label: string
-    value: any
+    value: unknown
 }
 
-interface CheckboxGroupProps<T> extends React.HTMLAttributes<HTMLDivElement> {
+interface CheckboxGroupProps<Ctx> extends React.HTMLAttributes<HTMLDivElement> {
     items: Item[]
-    name: string
+    ctx?: Ctx extends undefined ? never : Ctx
+    name: Ctx extends undefined ? string : ObjPaths<Ctx>
     label: string
     required?: boolean
     disabledError?: boolean
     divClassName?: string
 }
 
-export default function CheckboxGroup<T>({
+export default function CheckboxGroup<Ctx>({
     items = [],
     name,
     label,
     required,
-    className,
     id,
-    disabledError,
     divClassName,
-    ...rest
-}: CheckboxGroupProps<T>) {
-    const [field, meta, helpers] = useField(name)
+}: CheckboxGroupProps<Ctx>) {
+    const [field, _meta, helpers] = useField(name)
 
     const { setValue } = helpers
 
@@ -54,20 +53,26 @@ export default function CheckboxGroup<T>({
     return (
         <div className={divClassName}>
             <Label label={label} required={required} id={id} separator=":" />
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center gap-1 flex-wrap">
                 {items.map((item, index) => (
                     <div
-                        className="flex flex-row items-center justify-center"
-                        key={index}
+                        className="flex flex-row flex-grow  items-center justify-center"
+                        key={`${item.value}-${index}`}
                     >
-                        <Checkbox
-                            id={item.value}
-                            type="checkbox"
-                            checked={checkedValues.includes(item.value)}
-                            {...field}
-                            onChange={() => setCheckboxValue(item.value)}
+                        <div className="w-fit ">
+                            <Checkbox
+                                id={item.value}
+                                type="checkbox"
+                                checked={checkedValues.includes(item.value)}
+                                {...field}
+                                onChange={() => setCheckboxValue(item.value)}
+                            />
+                        </div>
+                        <Label
+                            className="flex-[3]"
+                            htmlFor={item.value}
+                            label={item.label}
                         />
-                        <Label htmlFor={item.value} label={item.label} />
                     </div>
                 ))}
             </div>
