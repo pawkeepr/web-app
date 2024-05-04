d421375a5e42dbf92c10dc23e5dcca8d6c61def7
murilomontino <murilomontinojr@hotmail.com>
2024-05-04 18:11:14 -0300

    feat: adiciona lista de animais de estimação em source/constants/pets.ts

---
+++ source/constants/pets.ts
@@ -0,0 +1,82 @@
+export const pets = [
+    {
+        label: 'Cachorro',
+        value: 'dog',
+        color: 'rgb(255 200 107);',
+    },
+    {
+        label: 'Gato',
+        value: 'cat',
+        color: 'rgb(255 200 107);',
+    },
+    {
+        label: 'Pássaro',
+        value: 'bird',
+        color: 'rgb(255 200 107);',
+    },
+    {
+        label: 'Peixe',
+        value: 'fish',
+        color: 'rgb(255 200 107);',
+    },
+    {
+        label: 'Roedor',
+        value: 'rodent',
+        color: 'rgb(255 200 107);',
+    },
+    {
+        label: 'Réptil',
+        value: 'reptile',
+        color: 'rgb(255 200 107);',
+    },
+    {
+        label: 'Equino',
+        value: 'equine',
+        color: 'rgb(255 200 107);',
+    },
+    {
+        label: 'Bovino',
+        value: 'bovine',
+        color: 'rgb(255 200 107);',
+    },
+    {
+        label: 'Caprino',
+        value: 'goat',
+        color: 'rgb(255 200 107);',
+    },
+    {
+        label: 'Ovino',
+        value: 'sheep',
+        color: 'rgb(255 200 107);',
+    },
+    {
+        label: 'Suíno',
+        value: 'swine',
+        color: 'rgb(255 200 107);',
+    },
+    {
+        label: 'Lagartixa',
+        value: 'gecko',
+        color: 'rgb(255 200 107);',
+    },
+    {
+        label: 'Aranha',
+        value: 'arachnid',
+        color: 'rgb(255 200 107);',
+    },
+    {
+        label: 'Inseto',
+        value: 'insect',
+        color: 'rgb(255 200 107);',
+    },
+    {
+        label: 'Anfíbio',
+        value: 'amphibian',
+        color: 'rgb(255 200 107);',
+    },
+    {
+        label: 'Primata',
+        value: 'primate',
+        color: 'rgb(255 200 107);',
+    },
+]
