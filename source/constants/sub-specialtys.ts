7a3f401a1f774d39b723eff8c3ea2ad2b523fae3
murilomontino <murilomontinojr@hotmail.com>
2024-05-04 18:11:24 -0300

    refactor: removidas especialidades de Acupuntura, Células-tronco, Cuidados paliativos, Fisioterapia e reabilitação e Internamentos. Adicionada especialidade de Clínica Geral.

--- source/constants/sub-specialtys.ts
+++ source/constants/sub-specialtys.ts
@@ -1,16 +1,11 @@
 export const sub_specialty = [
-    'Acupuntura',
     'Anestesiologia e controle da dor',
     'Cardiologia',
-    'Células-tronco',
     'Cirurgias',
-    'Cuidados paliativos',
     'Dermatologia',
     'Endocrinologia',
     'Felinos',
-    'Fisioterapia e reabilitação',
     'Gastroenterologia e Endoscopia',
-    'Internamentos',
     'Medicina Comportamental',
     'Nefrologia e Urologia',
     'Neurologia',
@@ -21,4 +16,5 @@ export const sub_specialty = [
     'Ortopedia',
     'Pneumologia',
     'Reprodução e Neonatologia',
+    'Clinica Geral',
 ]
