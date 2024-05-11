118f139c7244d6f81dd8e432ad3a854f7cb4aaed
murilomontino <murilomontinojr@hotmail.com>
2024-05-11 20:58:54 -0300

    refactor: constrói um componente auxiliar para items e nova seção adicionada ao passo final de criação de conta.

--- source/pages/Modules/veterinary/Authentication/components/steps-activation/step-finally.tsx
+++ source/pages/Modules/veterinary/Authentication/components/steps-activation/step-finally.tsx
@@ -1,25 +1,49 @@
+import cn from 'classnames'
 import { useFormikContext } from 'formik'
-import { twMerge } from 'tailwind-merge'
 import { BtnNeutral, BtnPrimary } from '~/Components/atoms/btn'
+import {
+    Pets,
+    Services,
+    Specialty,
+    SpecialtyAnimals,
+    type KeysPets,
+    type KeysServices,
+    type KeysSpecialty,
+    type KeysSpecialtyAnimals,
+} from '~/constants'
 import LOADING from '~/constants/loading'
 import { useAppSelector } from '~/store/hooks'
 import type { ActivateAccount } from '~/validations/activate'
 import type { StepProps } from './types'
 
-const listItem = 'flex gap-1 font-semibold text-gray-500 p-1 text-center w-full'
-const strongText = 'text-gray-700 mr-2'
-const pStyle = 'text-center w-full text-sm flex flex-row'
+type ItemProps = {
+    label: string
+    value: string
+    classNames?: {
+        label?: string
+        value?: string
+    }
+}
 
-const Specialty = {
-    domestics: 'Animais Domésticos',
-    large: 'Animais de Grande Porte',
-    midsize: 'Animais de Médio Porte',
-    wild: 'Animais Selvagens',
-    exotic: 'Animais Exóticos',
-    unconventional: 'Animais Não Convencionais',
-} as const
-
-type KeysSpecialty = keyof typeof Specialty
+const Item = ({ label, value, classNames }: ItemProps) => (
+    <li className="flex justify-between gap-1 p-1 text-xs font-semibold text-gray-500 mobile:w-full">
+        <div className="flex flex-row items-center !justify-between flew-wrap text-start w-full ">
+            <strong
+                className={cn(
+                    'mr-1 text-center text-gray-700 max-w-40 h-fit w-fit mobile:text-start',
+                    classNames?.label,
+                )}
+            >
+                {label}
+            </strong>
+            <span
+                className={cn('max-w-36 h-fit mobile:text-end', classNames?.value)}
+            >
+                {value}
+            </span>
+        </div>
+    </li>
+)
 
 const StepFinally = ({ prevStep }: StepProps) => {
     const { values, isValid, handleSubmit } = useFormikContext<ActivateAccount>()
@@ -37,139 +61,92 @@ const StepFinally = ({ prevStep }: StepProps) => {
 
     return (
         <div>
-            <h4 className="font-semibold font-sans text-base my-2 text-center text-primary-600">
-                Informações de Cadastro
+            <h4 className="my-2 font-sans text-base font-semibold text-center text-primary-600">
+                Informações Básicas
             </h4>
 
-            <ul className="grid grid-cols-3 mobile:grid-cols-1">
-                <li className={`${listItem}`}>
-                    <p className={pStyle}>
-                        <strong className={strongText}>Nome:</strong>
-                        <span className="text-capitalize ">
-                            {values.firstName} {values.lastName}
-                        </span>
-                    </p>
-                </li>
-                <li className={listItem}>
-                    <p className={pStyle}>
-                        <strong className={strongText}>CRMV:</strong>
-                        <span className="">{values.crmv}</span>
-                    </p>
-                </li>
-                <li className={listItem}>
-                    <p className={pStyle}>
-                        <strong className={strongText}>Documento:</strong>
-                        <span className="">{values.cpf_cnpj}</span>
-                    </p>
-                </li>
-
-                <li className={listItem}>
-                    <p className={pStyle}>
-                        <strong className={strongText}>Telefone:</strong>
-                        <span className="">{values?.contact?.phone}</span>
-                    </p>
-                </li>
-
-                <li className={listItem}>
-                    <p className={pStyle}>
-                        <strong className={strongText}>WhatsApp Comercial:</strong>
-                        <span className="">{values?.contact?.whatsapp}</span>
-                    </p>
-                </li>
+            <ul className="flex flex-wrap justify-between w-full">
+                <Item
+                    label="Nome:"
+                    value={`${values.firstName} ${values.lastName}`}
+                    classNames={{ value: '!max-w-fit' }}
+                />
+                <Item label="CRMV:" value={values.crmv} />
+                <Item label="Documento:" value={values.cpf_cnpj} />
+                <Item label="Telefone:" value={values?.contact?.phone as string} />
+                <Item
+                    label="WhatsApp Comercial:"
+                    value={values?.contact?.whatsapp as string}
+                />
             </ul>
 
-            <ul className="grid grid-cols-1">
-                <li className={twMerge(listItem, 'w-full col-span-full')}>
-                    <p className={pStyle}>
-                        <strong className={strongText}>Especialidade:</strong>
-                        <span className="">{values?.specialty?.label}</span>
-                    </p>
-                </li>
-
-                <li className={twMerge(listItem, 'w-full col-span-full')}>
-                    <p className={pStyle}>
-                        <strong className={strongText}>Serviços:</strong>
-                        <span className="">
-                            {values?.list_service_type
-                                ?.map((item: KeysSpecialty) => Specialty[item])
-                                .join(', ')}
-                        </span>
-                    </p>
-                </li>
-
-                <li className={twMerge(listItem, 'w-full col-span-full')}>
-                    <p className={pStyle}>
-                        <strong className={strongText}>Sub-Especialidade:</strong>
-                        <span className="">
-                            {values?.list_specialty
-                                ?.map((item) => item.label)
-                                .join(', ')}
-                        </span>
-                    </p>
-                </li>
+            <h4 className="my-2 font-sans text-base font-semibold text-center text-primary-600">
+                Informações de Serviço
+            </h4>
+            <ul className="flex flex-wrap justify-between w-full">
+                <Item
+                    label="Especialidade:"
+                    value={Specialty[values?.specialty as KeysSpecialty] as string}
+                />
+                <Item
+                    label="Tipos de Atendimento:"
+                    value={values?.types_service
+                        ?.map((item) => Services[item as KeysServices])
+                        ?.join(', ')}
+                />
+                <Item
+                    label="Animais Atendidos:"
+                    value={values?.list_service_type
+                        ?.map(
+                            (item) =>
+                                SpecialtyAnimals[item as KeysSpecialtyAnimals],
+                        )
+                        .join(', ')}
+                />
+                <Item
+                    label="Atendimento domiciliar:"
+                    value={
+                        values?.types_animals
+                            ?.map((item?: string) => Pets[item as KeysPets])
+                            ?.join(', ') || 'Não informado'
+                    }
+                />
+                <Item
+                    label="Outras Especialidades:"
+                    value={
+                        values?.list_specialty
+                            ?.map((item) => Specialty[item as KeysSpecialty])
+                            ?.join(', ') || 'Não informado'
+                    }
+                />
             </ul>
 
-            <h4 className="font-semibold font-sans text-base mb-2 text-center text-primary-600">
+            <h4 className="mb-2 font-sans text-base font-semibold text-center text-primary-600">
                 Endereço
             </h4>
 
-            <ul className="grid grid-cols-3 mobile:grid-cols-1">
-                <li className={listItem}>
-                    <p className={pStyle}>
-                        <strong className={strongText}>CEP:</strong>
-                        <span className="">{values?.location?.zipCode}</span>
-                    </p>
-                </li>
-                <li className={listItem}>
-                    <p className={pStyle}>
-                        <strong className={strongText}>Estado:</strong>
-                        <span className="text-capitalize ">
-                            {values?.location?.state}
-                        </span>
-                    </p>
-                </li>
-                <li className={listItem}>
-                    <p className={pStyle}>
-                        <strong className={strongText}>Cidade:</strong>
-                        <span className="text-capitalize ">
-                            {values?.location?.city}
-                        </span>
-                    </p>
-                </li>
-
-                <li className={listItem}>
-                    <p className={pStyle}>
-                        <strong className={strongText}>Rua:</strong>
-                        <span className="text-capitalize ">
-                            {values?.location?.street}
-                        </span>
-                    </p>
-                </li>
-                <li className={listItem}>
-                    <p className={pStyle}>
-                        <strong className={strongText}>Número:</strong>
-                        <span className="">{values?.location?.number}</span>
-                    </p>
-                </li>
-                <li className={listItem}>
-                    <p className={pStyle}>
-                        <strong className={strongText}>Bairro:</strong>
-                        <span className="text-capitalize ">
-                            {values?.location?.neighborhood}
-                        </span>
-                    </p>
-                </li>
-                <li className={`${listItem} col-span-full`}>
-                    <p className={pStyle}>
-                        <strong className={strongText}>Complemento:</strong>
-                        <span className="text-capitalize ">
-                            {values?.location?.complement || 'Não informado'}
-                        </span>
-                    </p>
-                </li>
+            <ul className="flex flex-wrap justify-between w-full">
+                <Item label="CEP:" value={values?.location?.zipCode as string} />
+                <Item label="País:" value={values?.location?.country as string} />
+                <Item label="Estado:" value={values?.location?.state as string} />
+                <Item label="Cidade:" value={values?.location?.city as string} />
+                <Item
+                    label="Bairro:"
+                    value={values?.location?.neighborhood as string}
+                />
+                <Item
+                    label="Rua:"
+                    value={values?.location?.street as string}
+                    classNames={{ value: 'mobile:max-w-fit' }}
+                />
+                <Item label="Número:" value={values?.location?.number as string} />
+                <Item
+                    label="Complemento:"
+                    value={values?.location?.complement || 'Não informado'}
+                />
             </ul>
 
-            <div className="flex gap-2 justify-center items-center mt-2 w-full">
+            <div className="flex items-center justify-center w-full gap-2 mt-2">
                 <BtnNeutral
                     condition={!isLoading}
                     disabled={isLoading}
