bf601884281d192720e952293c22b9284a2698ce
murilomontino <murilomontinojr@hotmail.com>
2024-05-11 19:20:32 -0300

    refactor: adiciona constantes de serviços e tipos de serviços.

---
+++ source/constants/index.ts
@@ -0,0 +1,4 @@
+export * from './pets'
+export * from './sub-specialtys'
+export * from './treatment-items'
+export * from './types-service'
---
+++ source/constants/types-service.ts
@@ -0,0 +1,61 @@
+export const types_service = [
+    {
+        label: 'Online',
+        value: 'online',
+    },
+    {
+        label: 'Clínica',
+        value: 'clinic',
+    },
+    {
+        label: 'Domiciliar',
+        value: 'domiciliary',
+    },
+    {
+        label: 'Emergencial',
+        value: 'emergency',
+    },
+    {
+        label: 'Hospitalar',
+        value: 'hospital',
+    },
+    {
+        label: 'Intensivo',
+        value: 'intensive',
+    },
+    {
+        label: 'Ambulatorial',
+        value: 'ambulatory',
+    },
+]
+
+export const list_service_type = [
+    {
+        label: 'Domésticos',
+        value: 'domestic',
+    },
+    {
+        label: 'Equinos',
+        value: 'horses',
+    },
+    {
+        label: 'Silvestres e Conservação',
+        value: 'wild',
+    },
+    {
+        label: 'Rurais',
+        value: 'rural',
+    },
+    {
+        label: 'Exóticos',
+        value: 'exotic',
+    },
+    {
+        label: 'Não Convencionais',
+        value: 'unconventional',
+    },
+    {
+        label: 'Aquáticos',
+        value: 'aquatic',
+    },
+]
