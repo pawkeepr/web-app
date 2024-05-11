7e17285ef89fcfadea20137ca1a9e8027e3ed285
murilomontino <murilomontinojr@hotmail.com>
2024-05-11 14:37:21 -0300

    refactor: adiciona componente ItemCheckbox para exibir item selecionado no grupo de checkboxes

---
+++ source/Components/organism/checkbox-modal-group/item-checkbox.tsx
@@ -0,0 +1,33 @@
+import { IoCloseOutline } from 'react-icons/io5'
+import { withFor } from '~/utils/for-control-statements'
+import type { Item } from './checkbox-modal-group'
+
+type ItemCheckboxProps = {
+    item?: string
+    options: Item[]
+    removeItem: (name: string) => void
+}
+
+function ItemCheckbox(props: ItemCheckboxProps) {
+    const { item, options, removeItem } = props
+    if (!item) return <></>
+
+    return (
+        <li
+            key={item}
+            className="flex items-center justify-center h-8 px-2 py-1 text-xs text-gray-500 rounded-full bg-secondary-300 "
+        >
+            {options.find((option) => option.value === item)?.label}
+            <button
+                type="button"
+                className="w-5 h-5"
+                onClick={() => removeItem(item as string)}
+            >
+                <span className="sr-only">Remover</span>
+                <IoCloseOutline className="w-full h-full" />
+            </button>
+        </li>
+    )
+}
+
+export default withFor<ItemCheckboxProps>(ItemCheckbox)
