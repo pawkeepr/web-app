dcc62ca01573074880707deda9e1dafd63c186d3
murilomontino <murilomontinojr@hotmail.com>
2024-05-11 12:38:22 -0300

    refactor: adiciona contador de checkboxes selecionados e items selecionados.

--- source/Components/organism/checkbox-modal-group/checkbox-modal-group.tsx
+++ source/Components/organism/checkbox-modal-group/checkbox-modal-group.tsx
@@ -2,6 +2,7 @@ import { useField } from 'formik'
 import { useState } from 'react'
 import Modal from '~/Components/organism/modal'
 
+import { IoCloseOutline } from 'react-icons/io5'
 import { tv } from 'tailwind-variants'
 import { BtnNeutral } from '~/Components/atoms/btn'
 import CheckboxIcon from '~/Components/atoms/checkbox-icon'
@@ -17,7 +18,7 @@ type Item = {
 
 export const option = tv({
     base: `
-        group w-full flex items-center justify-center
+        w-full flex items-center justify-center
         rounded-md px-2 py-4 text-sm gap-2
         hover:bg-primary-500 dark:hover:!bg-primary-600 hover:text-white
         !h-12
@@ -58,31 +59,73 @@ export default function CheckboxModal<Ctx>({
 
     const [checkedValues, setCheckedValues] = useState<string[]>([])
 
-    function setCheckboxValue(name: string) {
-        if (!checkedValues.includes(name)) {
-            return setCheckedValues((values) => {
-                const result = [...values, name]
-                setValue(result)
-                return result
-            })
-        }
-
-        setCheckedValues((values) => {
-            const result = values.filter((element) => element !== name)
+    function addItem(name: string) {
+        return setCheckedValues((values) => {
+            const result = [...values, name]
             setValue(result)
             return result
         })
     }
 
+    function removeItem(name: string) {
+        return setCheckedValues((items) => {
+            const result = items.filter((item) => item !== name)
+            setValue(result)
+            return result
+        })
+    }
+
+    function onChangeCheckbox(name: string) {
+        if (!checkedValues.includes(name)) return addItem(name)
+        return removeItem(name)
+    }
+
     return (
         <>
             {children?.({ showModal }) || (
-                <BtnNeutral
-                    outline
-                    type="button"
-                    onClick={showModal}
-                    label={label}
-                />
+                <>
+                    <div className="flex items-center justify-center overflow-visible ">
+                        <BtnNeutral
+                            outline
+                            type="button"
+                            onClick={showModal}
+                            label={label}
+                        />
+                        {/* Badge da contagem dos checkboxes selecionados */}
+                        {
+                            <span className="flex items-center justify-center w-8 h-8 px-2 py-1 ml-2 text-xs text-white rounded-full bg-primary-500">
+                                {checkedValues?.length || 0}
+                            </span>
+                        }
+                        {/* Badge dos items dos checkboxes selecionados */}
+                    </div>
+                    <div className="h-10 mt-1">
+                        {
+                            <ul className="flex gap-1">
+                                {checkedValues.map((value) => (
+                                    <li
+                                        key={value}
+                                        className="flex items-center justify-center h-8 px-2 py-1 text-xs text-gray-500 rounded-full bg-secondary-300 "
+                                    >
+                                        {
+                                            items.find(
+                                                (item) => item.value === value,
+                                            )?.label
+                                        }
+                                        <button
+                                            type="button"
+                                            className="w-5 h-5"
+                                            onClick={() => removeItem(value)}
+                                        >
+                                            <span className="sr-only">Remover</span>
+                                            <IoCloseOutline className="w-full h-full" />
+                                        </button>
+                                    </li>
+                                ))}
+                            </ul>
+                        }
+                    </div>
+                </>
             )}
             <Modal onClose={() => closeModal()} open={open}>
                 <div className={divClassName}>
@@ -92,7 +135,7 @@ export default function CheckboxModal<Ctx>({
                         id={id}
                         separator=":"
                     />
-                    <ul className="flex flex-col items-center justify-around gap-1 flex-wrap w-40 ">
+                    <ul className="flex flex-col flex-wrap items-center justify-around gap-1 w-80">
                         {items.map((item, index) => (
                             <li
                                 className={`flex ${styles['li-option-checkbox']} w-full flex-row items-center mobile:flex-grow justify-center`}
@@ -102,7 +145,7 @@ export default function CheckboxModal<Ctx>({
                                     type="button"
                                     className={option()}
                                     onClick={() =>
-                                        setCheckboxValue(item.value as string)
+                                        onChangeCheckbox(item.value as string)
                                     }
                                 >
                                     <span className="flex-1">
