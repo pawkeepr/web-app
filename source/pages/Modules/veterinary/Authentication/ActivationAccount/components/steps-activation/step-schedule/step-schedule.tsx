4e037f130235e03ff5296c725210d7238e7c3e0f
murilomontino <murilomontinojr@hotmail.com>
2024-05-12 21:48:20 -0300

    refactor: organiza tela de agenda.

--- source/pages/Modules/veterinary/Authentication/ActivationAccount/components/steps-activation/step-schedule/step-schedule.tsx
+++ source/pages/Modules/veterinary/Authentication/ActivationAccount/components/steps-activation/step-schedule/step-schedule.tsx
@@ -1,15 +1,15 @@
 import { useMemo } from 'react'
 import { BtnNeutral, BtnPrimary } from '~/Components/atoms/btn'
-import ControlSwitchDiv from '~/Components/molecules/control-switch-div'
+import ControlToggle from '~/Components/molecules/control-toggle'
 import useFormikContextSafe from '~/hooks/use-formik-context-safe'
 import type { ActivateAccount } from '~/validations/activate'
 import type { StepProps } from '../types'
 
-const Day = ({ label, value }: { label: string; value: string }) => {
+const Day = ({ label, value }: { label: string; value: any }) => {
     const { values, setFieldValue } = useFormikContextSafe<ActivateAccount>()
     return (
-        <li className="flex justify-between gap-1 p-1   text-xs font-semibold text-gray-500 mobile:w-full">
-            <ControlSwitchDiv
+        <li className="flex justify-between gap-1 text-xs font-semibold text-gray-500 mobile:w-full">
+            <ControlToggle
                 initialValue={null}
                 ctx={values}
                 divClassName="w-full"
@@ -31,14 +31,18 @@ const StepSchedule = ({ nextStep, prevStep }: StepProps) => {
 
     return (
         <section className="flex flex-row flex-wrap w-full">
+            <h1 className="w-full my-1 text-sm font-bold text-center text-gray-500">
+                Selecione os dias que você deseja realizar agendamentos na
+                plataforma
+            </h1>
             <ul className="flex flex-col w-full gap-1 ">
-                <Day label="Domingo" value="sunday" />
-                <Day label="Segunda" value="monday" />
-                <Day label="Terça" value="tuesday" />
-                <Day label="Quarta" value="wednesday" />
-                <Day label="Quinta" value="thursday" />
-                <Day label="Sexta" value="friday" />
+                <Day label="Segunda-feira" value="monday" />
+                <Day label="Terça-feira" value="tuesday" />
+                <Day label="Quarta-feira" value="wednesday" />
+                <Day label="Quinta-feira" value="thursday" />
+                <Day label="Sexta-feira" value="friday" />
                 <Day label="Sábado" value="saturday" />
+                <Day label="Domingo" value="sunday" />
             </ul>
             <div className="flex items-center justify-center w-full gap-2 mt-1">
                 <BtnNeutral
