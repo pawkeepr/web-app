a1138eb5ebe58b40708c476293f51cc44faab53b
murilomontino <murilomontinojr@hotmail.com>
2024-05-14 19:13:41 -0300

    feat: adiciona componentes de horários de funcionamento para veterinários.

--- source/pages/Modules/veterinary/Authentication/ActivationAccount/components/steps-activation/step-schedule/step-schedule.tsx
+++ source/pages/Modules/veterinary/Authentication/ActivationAccount/components/steps-activation/step-schedule/step-schedule.tsx
@@ -1,3 +1,4 @@
+import { FieldArray } from 'formik'
 import { useMemo } from 'react'
 import { BtnNeutral, BtnPrimary } from '~/Components/atoms/btn'
 import ControlToggle from '~/Components/molecules/control-toggle'
@@ -5,24 +6,45 @@ import useFormikContextSafe from '~/hooks/use-formik-context-safe'
 import type { ActivateAccount } from '~/validations/activate'
 import type { StepProps } from '../types'
 
-const Day = ({ label, value }: { label: string; value: any }) => {
-    const { values, setFieldValue } = useFormikContextSafe<ActivateAccount>()
+const Day = ({
+    label,
+    value,
+    index,
+}: { label: string; value: string; index: number }) => {
+    const { values } = useFormikContextSafe<ActivateAccount>()
     return (
-        <li className="flex justify-between gap-1 text-xs font-semibold text-gray-500 mobile:w-full">
-            <ControlToggle
-                initialValue={null}
-                ctx={values}
-                divClassName="w-full"
-                name={value}
-                label={label}
-            />
-        </li>
+        <FieldArray name="opening_days_times">
+            {({ replace }) => {
+                return (
+                    <li className="flex justify-between gap-1 text-xs font-semibold text-gray-500 mobile:w-full">
+                        <ControlToggle
+                            initialValue={null}
+                            ctx={values}
+                            divClassName="w-full"
+                            onChange={(e) => {
+                                if (e) {
+                                    replace(index, {
+                                        type_schedule: 'online',
+                                        day: value,
+                                        hour_start: '08:00',
+                                        hour_end: '18:00',
+                                        out_of_hours_service: 'false',
+                                    })
+                                } else {
+                                    replace(index, null)
+                                }
+                            }}
+                            name={`opening_days_times[${index}]` as any}
+                            label={label}
+                        />
+                    </li>
+                )
+            }}
+        </FieldArray>
     )
 }
 
 const StepSchedule = ({ nextStep, prevStep }: StepProps) => {
-    const { values, setFieldValue } = useFormikContextSafe<ActivateAccount>()
-
     const requiredValid = useMemo((): boolean => {
         const isValid = true
 
@@ -35,14 +57,14 @@ const StepSchedule = ({ nextStep, prevStep }: StepProps) => {
                 Selecione os dias que você deseja realizar agendamentos na
                 plataforma
             </h1>
-            <ul className="flex flex-col w-full gap-1 ">
-                <Day label="Segunda-feira" value="monday" />
-                <Day label="Terça-feira" value="tuesday" />
-                <Day label="Quarta-feira" value="wednesday" />
-                <Day label="Quinta-feira" value="thursday" />
-                <Day label="Sexta-feira" value="friday" />
-                <Day label="Sábado" value="saturday" />
-                <Day label="Domingo" value="sunday" />
+            <ul className="flex flex-col w-full gap-1">
+                <Day label="Segunda-feira" value="monday" index={0} />
+                <Day label="Terça-feira" value="tuesday" index={1} />
+                <Day label="Quarta-feira" value="wednesday" index={2} />
+                <Day label="Quinta-feira" value="thursday" index={3} />
+                <Day label="Sexta-feira" value="friday" index={4} />
+                <Day label="Sábado" value="saturday" index={5} />
+                <Day label="Domingo" value="sunday" index={6} />
             </ul>
             <div className="flex items-center justify-center w-full gap-2 mt-1">
                 <BtnNeutral
