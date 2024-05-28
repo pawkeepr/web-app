3c2875355a8e6840c6d6d71ddb6da467a58608ea
murilomontino <murilomontinojr@hotmail.com>
2024-05-28 13:07:52 -0300

    refactor: atualiza with-loading para adicionar suporte a tamanho e mensagem de carregamento.

--- source/Components/helpers/with-loading.tsx
+++ source/Components/helpers/with-loading.tsx
@@ -3,23 +3,33 @@ import type { ComponentType } from 'react'
 import Loading, { type KeysLoading } from '~/Components/organism/loader'
 
 // Estas são as props que o HOC irá adicionar
-type ControlProps = {
+export type LoadingControlProps = {
     isLoading?: boolean
     typeLoading?: KeysLoading
+    sizeLoading?: number
+    messageLoading?: string
 }
 
 function withLoading<T extends { [key: string]: any }>(
-    WrappedComponent: ComponentType<T & ControlProps>,
+    WrappedComponent: ComponentType<T & LoadingControlProps>,
 ) {
     // E este é o novo componente.
     return ({
         isLoading = false,
         typeLoading = 'ThreeDots',
+        messageLoading,
+        sizeLoading,
         ...props
-    }: T & ControlProps) => {
+    }: T & LoadingControlProps) => {
         // Verifica a prop "if".
         if (isLoading) {
-            return <Loading type={typeLoading} />
+            return (
+                <Loading
+                    type={typeLoading}
+                    size={sizeLoading}
+                    message={messageLoading}
+                />
+            )
         }
 
         // Adiciona uma prop "condition".
