0ccd88d1de2fe89be1f7a7dab40a5e9839cd5742
murilomontino <murilomontinojr@hotmail.com>
2024-05-29 21:32:10 -0300

    feat: adiciona componente Tag para exibir tags personalizadas.

---
+++ source/Components/atoms/tag/tag.tsx
@@ -0,0 +1,84 @@
+import { tv } from 'tailwind-variants'
+import withControl from '~/Components/helpers/with-control'
+
+type TagProps = {
+    name: string
+    disabled: boolean
+    selected: boolean
+    onClick: () => void
+    children: React.ReactNode
+}
+
+const tag = {
+    button: tv({
+        base: 'px-2 py-1 border rounded transition-colors',
+        variants: {
+            selected: {
+                true: 'bg-primary-500 text-white',
+                false: 'bg-secondary-200 text-black',
+            },
+            disabled: {
+                true: 'opacity-50 cursor-not-allowed',
+            },
+        },
+    }),
+    label: tv({
+        base: 'text-sm font-semibold border-l border-secondary-200 pl-2',
+        variants: {
+            selected: {
+                true: 'text-gray-50',
+                false: 'text-gray-500',
+            },
+        },
+    }),
+    icon: tv({
+        base: 'absolute left-2 text-xm',
+        variants: {
+            selected: {
+                true: 'text-gray-100',
+                false: 'text-gray-500',
+            },
+        },
+    }),
+}
+
+const Tag: React.FC<TagProps> = ({
+    name,
+    disabled,
+    selected,
+    onClick,
+    children,
+}) => {
+    return (
+        <button
+            type="button"
+            name={name}
+            disabled={disabled}
+            className={tag.button({
+                className: 'rounded-full border px-4 pl-2 relative',
+                selected,
+                disabled,
+            })}
+            onClick={onClick}
+        >
+            {selected && (
+                <span
+                    className={tag.icon({
+                        selected,
+                    })}
+                >
+                    x
+                </span>
+            )}
+            <label
+                className={tag.label({
+                    selected,
+                })}
+            >
+                {children}
+            </label>{' '}
+        </button>
+    )
+}
+
+export default withControl(Tag)
