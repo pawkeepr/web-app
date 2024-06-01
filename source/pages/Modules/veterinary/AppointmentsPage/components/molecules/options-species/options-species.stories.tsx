9fc8f3304b6490e4c5da14df861b121c1e892341
murilomontino <murilomontinojr@hotmail.com>
2024-06-01 19:50:31 -0300

    feat: adiciona componente OptionsSpecies para selecionar espécies de animais.

---
+++ source/pages/Modules/veterinary/AppointmentsPage/components/molecules/options-species/options-species.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryFn } from '@storybook/react'
+
+import { fn } from '@storybook/test'
+import { Formik } from 'formik'
+import OptionsSpecies from './options-species'
+
+const meta: Meta<typeof OptionsSpecies> = {
+    tags: ['autodocs'],
+    component: OptionsSpecies,
+    title: 'Modules/Veterinary/AppointmentsPage/Components/Molecules/OptionsSpecies',
+    decorators: [
+        (Story) => (
+            <Formik enableReinitialize initialValues={[]} onSubmit={() => {}}>
+                <Story />
+            </Formik>
+        ),
+    ],
+    args: {
+        onChange: fn(),
+    },
+} satisfies Meta<typeof OptionsSpecies>
+
+export default meta
+
+const Template: StoryFn<typeof OptionsSpecies> = (args) => (
+    <OptionsSpecies {...args} />
+)
+
+export const Default = Template.bind({})
+Default.args = {}
---
+++ source/pages/Modules/veterinary/AppointmentsPage/components/molecules/options-species/options-species.tsx
@@ -0,0 +1,62 @@
+import { tv } from 'tailwind-variants'
+import { SpeciesType, species } from '~/types/speciesType'
+
+enum EmojiPet {
+    cat = '🐱',
+    dog = '🐶',
+    rabbit = '🐰',
+    fish = '🐠',
+    bird = '🐦',
+    reptile = '🦎',
+    horse = '🐴',
+}
+
+export const option = tv({
+    base: `
+        group w-full flex items-center justify-center
+        rounded-md px-2 py-4 text-sm gap-2
+        hover:bg-primary-500 dark:hover:!bg-primary-600 hover:text-white
+        !h-12
+        active:bg-primary-500 dark:active:!bg-primary-500 active:text-white
+    `,
+    variants: {
+        selected: {
+            true: 'bg-secondary-500 dark:!bg-secondary-600 text-gray-600',
+            false: 'text-gray-600',
+        },
+    },
+})
+
+export type Option = SpeciesType
+
+export type MedicalRecordFormProps = {
+    onChange: (type: Option) => void
+}
+
+const OptionsSpecies = ({ onChange }: MedicalRecordFormProps) => {
+    return (
+        <div className="grid grid-cols-2 mobile:grid-cols-1">
+            {species?.map((item, index) => (
+                <button
+                    key={`${item.value}-${index}`}
+                    type="button"
+                    onClick={onChange.bind(null, item)}
+                    className={option()}
+                >
+                    <div className="flex justify-center flex-1 gap-2">
+                        <div className="flex flex-row items-center justify-center gap-2 w-60">
+                            <span className="items-center justify-center flex-1">
+                                {EmojiPet[item.value as keyof typeof EmojiPet]}
+                            </span>
+                            <span className=" flex-[2] items-center justify-center">
+                                {item.label}
+                            </span>
+                        </div>
+                    </div>
+                </button>
+            ))}
+        </div>
+    )
+}
+
+export default OptionsSpecies
