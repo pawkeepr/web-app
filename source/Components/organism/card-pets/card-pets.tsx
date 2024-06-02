d6505372d4393aede208aaa49cf62507c901b52e
murilomontino <murilomontinojr@hotmail.com>
2024-06-02 13:04:36 -0300

    feat: atualiza traduções em card.

--- source/Components/organism/card-pets/card-pets.tsx
+++ source/Components/organism/card-pets/card-pets.tsx
@@ -1,13 +1,9 @@
 import { useMemo } from 'react'
 import AvatarPet from '~/Components/molecules/avatar-pet'
+import ptBr from '~/common/languages/pt-BR/common.json'
 import { BreedNames } from '~/types/breedType'
 import type { IPetV2Data } from '~/types/pet-v2'
-import {
-    GenderBR,
-    MapOptionSpecies,
-    Species,
-    type Gender,
-} from '~/types/speciesType'
+import { GenderBR, MapOptionSpecies, type Gender } from '~/types/speciesType'
 import { calcAge } from '~/utils/calc-age'
 import BoxButtonsPets from '../box-buttons-pets'
 import ModalBoxButtonsPet from '../box-buttons-pets/modal-box-buttons-pets'
@@ -23,7 +19,7 @@ const CardPet = ({ pet }: CardPetProps) => {
     const item = useMemo(
         () => ({
             ...pet,
-            specie: Species[pet?.specie as keyof typeof Species],
+            specie: ptBr[pet?.specie as keyof typeof ptBr],
             race: BreedNames[pet?.race as keyof typeof BreedNames],
             sex: GenderBR[pet?.sex as Gender],
         }),
@@ -37,31 +33,31 @@ const CardPet = ({ pet }: CardPetProps) => {
             boxButtons={() => <BoxButtonsPets item={pet} />}
             item={pet}
             modal={ModalBoxButtonsPet}
-            className='mobile:!px-2 mobile:!py-2 px-4 py-4 '
+            className="mobile:!px-2 mobile:!py-2 px-4 py-4 "
             sectionAvatar={() => (
                 <>
                     <AvatarPet
                         name_pet={pet?.name_pet}
                         specie={
                             MapOptionSpecies[
-                            pet.specie as keyof typeof MapOptionSpecies
+                                pet.specie as keyof typeof MapOptionSpecies
                             ] as Species
                         }
                     />
                     <div className="flex flex-row gap-1">
-                        <h1 className="text-center font-bold text-lg mobile:text-sm text-gray-400">
+                        <h1 className="text-lg font-bold text-center text-gray-400 mobile:text-sm">
                             {`${pet?.name_pet}`}
                         </h1>
                         <Gender />
                     </div>
-                    <h2 className="text-center text-xs">
+                    <h2 className="text-xs text-center">
                         {calcAge(pet?.date_birth)} ano(s)
                     </h2>
                 </>
             )}
         >
             <>
-                <div className="text-gray-500 space-y-1">
+                <div className="space-y-1 text-gray-500">
                     <div className="text-gray-500 ">
                         <h3 className="font-bold">Pet:</h3>
                         <p>{`${item?.specie}, ${item?.race}`}</p>
