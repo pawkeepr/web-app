e45ebbc8ed7bddaa778044c84b257bf1d824a3e7
murilomontino <murilomontinojr@hotmail.com>
2024-06-05 13:26:05 -0300

    fix: atualiza hook useProfile para utilizar chave o email do usuário logado.

--- source/store/hooks/profile/use-profile.tsx
+++ source/store/hooks/profile/use-profile.tsx
@@ -1,5 +1,11 @@
+import { cnpj, cpf } from 'cpf-cnpj-validator'
+import * as Yup from 'yup'
 import useAppQuery from '~/hooks/use-app-query'
-import { getTutorProfile, getVetProfile } from '~/services/helpers'
+import {
+    getTutorProfile,
+    getTutorProfileFromVet,
+    getVetProfile,
+} from '~/services/helpers'
 import { AttributeTypeProfile } from '~/services/helpers/types'
 import { useAppSelector } from '~/store/hooks'
 import type { IProfile } from '~/types/profile'
@@ -8,7 +14,7 @@ export const NAME = 'profile'
 
 const useProfile = () => {
     const { user } = useAppSelector((state) => state.Profile)
-    const superKeys = [NAME, document]
+    const superKeys = [NAME, user?.email]
 
     const type = user?.['custom:type_profile']
 
@@ -19,6 +25,27 @@ const useProfile = () => {
         enabled: !!user,
         staleTime: 1000 * 60 * 60,
     })
+}
+
+const validationDocument = Yup.string()
+    .required('Este campo é obrigatório')
+    .transform((value) => value.replace(/[^\d]/g, ''))
+    .test('cpf-cnpj-validator', 'CPF/CNPJ inválido', (value) => {
+        if (!value) return false
+        return cpf.isValid(value) || cnpj.isValid(value)
+    })
+
+export const useTutorProfileFromVet = ({ cpf_cnpj }: { cpf_cnpj: string }) => {
+    const number = cpf_cnpj.replace(/[^\d]/g, '')
+    const superKeys = [NAME, number]
+
+    return useAppQuery<IProfile>(
+        superKeys,
+        getTutorProfileFromVet.bind(null, `${number}@pawkeepr.com.br`),
+        {
+            enabled: !!number && validationDocument.isValidSync(number),
+        },
+    )
 }
 
 export default useProfile
