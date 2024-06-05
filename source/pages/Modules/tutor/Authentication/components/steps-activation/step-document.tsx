6009624a11918712e532e540aed0ad7930e1f0b3
murilomontino <murilomontinojr@hotmail.com>
2024-06-05 13:27:50 -0300

    fix: atualiza campos com dados de perfil, caso tenha registro.

--- source/pages/Modules/tutor/Authentication/components/steps-activation/step-document.tsx
+++ source/pages/Modules/tutor/Authentication/components/steps-activation/step-document.tsx
@@ -1,12 +1,12 @@
-import { useFormikContext } from 'formik'
-
-import { useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
 
 import { BtnPrimary } from '~/Components/atoms/btn'
 
 import { cnpj, cpf } from 'cpf-cnpj-validator'
 import * as Yup from 'yup'
 import FieldDocument from '~/Components/molecules/field-document'
+import useFormikContextSafe from '~/hooks/use-formik-context-safe'
+import { useTutorProfileFromVet } from '~/store/hooks/profile/use-profile'
 import type { ActivateAccount } from '../../activate'
 import type { StepProps } from './types'
 
@@ -19,7 +19,34 @@ const validationDocument = Yup.string()
     })
 
 const StepSignUpDocument = ({ nextStep }: StepProps) => {
-    const { values } = useFormikContext<ActivateAccount>()
+    const { values, setFieldValue } = useFormikContextSafe<ActivateAccount>()
+
+    const { data, isPending } = useTutorProfileFromVet({
+        cpf_cnpj: values.cpf_cnpj,
+    })
+
+    useEffect(() => {
+        if (!data) return
+        setFieldValue('id', data.id)
+        setFieldValue('firstName', data.user_information?.first_name)
+        setFieldValue('lastName', data.user_information?.last_name)
+        setFieldValue(
+            'contact.whatsapp',
+            data.user_information?.contact?.whatsapp ||
+                data.user_information?.contact?.phone,
+        )
+
+        setFieldValue('location', {
+            country: 'BR',
+            street: data.user_information?.address?.street,
+            number: data.user_information?.address?.number,
+            complement: data.user_information?.address?.complement,
+            neighborhood: data.user_information?.address?.neighborhood,
+            city: data.user_information?.address?.city,
+            state: data.user_information?.address?.state,
+            zipCode: data.user_information?.address?.zipCode,
+        })
+    }, [data])
 
     const requiredValid = useMemo((): boolean => {
         const isValid = validationDocument.isValidSync(values.cpf_cnpj)
@@ -37,7 +64,7 @@ const StepSignUpDocument = ({ nextStep }: StepProps) => {
                 placeholder="CPF/CNPJ"
                 required
             />
-
+            {isPending && requiredValid && <p>Carregando...</p>}
             <div className="flex items-center justify-center mt-1 col-span-full">
                 <BtnPrimary
                     onClick={nextStep}
