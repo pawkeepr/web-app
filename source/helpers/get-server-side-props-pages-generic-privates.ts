5b18f81b2a22aedf56f105ef15dcd0c5c6749a53
murilomontino <murilomontinojr@hotmail.com>
2024-06-06 15:54:17 -0300

    feat: add helper getServerSidePropsPages para páginas genéricas  a qualquer perfil.

---
+++ source/helpers/get-server-side-props-pages-generic-privates.ts
@@ -0,0 +1,75 @@
+import optionsCookies from '~/constants/cookies'
+
+import type {
+    GetServerSideProps,
+    GetServerSidePropsContext,
+    PreviewData,
+} from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import type { ParsedUrlQuery } from 'node:querystring'
+import { PUBLIC_ROUTES } from '~/common/public-routes'
+import { getCookie } from '~/utils/cookies-utils'
+import { fetchProfile } from './get-server-side-props-pages-veterinary-privates'
+export type Context =
+    | GetServerSidePropsContext<ParsedUrlQuery, PreviewData>
+    | undefined
+
+const getServerSidePropsPagesGenericsPrivates =
+    (callback?: GetServerSideProps) => async (ctx: Context) => {
+        if (!ctx) {
+            return {
+                redirect: {
+                    destination: '/sign-in',
+                    permanent: false,
+                },
+            }
+        }
+
+        const name = optionsCookies.token.name
+        const token = getCookie(name, ctx)
+
+        if (!token) {
+            return {
+                redirect: {
+                    destination: '/sign-in',
+                    permanent: false,
+                },
+            }
+        }
+
+        const route = ctx.resolvedUrl
+        const attr = fetchProfile(ctx)
+
+        if (PUBLIC_ROUTES.includes(route)) {
+            return {
+                redirect: {
+                    destination: '/dashboard',
+                    permanent: false,
+                },
+            }
+        }
+
+        const hasProfile = attr?.['custom:has_profile'] === 'yes'
+
+        if (!hasProfile) {
+            return {
+                redirect: {
+                    destination: '/tutor/activation',
+                    permanent: false,
+                },
+            }
+        }
+
+        if (callback) return callback(ctx)
+
+        const lg = ctx.locale || 'pt-br'
+        const locale = await serverSideTranslations(lg, ['common'])
+
+        return {
+            props: {
+                ...locale,
+            },
+        }
+    }
+
+export default getServerSidePropsPagesGenericsPrivates
