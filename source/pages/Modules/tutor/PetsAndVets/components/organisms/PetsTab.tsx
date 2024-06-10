3556556cf032a7955df314f3e26490431e95223b
murilomontino <murilomontinojr@hotmail.com>
2024-06-10 14:03:29 -0300

    refactor: atualiza componente PetsTab para usar o novo componente CardFeedPets.

--- source/pages/Modules/tutor/PetsAndVets/components/organisms/PetsTab.tsx
+++ source/pages/Modules/tutor/PetsAndVets/components/organisms/PetsTab.tsx
@@ -1,14 +1,42 @@
 import { MdPets } from 'react-icons/md'
 import { BtnLinkFloating } from '~/Components/molecules/btn-floating/btn-floating'
-import CardPets from '~/Components/organism/card-pets'
-import { useListPetsFromTutor } from '~/store/hooks/list-pets-by-tutor/use-list-pet-by-tutor'
+import CardFeedPets from '~/Components/organism/card-feed-pets'
+import {
+    useListPetsFromTutor,
+    type Pet,
+} from '~/store/hooks/list-pets-by-tutor/use-list-pet-by-tutor'
 import useProfile from '~/store/hooks/profile/use-profile'
+
+type MapCardFeedPetsProps = {
+    pets?: Pet[]
+    isLoading: boolean
+}
+
+const MapCardFeedPets = ({ pets, isLoading }: MapCardFeedPetsProps) => {
+    const { data: profile } = useProfile()
+
+    if (isLoading) {
+        return <span>Carregando...</span>
+    }
+
+    return (
+        <div className="gap-1 mt-1 overflow-x-auto scrollable-x">
+            {pets?.map((pet) => (
+                <CardFeedPets
+                    key={pet?.id_pet}
+                    pet={{
+                        ...pet,
+                        cpf_cnpj: profile?.user_information?.cpf_cnpj as string,
+                        castrated: pet?.castrated === 'yes',
+                    }}
+                />
+            ))}
+        </div>
+    )
+}
 
 const PetsTab = () => {
     const { data: pets, isPending } = useListPetsFromTutor()
-    const { data: profile } = useProfile()
-
-    if (isPending) return <div>Loading...</div>
 
     return (
         <section className="w-full px-4 mobile:!px-2">
@@ -18,16 +46,7 @@ const PetsTab = () => {
                 </div>
             )}
 
-            {pets?.map((pet) => (
-                <CardPets
-                    key={pet.id_pet}
-                    pet={{
-                        ...pet,
-                        cpf_cnpj: profile?.user_information?.cpf_cnpj as string,
-                        castrated: pet.castrated === 'yes',
-                    }}
-                />
-            ))}
+            <MapCardFeedPets pets={pets as Pet[]} isLoading={isPending} />
 
             <BtnLinkFloating
                 icon={(props) => <MdPets {...props} />}
