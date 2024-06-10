e6b2969cb67ed9a624d022c2b7a543170682f253
murilomontino <murilomontinojr@hotmail.com>
2024-06-10 14:19:01 -0300

    refactor: atualiza componente  CardFeedPets com efeito de carregamento.

--- source/pages/Modules/tutor/PetsAndVets/components/organisms/PetsTab.tsx
+++ source/pages/Modules/tutor/PetsAndVets/components/organisms/PetsTab.tsx
@@ -1,6 +1,8 @@
 import { MdPets } from 'react-icons/md'
 import { BtnLinkFloating } from '~/Components/molecules/btn-floating/btn-floating'
+import { card } from '~/Components/organism/card'
 import CardFeedPets from '~/Components/organism/card-feed-pets'
+
 import {
     useListPetsFromTutor,
     type Pet,
@@ -16,7 +18,27 @@ const MapCardFeedPets = ({ pets, isLoading }: MapCardFeedPetsProps) => {
     const { data: profile } = useProfile()
 
     if (isLoading) {
-        return <span>Carregando...</span>
+        return (
+            <div className="w-full gap-1 mt-1 overflow-x-auto h-fit scrollable-x">
+                {[0, 0, 0, 0].map((_, index) => (
+                    <div
+                        key={index}
+                        className={card({
+                            className:
+                                'w-28 !h-32 bg-gradient-to-r from-white via-gray-200 to-gray-300 animate-pulse',
+                        })}
+                    />
+                ))}
+            </div>
+        )
+    }
+
+    if (!pets) {
+        return (
+            <div className="text-center w-full !h-32 flex items-center justify-center">
+                <span>Não há Pets Cadastrados</span>
+            </div>
+        )
     }
 
     return (
@@ -39,7 +61,7 @@ const PetsTab = () => {
     const { data: pets, isPending } = useListPetsFromTutor()
 
     return (
-        <section className="w-full px-4 mobile:!px-2">
+        <section className="flex flex-1 flex-col w-full px-4 mobile:!px-2">
             {pets?.length === 0 && (
                 <div className="text-center">
                     <span>Não há Pets Cadastrados</span>
@@ -47,7 +69,9 @@ const PetsTab = () => {
             )}
 
             <MapCardFeedPets pets={pets as Pet[]} isLoading={isPending} />
-
+            <section className="flex items-center justify-center flex-1 min-h-[50vh] ">
+                Estamos Trabalhando no seu Feed. Confie em nós. Beba água!
+            </section>
             <BtnLinkFloating
                 icon={(props) => <MdPets {...props} />}
                 title="Adicionar Pet"
