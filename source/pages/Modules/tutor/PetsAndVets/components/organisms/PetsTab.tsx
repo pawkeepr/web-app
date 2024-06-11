4ac88e61c2397316a7d86e836257be97df93f212
murilomontino <murilomontinojr@hotmail.com>
2024-06-10 21:18:37 -0300

    refactor:  adiciona loading com card de template.

--- source/pages/Modules/tutor/PetsAndVets/components/organisms/PetsTab.tsx
+++ source/pages/Modules/tutor/PetsAndVets/components/organisms/PetsTab.tsx
@@ -12,26 +12,11 @@ import useProfile from '~/store/hooks/profile/use-profile'
 type MapCardFeedPetsProps = {
     pets?: Pet[]
     isLoading: boolean
+    isPending: boolean
 }
 
-const MapCardFeedPets = ({ pets, isLoading }: MapCardFeedPetsProps) => {
+const MapCardFeedPets = ({ pets, isPending, isLoading }: MapCardFeedPetsProps) => {
     const { data: profile } = useProfile()
-
-    if (isLoading) {
-        return (
-            <div className="w-full gap-1 mt-1 overflow-x-auto h-fit scrollable-x">
-                {[0, 0, 0, 0].map((_, index) => (
-                    <div
-                        key={index}
-                        className={card({
-                            className:
-                                'w-28 !h-32 bg-gradient-to-r from-white via-gray-200 to-gray-300 animate-pulse',
-                        })}
-                    />
-                ))}
-            </div>
-        )
-    }
 
     if (!pets) {
         return (
@@ -42,33 +27,53 @@ const MapCardFeedPets = ({ pets, isLoading }: MapCardFeedPetsProps) => {
     }
 
     return (
-        <div className="gap-1 mt-1 overflow-x-auto scrollable-x">
-            {pets?.map((pet) => (
-                <CardFeedPets
-                    key={pet?.id_pet}
-                    pet={{
-                        ...pet,
-                        cpf_cnpj: profile?.user_information?.cpf_cnpj as string,
-                        castrated: pet?.castrated === 'yes',
-                    }}
-                />
-            ))}
+        <div className="gap-1 mt-1 !overflow-x-auto flex flex-row w-screen scrollable-x">
+            {!isPending &&
+                pets?.map((pet) => (
+                    <CardFeedPets
+                        key={pet?.id_pet}
+                        pet={{
+                            ...pet,
+                            cpf_cnpj: profile?.user_information?.cpf_cnpj as string,
+                            castrated: pet?.castrated === 'yes',
+                        }}
+                    />
+                ))}
+            {(isLoading || isPending) && (
+                <>
+                    {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((_, index) => (
+                        <div
+                            // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                            key={index}
+                            className={card({
+                                className:
+                                    'px-2 my-2 py-2 flex-1 w-28 h-36 bg-gradient-to-r from-white via-gray-200 to-gray-300 animate-pulse',
+                            })}
+                            style={{ minWidth: '7rem ', minHeight: '9rem' }} // fix width and height
+                        />
+                    ))}
+                </>
+            )}
         </div>
     )
 }
 
 const PetsTab = () => {
-    const { data: pets, isPending } = useListPetsFromTutor()
+    const { data: pets, isPending, isLoading } = useListPetsFromTutor()
 
     return (
         <section className="flex flex-1 flex-col w-full px-4 mobile:!px-2">
             {pets?.length === 0 && (
-                <div className="text-center">
+                <div className="flex items-center justify-center h-32 text-center">
                     <span>Não há Pets Cadastrados</span>
                 </div>
             )}
 
-            <MapCardFeedPets pets={pets as Pet[]} isLoading={isPending} />
+            <MapCardFeedPets
+                pets={pets as Pet[]}
+                isPending={isPending}
+                isLoading={true}
+            />
             <section className="flex items-center justify-center flex-1 min-h-[50vh] ">
                 Estamos Trabalhando no seu Feed. Confie em nós. Beba água!
             </section>
