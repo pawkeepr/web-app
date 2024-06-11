f65e283a1f3a01b067f312e1212c6ae27ee651e5
murilomontino <murilomontinojr@hotmail.com>
2024-06-11 17:45:25 -0300

    feat: Atualiza componente QrCodePet para exibir QR code do pet com URL completa.

--- source/Components/molecules/qr-code-pet/qr-code-pet.tsx
+++ source/Components/molecules/qr-code-pet/qr-code-pet.tsx
@@ -1,18 +1,22 @@
 import { QRCode } from 'react-qrcode-logo'
+import { encodeBase64 } from '~/utils/encode-base-64'
 
 type QrCodePetProps = {
     id_pet: string
 }
 
 const QrCodePet = ({ id_pet }: QrCodePetProps) => {
+    const currentUrl = `${window.location.protocol}//${window.location.host}`
+
     return (
         <QRCode
-            value={id_pet}
+            value={`${currentUrl}/pet-was-found/${encodeBase64(id_pet)}`}
             logoImage="/logo-sm-qr-code.png"
-            logoWidth={40}
-            logoHeight={40}
+            logoWidth={30}
+            logoHeight={30}
             logoOpacity={1}
             qrStyle="squares"
+            size={200}
             removeQrCodeBehindLogo
             eyeRadius={10}
         />
