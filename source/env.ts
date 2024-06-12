3182a613c53725b20bc58353fdfd44f0d7f2899b
murilomontino <murilomontinojr@hotmail.com>
2024-06-12 20:54:06 -0300

    fix: acesso a var ambientes e janela de navegador.

--- source/env.ts
+++ source/env.ts
@@ -1,16 +1,15 @@
-
-const FLAG_DEV = window.location.host.startsWith('development') || window.location.host.startsWith('localhost')
-const keys = {
-    FLAG_DEV,
-    API_URL: process.env.REACT_APP_API_URL || '',
-    SECRET_KEY: process.env.SECRET_KEY || '',
-    REGION: process.env.REGION || '',
-    USER_POOL_ID: process.env.USER_POOL_ID || '',
-    USER_POOL_WEB_CLIENT_ID: process.env.USER_POOL_WEB_CLIENT_ID || '',
-} as const
-type Keys = keyof typeof keys
-
 export const Env = () => {
+    const FLAG_DEV = window?.location?.host?.startsWith('development') || window?.location?.host?.startsWith('localhost')
+    const keys = {
+        FLAG_DEV,
+        API_URL: process.env.REACT_APP_API_URL || '',
+        SECRET_KEY: process.env.SECRET_KEY || '',
+        REGION: process.env.REGION || '',
+        USER_POOL_ID: process.env.USER_POOL_ID || '',
+        USER_POOL_WEB_CLIENT_ID: process.env.USER_POOL_WEB_CLIENT_ID || '',
+    } as const
+    type Keys = keyof typeof keys
+
     const get = (key: Keys) => {
         return keys[key] as typeof keys[keyof typeof keys]
     }
