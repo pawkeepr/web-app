fc999e828eb889e92ef84c0cdbf00296261ccafb
murilomontino <murilomontinojr@hotmail.com>
2024-06-14 10:29:12 -0300

    refactor: adiciona meta tag de viewport no _document.tsx.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,18 @@
 import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="pt-br">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="pt-br">
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
