e1f4f3a1ce3d2bf8fa30d89811ecea575ce9275d
murilomontino <murilomontinojr@hotmail.com>
2024-06-29 14:00:39 -0300

    fix: atualiza configuração do Cognito em amplify.

--- source/aws.ts
+++ source/aws.ts
@@ -2,15 +2,17 @@ import { Amplify } from 'aws-amplify'
 
 Amplify.configure({
     Auth: {
-        // REQUIRED - Amazon Cognito Identity Pool ID
         Cognito: {
             identityPoolId: process.env.IDENTITY_POOL_ID as string,
+            userPoolClientId: process.env.USER_POOL_WEB_CLIENT_ID as string,
             userPoolId: process.env.USER_POOL_ID as string,
-            userPoolClientId: process.env.USER_POOL_CLIENT_ID as string,
-            allowGuestAccess: false,
+            loginWith: {
+                email: true,
+                username: true,
+            },
         },
-        // other configuration
     },
+    // other configuration
 })
 
 // You can get the current config object
