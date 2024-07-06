3ad087e760499128bbe9a7ae93aa06939ae8d2ae
murilomontino <murilomontinojr@hotmail.com>
2024-07-06 17:00:31 -0300

    refactor: requisitos de senha no formulário de login.

--- source/aws.ts
+++ source/aws.ts
@@ -6,6 +6,13 @@ Amplify.configure({
             identityPoolId: process.env.IDENTITY_POOL_ID as string,
             userPoolClientId: process.env.USER_POOL_WEB_CLIENT_ID as string,
             userPoolId: process.env.USER_POOL_ID as string,
+            passwordFormat: {
+                minLength: 8,
+                requireLowercase: true,
+                requireNumbers: true,
+                requireUppercase: true,
+                requireSpecialCharacters: true,
+            },
             loginWith: {
                 email: true,
                 username: true,
