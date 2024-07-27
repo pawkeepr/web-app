f35c98643f405107a3a1fb8c6ffa3ca8c9250abf
murilomontino <murilomontinojr@hotmail.com>
2024-07-27 12:38:58 -0300

    fix: corrige cor de menu da página 'historic'.

--- source/pages/Modules/veterinary/HistoricPage/HistoricPage.tsx
+++ source/pages/Modules/veterinary/HistoricPage/HistoricPage.tsx
@@ -1,37 +1,35 @@
-import DefaultLayout from "../../_layouts/dashboard/dashboard";
+import DefaultLayout from '../../_layouts/dashboard/dashboard'
 
-import { MdCancel, MdCheckCircle } from "react-icons/md";
-import HorizontalTabs from "~/Components/organism/horizontal-list/horizontal-list";
-import StepScheduledCanceled from "./components/organisms/steps/step-scheduled-canceled";
-import StepScheduledDone from "./components/organisms/steps/step-scheduled-done";
+import { MdCancel, MdCheckCircle } from 'react-icons/md'
+import HorizontalTabs from '~/Components/organism/horizontal-list/horizontal-list'
+import StepScheduledCanceled from './components/organisms/steps/step-scheduled-canceled'
+import StepScheduledDone from './components/organisms/steps/step-scheduled-done'
 
 const Tabs = () => [
     {
         id: 5,
-        title: "Consultas Realizadas",
-        href: "#done",
+        title: 'Consultas Realizadas',
+        href: '#done',
         icon: <MdCheckCircle className="w-6 h-6" />,
         tab: <StepScheduledDone />,
     },
     {
         id: 3,
-        title: "Consultas Canceladas",
-        href: "#canceled",
-        icon: (
-            <MdCancel className="w-6 h-6" />
-        ),
+        title: 'Consultas Canceladas',
+        href: '#canceled',
+        icon: <MdCancel className="w-6 h-6" />,
         tab: <StepScheduledCanceled />,
-    }
-];
+    },
+]
 
 const HistoricPage = () => {
-    const categories = Tabs();
+    const categories = Tabs()
 
     return (
         <DefaultLayout title="Dashboard" searchBlock name="historic">
-            <HorizontalTabs categories={categories} />
+            <HorizontalTabs categories={categories} menu />
         </DefaultLayout>
-    );
-};
+    )
+}
 
-export default HistoricPage;
+export default HistoricPage
