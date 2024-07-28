582f470890553978b4c5d2a6eac5d61ff50032b0
murilomontino <murilomontinojr@hotmail.com>
2024-07-28 18:08:07 -0300

    revert: remove middlewares.

--- pages/api/middleware.ts
+++ pages/api/middleware.ts
@@ -1,10 +1,8 @@
 import { NextResponse, type NextRequest } from 'next/server'
-import Authorization from '~/api/middleware/Authorization'
-import Cors from '~/api/middleware/Cors'
 
 export function middleware(request: NextRequest) {
-    Cors(request) // Enable CORS
-    Authorization(request) // Check Authorization header
+    // Cors(request) // Enable CORS
+    // Authorization(request) // Check Authorization header
 
     return NextResponse.next()
 }
