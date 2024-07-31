eff880fd529c0c3d6f22fca79cdfd10253445752
murilomontino <murilomontinojr@hotmail.com>
2024-07-30 21:07:35 -0300

    feat: adiciona serviço complementar de atualização de foto de perfil.

--- source/services/helpers/profile.ts
+++ source/services/helpers/profile.ts
@@ -10,6 +10,10 @@ const urls = {
         `/api-user/update-user/${type_user}`,
     GET_SIGNED_URL: () => '/api/get-file-signed-url/',
     FETCH_PROFILE_IMG: () => '/api-s3handler/get-object-s3',
+    POST_PROFILE: (type_user: KEYS_TYPE_USERS) =>
+        `/api/s3handler/upload-object-s3/${type_user}`,
+    UPDATE_PROFILE_PICTURE: (type_user: KEYS_TYPE_USERS) =>
+        `api-user/update-file-user/${type_user}`,
 }
 
 export const updateProfileV2 = async (data: IProfile, type_user: KEYS_TYPE_USERS) =>
@@ -26,11 +30,32 @@ export type GetSignedUrl = {
 
 export const getSignedUrl = () => apiFile.get<GetSignedUrl>(urls.GET_SIGNED_URL())
 
-export const updateProfilePicture = (formData: FormData) => {
-    return axios.post('/api/s3handler/upload-object-s3', formData, {
+type UpdateProfilePicture = {
+    object_name: string
+}
+export const updateProfilePictureV2 = (
+    data: UpdateProfilePicture,
+    type_user: KEYS_TYPE_USERS,
+    user_id: string,
+) =>
+    api.post<GetSignedUrl>(urls.UPDATE_PROFILE_PICTURE(type_user), data, {
+        params: {
+            user_id: user_id,
+        },
+    })
+
+export const updateProfilePicture = (
+    formData: FormData,
+    type_user: KEYS_TYPE_USERS,
+    user_id: string,
+) => {
+    return axios.put(urls.POST_PROFILE(type_user), formData, {
         headers: {
             Authorization: `Bearer ${getCookie(cookies.token.name)}`,
             'Content-Type': 'multipart/form-data',
+        },
+        params: {
+            user_id: user_id,
         },
     })
 }
@@ -44,3 +69,24 @@ export const fetchProfilePhoto = (data: FetchProfileImg) =>
             type_doc: 1,
         },
     })
+
+export const uploadToS3 = async (img: string) => {
+    const { data } = await getSignedUrl()
+
+    const image = Buffer.from(img, 'base64')
+
+    await axios.put(data.url, image, {
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'image/jpeg',
+            'Content-Length': image.length.toString(),
+        },
+    })
+
+    return {
+        status: 200,
+        message: {
+            filename: data.filename,
+        },
+    }
+}
