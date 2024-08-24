745fb2a84b063eb178567a1f95c908fec0ac946b
murilomontino <murilomontinojr@hotmail.com>
2024-08-23 21:44:03 -0300

    feat: componente animado para envio de arquivos.

---
+++ source/ui/file-upload/file-upload.tsx
@@ -0,0 +1,195 @@
+import { motion } from 'framer-motion'
+import { useRef, useState } from 'react'
+import { useDropzone } from 'react-dropzone'
+import { cn } from '../lib/utils'
+
+const mainVariant = {
+    initial: {
+        x: 0,
+        y: 0,
+    },
+    animate: {
+        x: 20,
+        y: -20,
+        opacity: 0.9,
+    },
+}
+
+type FileUploadProps = {
+    onChange?: (files: File[]) => void
+    acceptDragDrop?: {
+        [key: string]: string[]
+    }
+    accept?: string
+    multiple?: boolean
+    children?: React.ReactNode
+    disabled?: boolean
+}
+
+export const FileUpload = ({
+    onChange,
+    accept,
+    acceptDragDrop,
+    disabled,
+    children,
+    multiple,
+}: FileUploadProps) => {
+    const [files, setFiles] = useState<File[]>([])
+    const fileInputRef = useRef<HTMLInputElement>(null)
+
+    const handleFileChange = (newFiles: File[]) => {
+        setFiles((prevFiles) => [...prevFiles, ...newFiles])
+        onChange?.(newFiles)
+    }
+
+    const handleClick = () => {
+        fileInputRef.current?.click()
+    }
+
+    const { getRootProps } = useDropzone({
+        multiple: multiple,
+        noClick: true,
+        disabled: disabled,
+        accept: acceptDragDrop,
+        onDrop: handleFileChange,
+        onDropRejected: (error) => {
+            console.log(error)
+        },
+    })
+
+    return (
+        <div className="w-full" {...getRootProps()}>
+            <motion.div
+                onClick={handleClick}
+                whileHover="animate"
+                className="relative block w-full overflow-hidden rounded-lg cursor-pointer group/file"
+            >
+                <input
+                    ref={fileInputRef}
+                    id="file-upload-handle"
+                    type="file"
+                    accept={accept}
+                    multiple={multiple}
+                    disabled={disabled}
+                    onChange={(e) =>
+                        handleFileChange(Array.from(e.target.files || []))
+                    }
+                    className="hidden"
+                />
+                <div className="absolute inset-0 [mask-image:radial-gradient(ellipse_at_center,white,transparent)]">
+                    <GridPattern />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <p className="relative z-20 font-sans text-base font-bold text-neutral-700 dark:text-neutral-300">
+                        Enviar Arquivo
+                    </p>
+                    <p className="relative z-20 mt-2 font-sans text-base font-normal mobile:hidden text-neutral-400 dark:text-neutral-400">
+                        Arraste e solte arquivos aqui ou clique para selecionar
+                    </p>
+                    <div className="relative w-full max-w-xl mx-auto">
+                        {files.length > 0 &&
+                            files.map((file, idx) => (
+                                <motion.div
+                                    key={`file${idx}`}
+                                    layoutId={
+                                        idx === 0
+                                            ? 'file-upload'
+                                            : `file-upload-${idx}`
+                                    }
+                                    className={cn(
+                                        'relative overflow-hidden z-40 bg-white dark:bg-neutral-900 flex flex-col items-start justify-start md:h-24 p-4 mt-4 w-full mx-auto rounded-md',
+                                        'shadow-sm',
+                                    )}
+                                >
+                                    <div className="flex items-center justify-between w-full gap-4">
+                                        <motion.p
+                                            initial={{ opacity: 0 }}
+                                            animate={{ opacity: 1 }}
+                                            layout
+                                            className="max-w-xs text-base truncate text-neutral-700 dark:text-neutral-300"
+                                        >
+                                            {file.name}
+                                        </motion.p>
+                                        <motion.p
+                                            initial={{ opacity: 0 }}
+                                            animate={{ opacity: 1 }}
+                                            layout
+                                            className="flex-shrink-0 px-2 py-1 text-sm rounded-lg w-fit text-neutral-600 dark:bg-neutral-800 dark:text-white shadow-input"
+                                        >
+                                            {(file.size / (1024 * 1024)).toFixed(2)}{' '}
+                                            MB
+                                        </motion.p>
+                                    </div>
+
+                                    <div className="flex flex-col items-start justify-between w-full mt-2 text-sm md:flex-row md:items-center text-neutral-600 dark:text-neutral-400">
+                                        <motion.p
+                                            initial={{ opacity: 0 }}
+                                            animate={{ opacity: 1 }}
+                                            layout
+                                            className="px-1 py-0.5 rounded-md bg-gray-100 dark:bg-neutral-800 "
+                                        >
+                                            {file.type}
+                                        </motion.p>
+
+                                        <motion.p
+                                            initial={{ opacity: 0 }}
+                                            animate={{ opacity: 1 }}
+                                            layout
+                                        >
+                                            modified{' '}
+                                            {new Date(
+                                                file.lastModified,
+                                            ).toLocaleDateString()}
+                                        </motion.p>
+                                    </div>
+                                </motion.div>
+                            ))}
+                        {!files.length && (
+                            <motion.div
+                                layoutId="file-upload"
+                                variants={mainVariant}
+                                transition={{
+                                    type: 'spring',
+                                    stiffness: 300,
+                                    damping: 20,
+                                }}
+                                className={cn(
+                                    'relative group-hover/file:shadow-2xl z-40 bg-white dark:bg-neutral-900 flex items-center justify-center h-32 mt-4 w-full max-w-[8rem] mx-auto rounded-md',
+                                    'shadow-[0px_10px_50px_rgba(0,0,0,0.1)]',
+                                )}
+                            >
+                                {children}
+                            </motion.div>
+                        )}
+                    </div>
+                </div>
+            </motion.div>
+        </div>
+    )
+}
+
+export function GridPattern() {
+    const columns = 41
+    const rows = 11
+    return (
+        <div className="flex flex-wrap items-center justify-center flex-shrink-0 scale-105 bg-gray-100 dark:bg-neutral-900 gap-x-px gap-y-px">
+            {Array.from({ length: rows }).map((_, row) =>
+                Array.from({ length: columns }).map((_, col) => {
+                    const index = row * columns + col
+                    return (
+                        <div
+                            key={`${col}-${row}`}
+                            className={`w-10 h-10 flex flex-shrink-0 rounded-[2px] ${
+                                index % 2 === 0
+                                    ? 'bg-gray-50 dark:bg-neutral-950'
+                                    : 'bg-gray-50 dark:bg-neutral-950 shadow-[0px_0px_1px_3px_rgba(255,255,255,1)_inset] dark:shadow-[0px_0px_1px_3px_rgba(0,0,0,1)_inset]'
+                            }`}
+                        />
+                    )
+                }),
+            )}
+        </div>
+    )
+}
+
+export default FileUpload
