6058450baed463f933289386b37cdb8e18b18a89
murilomontino <murilomontinojr@hotmail.com>
2024-08-31 11:31:42 -0300

    refactor(pre-build.js): move todos os modulos nao utilizados

--- script/pre-build.js
+++ script/pre-build.js
@@ -1,24 +1,23 @@
 const fs = require('node:fs')
 const path = require('node:path')
-const mode = require('./mode')
 
-// Caminho para a pasta de páginas e backup
-const pagesDir = path.join(__dirname, '..', 'pages', mode)
-const backupDir = path.join(__dirname, '..', 'pages-backup')
-
-// Cria o diretório de backup se não existir
-if (!fs.existsSync(backupDir)) {
-    fs.mkdirSync(backupDir, { recursive: true })
-}
+/**
+ * @typedef {string[]} Mode
+ */
+const mode = /** @type {Mode} */ (require('./mode'))
 
 // Função para mover arquivos e diretórios para o backup
 function movePagesToBackup(dir, backupRoot) {
     const files = fs.readdirSync(dir)
+    // Cria o diretório de backup se não existir
+    if (!fs.existsSync(backupRoot)) {
+        fs.mkdirSync(backupRoot, { recursive: true })
+    }
 
     for (let i = 0; i < files.length; i++) {
         const file = files[i]
         const filePath = path.join(dir, file)
-        const backupPath = path.join(backupRoot, path.relative(pagesDir, filePath))
+        const backupPath = path.join(backupRoot, path.relative(dir, filePath))
 
         try {
             const stats = fs.statSync(filePath)
@@ -40,4 +39,11 @@ function movePagesToBackup(dir, backupRoot) {
     }
 }
 
-movePagesToBackup(pagesDir, backupDir)
+for (let i = 0; i < mode.length; i++) {
+    const modeName = mode[i]
+    const pagesDir = path.join(__dirname, '..', 'pages', modeName)
+    const backupDir = path.join(__dirname, '..', 'pages-backup', modeName)
+    movePagesToBackup(pagesDir, backupDir)
+}
+
+console.log('Done!')
