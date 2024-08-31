7c1b202fb33436d17acc1fbf2556a93e2f25ca0d
murilomontino <murilomontinojr@hotmail.com>
2024-08-31 11:31:06 -0300

    docs(mode.js): add description

--- script/mode.js
+++ script/mode.js
@@ -1,9 +1,20 @@
 require('dotenv').config()
 
+/**
+ * @description
+ * MODE_PROFILE serve para definir quais páginas devem ser renderizadas para cada perfil.
+ * O array de cada chave é o inverso do que se espera, pois ele define quais páginas
+ * NÃO devem ser renderizadas para aquele perfil.
+ * Por exemplo, o perfil de tutor não deve renderizar as páginas de 'external' e 'veterinary'.
+ * @typedef {Object.<string, string[]>} ModeProfile
+ */
 const MODE_PROFILE = {
-    tutor: 'tutor',
-    external: 'external',
-    vet: 'veterinary',
+    tutor: ['external', 'veterinary'],
+    external: ['tutor', 'veterinary'],
+    vet: ['tutor', 'external'],
 }
 
-module.exports = MODE_PROFILE[process.env.MODE_PROFILE]
+/**
+ * @typedef {string[]} Mode
+ */
+module.exports = /** @type {ModeProfile} */ (MODE_PROFILE[process.env.MODE_PROFILE])
