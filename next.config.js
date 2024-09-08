cc3a2f22ce3de29732a8ae97dee36409142f72c9
murilomontino <murilomontinojr@hotmail.com>
2024-09-08 14:54:45 -0300

    refactor: disable pwa em modo dev

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const webpack = require('webpack')
 
 const withPWA = require('next-pwa')({
     dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
 })
 
 /**@type {import('next').NextConfig} */
