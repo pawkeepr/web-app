432e93c3a93ca2fff32660a3abb26d5d769aa9fd
murilomontino <murilomontinojr@hotmail.com>
2024-09-08 14:03:27 -0300

    feat: modal para adicionar/editar plano de saude

---
+++ source/Components/modals/health-plans/health-plans.tsx
@@ -0,0 +1,110 @@
+import { Form, Formik } from 'formik'
+import { BtnCancel, BtnPrimary } from '~/Components/atoms/btn'
+import withCompose from '~/Components/helpers/with-compose'
+import FieldControl from '~/Components/molecules/field-control'
+import Modal from '~/Components/organism/modal'
+import useModal from '~/hooks/use-modal'
+import type { IHealthPlan } from '~/services/helpers/health-plans'
+
+type ModalHealthPlansProps = {
+    children?: (showModal: () => void) => JSX.Element
+    healthPlan: IHealthPlan
+}
+
+const ModalHealthPlans = ({ children, healthPlan }: ModalHealthPlansProps) => {
+    const { closeModal, open, showModal } = useModal()
+
+    return (
+        <>
+            {children?.(showModal)}
+            {!children && (
+                <div className="flex items-center justify-center">
+                    <button
+                        type="button"
+                        onClick={() => showModal()}
+                        className="px-4 py-2 text-sm font-medium text-white rounded-md bg-secondary-500 bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+                    >
+                        Planos de Saúde
+                    </button>
+                </div>
+            )}
+
+            <Modal
+                onClose={() => closeModal()}
+                open={open}
+                mobilePage={false}
+                classNames={{
+                    modal: 'w-fit h-fit rounded-3xl',
+                }}
+            >
+                <Formik
+                    enableReinitialize
+                    initialValues={{
+                        id: healthPlan?.id || '',
+                        name: healthPlan?.name || '',
+                        type_health: healthPlan?.type_health || '',
+                        number_health: healthPlan?.number_health || '',
+                        validity: healthPlan?.validity || '',
+                        dat_ini: healthPlan?.dat_ini || '',
+                        dat_end: healthPlan?.dat_end || '',
+                    }}
+                    onSubmit={() => {}}
+                >
+                    <Form className="flex flex-col p-4 space-y-1">
+                        <h2 className="text-xl font-semibold text-center text-gray-800">
+                            {healthPlan
+                                ? 'Editar Plano de Saúde'
+                                : 'Adicionar Plano de Saúde'}
+                        </h2>
+                        <FieldControl
+                            ctx={healthPlan}
+                            name="name"
+                            label="Nome do Plano"
+                        />
+
+                        <FieldControl
+                            ctx={healthPlan}
+                            name="type_health"
+                            label="Tipo de Plano"
+                            placeholder="Digite o tipo de plano"
+                        />
+                        <FieldControl
+                            ctx={healthPlan}
+                            name="number_health"
+                            label="Número do Plano"
+                            placeholder="Digite o número do plano"
+                        />
+                        <FieldControl
+                            ctx={healthPlan}
+                            name="validity"
+                            label="Validade"
+                            type="date"
+                        />
+                        <FieldControl
+                            ctx={healthPlan}
+                            name="dat_ini"
+                            label="Data de Início"
+                            type="date"
+                        />
+                        <FieldControl
+                            ctx={healthPlan}
+                            name="dat_end"
+                            label="Data de Fim"
+                            type="date"
+                        />
+                        <div className="flex items-center justify-center gap-2 mt-4">
+                            <BtnCancel
+                                type="button"
+                                onClick={() => closeModal()}
+                                label="Cancelar"
+                            />
+                            <BtnPrimary type="submit" label="Salvar" />
+                        </div>
+                    </Form>
+                </Formik>
+            </Modal>
+        </>
+    )
+}
+
+export default withCompose(ModalHealthPlans)
--- source/pages/Modules/tutor/HealthPlan/components/container-health-plans/container-health-plans.tsx
+++ source/pages/Modules/tutor/HealthPlan/components/container-health-plans/container-health-plans.tsx
@@ -1,4 +1,5 @@
 import { FaHeartbeat, FaPlus } from 'react-icons/fa'
+import ModalHealthPlans from '~/Components/modals/health-plans'
 import type { IHealthPlan } from '~/services/helpers/health-plans'
 import { useHealthPlans } from '~/store/hooks/health-plans'
 
@@ -26,32 +27,37 @@ type ContainerHealthPlansProps = {
 const ContainerHealthPlans = ({ id_pet }: ContainerHealthPlansProps) => {
     if (!id_pet) return null
 
-    const { data, isLoading, isError } = useHealthPlans({ id_pet })
+    const { data, isPending, isError } = useHealthPlans({ id_pet })
 
     return (
         <>
-            <button
-                type="button"
-                onClick={() => console.log('Plano de saúde adicionado!')}
-                className="flex items-center w-full p-6 font-sans border-2 border-blue-400 border-dashed rounded-lg cursor-pointer shadow-theme-7 bg-gray-50 hover:bg-gray-100"
-            >
-                <div className="relative flex items-center justify-center p-4 bg-blue-200 rounded-full ">
-                    <div className="relative w-fit">
-                        <FaHeartbeat className="text-4xl text-blue-600 " />
-                        <FaPlus className="absolute bottom-0 right-0 ml-1 text-sm text-white " />
-                    </div>
-                </div>
-                <div className="ml-4 text-left">
-                    <h2 className="text-xl font-semibold text-gray-800">
-                        Adicionar Plano de Saúde
-                    </h2>
-                    <p className="text-gray-600">
-                        Clique para adicionar um novo plano de saúde ao seu Pet.
-                    </p>
-                </div>
-            </button>
+            <ModalHealthPlans>
+                {(showModal) => (
+                    <button
+                        type="button"
+                        onClick={() => showModal()}
+                        className="flex items-center w-full p-6 font-sans border-2 border-blue-400 border-dashed rounded-lg cursor-pointer shadow-theme-7 bg-gray-50 hover:bg-gray-100"
+                    >
+                        <div className="relative flex items-center justify-center p-4 bg-blue-200 rounded-full ">
+                            <div className="relative w-fit">
+                                <FaHeartbeat className="text-4xl text-blue-600 " />
+                                <FaPlus className="absolute bottom-0 right-0 ml-1 text-sm text-white " />
+                            </div>
+                        </div>
+                        <div className="ml-4 text-left">
+                            <h2 className="text-xl font-semibold text-gray-800">
+                                Adicionar Plano de Saúde
+                            </h2>
+                            <p className="text-gray-600">
+                                Clique para adicionar um novo plano de saúde ao seu
+                                Pet.
+                            </p>
+                        </div>
+                    </button>
+                )}
+            </ModalHealthPlans>
 
-            {isLoading && <div>Loading...</div>}
+            {isPending && <div>Loading...</div>}
 
             {isError && <div>Error, tente novamente</div>}
 
