f36eb75a53ce90525673285936f1432c443c8aa9
murilomontino <murilomontinojr@hotmail.com>
2024-09-09 18:08:10 -0300

    fix(pre-build): correcao na exclusao de pastas

--- script/pre-build.js
+++ script/pre-build.js
@@ -1,5 +1,5 @@
 const path = require('node:path')
-const fs = require('node:fs')
+const fs = require('fs')
 /**
  * @typedef {string[]} Mode
  */
@@ -10,8 +10,8 @@ for (let i = 0; i < mode?.length; i++) {
     const pagesDir = path.join(__dirname, '..', 'pages', modeName)
     const backupDir = path.join(__dirname, '..', 'pages-backup', modeName)
     try {
-        fs.cpSync(pagesDir, backupDir, { recursive: true })
-        fs.rmSync(backupDir, { recursive: true, force: true })
+        fs.cpSync(pagesDir, backupDir, { recursive: true, force: true })
+        fs.rmSync(pagesDir, { recursive: true, force: true })
     } catch (error) {
         if (error.code === 'ENOENT') {
             console.log(`Diretório ${pagesDir} não encontrado`)
