5d49b579abc23099a6e976b257b7a3f0f4f08266
Anabizo <aninhadealmeida0502@gmail.com>
2024-10-12 19:26:07 -0300

    test: configurando e atualizando storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,28 +1,40 @@
-import type { StorybookConfig } from '@storybook/nextjs'
+import type { StorybookConfig } from "@storybook/nextjs";
 
-import { dirname, join } from 'node:path'
+import { dirname, join } from "node:path";
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value: string): any {
-    return dirname(require.resolve(join(value, 'package.json')))
+  return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-    stories: ['../source/**/*.mdx', '../source/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-    addons: [
-        getAbsolutePath('@storybook/addon-onboarding'),
-        getAbsolutePath('@storybook/addon-links'),
-        getAbsolutePath('@storybook/addon-essentials'),
-        getAbsolutePath('@chromatic-com/storybook'),
-        getAbsolutePath('@storybook/addon-interactions'),
-        getAbsolutePath('@storybook/addon-a11y'),
-    ],
-    framework: {
-        name: getAbsolutePath('@storybook/nextjs'),
-        options: {},
-    },
-    staticDirs: ['../public'],
-}
-export default config
+  stories: [
+    "../source/**/*.mdx",
+    "../source/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+
+  addons: [
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath('@storybook/addon-a11y'),
+  ],
+
+  framework: {
+    name: getAbsolutePath("@storybook/nextjs"),
+    options: {},
+  },
+
+  staticDirs: ["../public"],
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
+};
+export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,18 +1,16 @@
-import type { Preview } from '@storybook/react'
-
-// CSS imports
-import '~/globals.scss'
-import '~/tailwind.css'
+import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
-    parameters: {
-        controls: {
-            matchers: {
-                color: /(background|color)$/i,
-                date: /Date$/i,
-            },
-        },
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
     },
-}
+  },
 
-export default preview
+  tags: ["autodocs"]
+};
+
+export default preview;
