ca72ad7aca1a5d65fb9d31133d98c652e5f26426
murilomontino <murilomontinojr@hotmail.com>
2024-10-13 19:27:20 -0300

    revert: remove dep @bbbtech/storybook-formik

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -21,7 +21,6 @@ const config: StorybookConfig = {
         getAbsolutePath('@storybook/addon-a11y'),
         getAbsolutePath('@abcaustralia/storybook-addon-a11ydocs'),
         getAbsolutePath('storybook-addon-performance'),
-        getAbsolutePath('@bbbtech/storybook-formik'),
     ],
 
     framework: {
