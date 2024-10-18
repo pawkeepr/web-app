4947a7b3b5ffd24494702e60bc1ed3342ea36f32
murilomontino <murilomontinojr@hotmail.com>
2024-10-18 10:04:24 -0300

    refactor: adição de tamanho min em card de pets

--- source/Components/molecules/map-card-feed-pets/map-card-feed-pets.tsx
+++ source/Components/molecules/map-card-feed-pets/map-card-feed-pets.tsx
@@ -21,7 +21,7 @@ const MapCardFeedPets = ({
     }
 
     return (
-        <div className="gap-1 mt-1 !overflow-x-auto flex flex-row w-full scrollable-x px-1 mobile:px-2">
+        <div className="gap-1 mt-1 !overflow-x-auto flex flex-row w-full scrollable-x px-1 mobile:px-2 h-40">
             {!isPending &&
                 pets?.map((pet) => (
                     <CardFeedPets
@@ -35,25 +35,22 @@ const MapCardFeedPets = ({
                         }}
                     />
                 ))}
-            {(isLoading || isPending) && (
-                <>
-                    {[0, 0, 0, 0, 0, 0].map((_, index) => (
-                        <div
-                            // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-                            key={index}
-                            className={card({
-                                className:
-                                    'px-2 my-2 py-2 flex-1 w-28 h-36 bg-gradient-to-r from-white via-gray-200 to-gray-300 animate-pulse',
-                            })}
-                            style={{
-                                minWidth: '7rem ',
-                                minHeight: '9rem',
-                                maxWidth: '7rem',
-                            }} // fix width and height
-                        />
-                    ))}
-                </>
-            )}
+            {(isLoading || isPending) &&
+                [0, 0, 0, 0, 0, 0].map((_, index) => (
+                    <div
+                        // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                        key={index}
+                        className={card({
+                            className:
+                                'px-2 my-2 py-2 flex-1 w-28 h-36 bg-gradient-to-r from-white via-gray-200 to-gray-300 animate-pulse',
+                        })}
+                        style={{
+                            minWidth: '7rem ',
+                            minHeight: '9rem',
+                            maxWidth: '7rem',
+                        }} // fix width and height
+                    />
+                ))}
         </div>
     )
 }
