eb7c63835a6c2acdafa7a84f0b5b268320aaa48a
murilomontino <murilomontinojr@hotmail.com>
2024-11-08 10:13:58 -0300

    test: corrige btn stories

--- source/Components/atoms/btn/btn.stories.tsx
+++ source/Components/atoms/btn/btn.stories.tsx
@@ -3,19 +3,14 @@ import { fn } from '@storybook/test'
 
 import { BtnCompose, type BtnProps } from './btn'
 
-BtnCompose.prototype.displayName = 'Btn'
-
 const meta = {
     title: 'Components/Atoms/Button',
     component: BtnCompose,
-
     parameters: {
-        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
         layout: 'centered',
     },
-    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+    args: { onClick: fn() },
     tags: ['autodocs'],
-    // More on argTypes: https://storybook.js.org/docs/api/argtypes
     argTypes: {
         color: {
             description: 'The color of the button',
@@ -35,19 +30,7 @@ const meta = {
                 },
             },
         },
-        isLoading: {
-            control: {
-                type: 'boolean',
-            },
-        },
-        condition: {
-            control: {
-                type: 'boolean',
-            },
-        },
     },
-    // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-    args: { onClick: fn() },
 } satisfies Meta<typeof BtnCompose>
 export default meta
 const Template: StoryFn<BtnProps> = (args) => <BtnCompose {...args} />
