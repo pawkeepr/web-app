90aca1794c512a449dcfb3e8a91b2b7f4b245bcb
murilomontino <murilomontinojr@hotmail.com>
2024-11-08 10:22:29 -0300

    fix: remove displayName em 'tag.stories'

--- source/Components/atoms/tag/tag.stories.tsx
+++ source/Components/atoms/tag/tag.stories.tsx
@@ -1,24 +1,15 @@
 import type { Meta, StoryFn } from '@storybook/react'
 import { fn } from '@storybook/test'
-import React from 'react'
 
 import Tag from './tag'
-
-Tag.prototype.displayName = 'Tag'
 
 const meta = {
     title: 'Components/Atoms/Tag',
     component: Tag,
-
     parameters: {
-        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
         layout: 'centered',
     },
-    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
     tags: ['autodocs'],
-    // More on argTypes: https://storybook.js.org/docs/api/argtypes
-    argTypes: {},
-    // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
     args: { onClick: fn() },
 } satisfies Meta<typeof Tag>
 export default meta
