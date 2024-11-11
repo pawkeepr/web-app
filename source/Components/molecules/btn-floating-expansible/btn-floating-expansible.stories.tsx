271d83f06aa1e7bd3b4017afb1e3610724345ff2
murilomontino <murilomontinojr@hotmail.com>
2024-11-11 10:25:22 -0300

    fix: add btn-floating-expansible component with child links in stories

--- source/Components/molecules/btn-floating-expansible/btn-floating-expansible.stories.tsx
+++ source/Components/molecules/btn-floating-expansible/btn-floating-expansible.stories.tsx
@@ -1,15 +1,36 @@
-import type { Meta, StoryFn } from "@storybook/react"
+import type { Meta, StoryObj } from "@storybook/react"
 import { MdPets } from "react-icons/md"
 import {
-    BtnFloatingExpansible,
-    type BtnFloatingExpansibleProps,
+    BtnFloatingExpansible
 } from "./btn-floating-expansible"
+const childrenMock = [
+    {
+        title: "Title 1",
+        icon: () => <MdPets />,
+        onClick: () => { },
+    },
+    {
+        title: "Title 2",
+        icon: () => <MdPets />,
+        href: "/tutor/pet",
+    },
+    {
+        title: "Title 3",
+        icon: () => <MdPets />,
+        href: "/tutor/pet",
+    },
+]
 
 const meta = {
     title: "Components/Molecules/ButtonFloatingExpansible",
     component: BtnFloatingExpansible,
     parameters: {
         layout: "centered",
+    },
+    args: {
+        title: "Some Title",
+        icon: () => <MdPets />,
+        childLinks: childrenMock,
     },
     tags: ["autodocs"],
     argTypes: {
@@ -49,31 +70,15 @@ const meta = {
 } satisfies Meta<typeof BtnFloatingExpansible>
 export default meta
 
-const Template: StoryFn<BtnFloatingExpansibleProps> = (args) => (
-    <BtnFloatingExpansible {...args} />
-)
 
-const childrenMock = [
-    {
-        title: "Title 1",
-        icon: () => <MdPets />,
-        onClick: () => { },
-    },
-    {
-        title: "Title 2",
-        icon: () => <MdPets />,
-        href: "/tutor/pet",
-    },
-    {
-        title: "Title 3",
-        icon: () => <MdPets />,
-        href: "/tutor/pet",
-    },
-]
+type Story = StoryObj<typeof BtnFloatingExpansible>
 
-export const Primary = Template.bind({})
-Primary.args = {
-    icon: () => <MdPets />,
-    title: "Some Title",
-    childLinks: childrenMock,
+export const Default: Story = {
+    args: {
+        icon: () => <MdPets />,
+        title: "Some Title",
+        childLinks: childrenMock,
+    }
 }
+
+
