e80137c9d1580f7cd73fa5c9c584d8a8b3833ffc
murilomontino <murilomontinojr@hotmail.com>
2024-11-14 22:55:02 -0300

    chore: adiciona mock para auth-helpers utilizando Vitest

---
+++ __mocks__/auth-helpers.ts
@@ -0,0 +1,11 @@
+import { vi } from 'vitest';
+
+vi.mock('~/services/helpers/auth', async () => {
+    const getUser = vi.fn().mockResolvedValue({ data: { id: 1, name: 'John Doe' } })
+    const postJwtLogin = vi.fn().mockResolvedValue({ data: { access_token: 'token' } })
+
+    return {
+        getUser,
+        postJwtLogin
+    }
+})
