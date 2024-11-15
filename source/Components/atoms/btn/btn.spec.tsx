c91fcdd9f8aa8a4d4da799e1d503df3180d46ed2
murilomontino <murilomontinojr@hotmail.com>
2024-11-14 22:23:01 -0300

    refactor: atualiza testes do componente Btn para usar 'stories' do Storybook

--- source/Components/atoms/btn/btn.spec.tsx
+++ source/Components/atoms/btn/btn.spec.tsx
@@ -1,33 +1,49 @@
 import { composeStories } from '@storybook/react'
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import { describe, expect, it, vi } from 'vitest'
-import { Btn } from './btn'
 
 import * as stories from './btn.stories'
 
-const { Primary } = composeStories(stories)
-
-
+const { Primary: Default } = composeStories(stories)
 
 describe('Test Btn component (Unit)', () => {
-    it('should render the button with label', () => {
-        render(<Btn label="Click me" />)
+    it('should render the button with label', async () => {
+        await Default.run({
+            args: {
+                label: 'Click me',
+            },
+        })
+
         expect(screen.getByText('Click me')).toBeInTheDocument()
     })
 
-    it('should render the button with icon', () => {
-        render(<Btn icon={<span>🚀</span>} />)
+    it('should render the button with icon', async () => {
+        await Default.run({
+            args: {
+                icon: <span>🚀</span>,
+            },
+        })
         expect(screen.getByText('🚀')).toBeInTheDocument()
     })
 
-    it('should render the button with custom class', () => {
-        render(<Btn className="custom-btn" data-testid="btn" />)
+    it('should render the button with custom class', async () => {
+        await Default.run({
+            args: {
+                className: 'custom-btn',
+                'data-testid': 'btn',
+            },
+        })
         expect(screen.getByTestId('btn')).toHaveClass('custom-btn')
     })
 
-    it('should call onClick handler when clicked', () => {
+    it('should call onClick handler when clicked', async () => {
         const handleClick = vi.fn()
-        render(<Btn onClick={handleClick} data-testid="btn" />)
+        await Default.run({
+            args: {
+                'data-testid': 'btn',
+                onClick: handleClick,
+            },
+        })
         screen.getByTestId('btn').click()
         expect(handleClick).toHaveBeenCalled()
     })
