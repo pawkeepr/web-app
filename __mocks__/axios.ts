7dbb93829e316c78eafad3d9539cdb758c8473a2
murilomontino <murilomontinojr@hotmail.com>
2024-11-14 22:54:55 -0300

    chore: adiciona mock do axios para testes com Vitest

---
+++ __mocks__/axios.ts
@@ -0,0 +1,18 @@
+// import axiosMock from 'axios-mock-adapter';
+// import { vi } from 'vitest';
+// import { api } from '~/services/api';
+
+// vi.mock('axios', async () => {
+//     const actual = await vi.importActual("axios")
+//     const mock = new axiosMock(api, { delayResponse: 2000 });
+
+//     mock.onGet('/usuarios/login').reply(200, { id: 1, name: 'John' });
+//     mock.onGet('/usuarios/logado').reply(200, { id: 1, name: 'John' });
+
+//     return {
+//         ...actual,
+//         default: mock
+
+//     };
+// })
+
